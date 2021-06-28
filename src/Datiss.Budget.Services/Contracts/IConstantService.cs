b07ffe27ae4b34f0c156dca5f74e29592d2ad5cb
iman nemati <imun22@gmmail.com>
2021-06-28 18:00:18 +0430

    Add GetParentsAsync to ConstantService

--- src/Datiss.Budget.Services/Contracts/IConstantService.cs
+++ src/Datiss.Budget.Services/Contracts/IConstantService.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Datiss.Budget.Services.Infrastructure;
+using Datiss.Budget.Services.Models;
 using Datiss.Budget.ViewModels;
 
 namespace Datiss.Budget.Services.Contracts
@@ -12,5 +14,7 @@ namespace Datiss.Budget.Services.Contracts
         Task<ValidationResult> UpdateAsync(UpdateConstantViewModel model);
 
         Task<ValidationResult> SoftDeleteAsync(int id);
+
+        Task<IEnumerable<DropDownItem>> GetParentsAsync();
     }
 }
