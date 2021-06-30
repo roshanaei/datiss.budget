b6b8157710674a5c18643cce33aedcaf746ffb8b
Hessam <77877018+roshanaei@users.noreply.github.com>
2021-06-30 13:48:54 +0430

    Hessam/refactor (#3)
    * winstallfeeservice
    
    * IWaterInstallFee

---
+++ src/Datiss.Budget.Services/Contracts/IWaterInstallFeeService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Datiss.Budget.Services.Infrastructure;
+using Datiss.Budget.Services.Models;
+using Datiss.Budget.ViewModels;
+
+namespace Datiss.Budget.Services.Contracts
+{
+    public interface IWaterInstallFeeService
+    {
+        Task<ValidationResult> AddAsync(AddWaterInstallFeeViewModel model);
+
+        Task<ValidationResult> UpdateAsync(UpdateWaterInstallFeeViewModel model);
+
+        Task<ValidationResult> HardDeleteAsync(int Id);
+    }
+}
