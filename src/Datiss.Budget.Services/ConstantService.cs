56a93f8974ef76e4ed72bc318eafbc9f51f2e995
iman nemati <imun22@gmmail.com>
2021-07-03 16:56:50 +0430

    Fix Constant Service SoftDelete

--- src/Datiss.Budget.Services/ConstantService.cs
+++ src/Datiss.Budget.Services/ConstantService.cs
@@ -83,6 +83,8 @@ namespace Datiss.Budget.Services
 
             entity.Status = EntityStatus.Deleted;
 
+            await _uow.SaveChangesAsync();
+
             return ValidationResult.Success();
         }
 
