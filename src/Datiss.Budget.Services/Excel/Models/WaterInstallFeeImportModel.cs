40df1df4dbd8e0dc6ffa69ea3b03d2a7fbe4dbf3
iman nemati <imun22@gmmail.com>
2021-07-19 18:41:00 +0430

    Add Sample Import model

---
+++ src/Datiss.Budget.Services/Excel/Models/WaterInstallFeeImportModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ganss.Excel;
+
+namespace Datiss.Budget.Services.Excel.Models
+{
+    public class WaterInstallFeeImportModel
+    {
+        [Column(0)]
+        public int YearId { get; set; }
+
+        [Column(1)]
+        public int OrganizationId { get; set; }
+
+        [Column(2)]
+        public int DWaterTypeId { get; set; }
+
+        [Column(3)]
+        public int WInstllFee { get; set; }
+    }
+}
