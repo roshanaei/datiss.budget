24a8558e1b46b7593e35871c777dc0f95b49c126
kate <77887199+katayon@users.noreply.github.com>
2021-08-26 12:41:53 +0430

    Kati/waste sale split (#12)
    * DesignMenu
    
    * AddWasteSalesSplitEntities
    
    * AddwasteSalesSplitConfiguration
    
    * AddWasteSalesSplitMigration
    
    * AddWasteSalesSplitViewModel
    
    * add-IWastesalesSplit-wasteSalesSplitDTO-wasteSalesSplitService-wasteSalesSplitController-ViewModel-View
    
    * AddWasteSalesSplitToIOC
    
    * RemoveAppSettingChanges

---
+++ src/Datiss.Budget.Entities/DWH/WasteSalesSplit.cs
@@ -0,0 +1,36 @@
+﻿using Datiss.Budget.Entities.AuditableEntity;
+
+namespace Datiss.Budget.Entities.DWH
+{
+    public  class WasteSalesSplit:IAuditableEntity
+    {
+        public WasteSalesSplit() { }
+
+        #region Properties
+         
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+
+        public int OrganizationId { get; set;}
+
+        public int UserTypeId { get; set; }
+
+        public int WsPipeDiameterId { get; set; }
+
+        public int NumberSales { get; set; }
+
+        public int UnitSales { get; set; }
+        #endregion
+
+        #region Navigation
+        public FinanceYear FinanceYear { get; set; }
+
+        public Organization Organization { get; set; }
+
+        public Constant UserType { get; set; }
+
+        public Constant WsPipeDiameter { get; set; }
+        #endregion
+    }
+}
