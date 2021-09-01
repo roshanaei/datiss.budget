edd2ba321e464ba8d7ef8eb95b652d9e4ce243b5
Your Name <you@example.com>
2021-09-01 15:21:40 +0430

    AddTablesFieldTitleEntities

---
+++ src/Datiss.Budget.Entities/DWH/TablesFieldTitle.cs
@@ -0,0 +1,28 @@
+﻿using Datiss.Budget.Entities.AuditableEntity;
+using Datiss.Budget.Enum;
+
+
+namespace Datiss.Budget.Entities.DWH
+{
+    public class TablesFiledTitle : IAuditableEntity
+    {
+        public TablesFiledTitle() { }
+
+        #region Properties
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public EntityStatus Status { get; set; }
+
+        public TablesName TableName { get; set; }
+
+        public SectionName SectionName { get; set; }
+
+
+        public int DisplayOrder { get; set; }
+
+        #endregion
+
+    }
+}
---
+++ src/Datiss.Budget.Entities/Enum/TablesName.cs
@@ -0,0 +1,7 @@
+﻿namespace Datiss.Budget.Enum
+{
+    public enum TablesName
+    {
+
+    }
+}
