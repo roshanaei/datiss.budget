214df10acc3c67a1170f15614fc90247cbf60064
Your Name <you@example.com>
2021-09-04 10:44:20 +0430

    AddMigrationForaddAverageCapacityTosalessplit

---
+++ src/Datiss.Budget.DataLayer/Migrations/20210904060230_AddAverageCapacityToSalesSplits.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Datiss.Budget.DataLayer.Migrations
+{
+    public partial class AddAverageCapacityToSalesSplits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "AverageCapacity",
+                table: "SalesSplitWs_Y",
+                type: "decimal(18,6)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "AverageCapacity",
+                table: "SalesSplitW_Y",
+                type: "decimal(18,6)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AverageCapacity",
+                table: "SalesSplitWs_Y");
+
+            migrationBuilder.DropColumn(
+                name: "AverageCapacity",
+                table: "SalesSplitW_Y");
+        }
+    }
+}
