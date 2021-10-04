70bca74853aa01390115f2e4769e40b84870b297
iman nemati <imun22@gmmail.com>
2021-10-04 17:22:46 +0330

    Add Report and ReportParam entities

---
+++ src/Datiss.Budget.Entities/Enum/ReportParamType.cs
@@ -0,0 +1,12 @@
+﻿namespace Datiss.Budget.Enum
+{
+    public enum ReportParamType {
+
+        Text = 0,
+        Number = 1,
+        Date = 2,
+        Year = 3,
+        Organization = 4,
+        Constant = 5
+    }
+}
