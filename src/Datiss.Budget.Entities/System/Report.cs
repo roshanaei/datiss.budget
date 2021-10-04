26132ea496b7573f35649a4462fd2f3dbc7ae0a8
iman nemati <imun22@gmmail.com>
2021-10-04 17:22:46 +0330

    Add Report and ReportParam entities

---
+++ src/Datiss.Budget.Entities/System/Report.cs
@@ -0,0 +1,39 @@
+﻿using Datiss.Budget.Entities.AuditableEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datiss.Budget.Enum;
+
+namespace Datiss.Budget.Entities
+{
+    public class Report : IAuditableEntity
+    {
+
+        public Report() {
+            Params = new HashSet<ReportParam>();
+        }
+
+        #region Properties
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public EntityStatus Status { get; set; }
+
+        public byte[] FileData { get; set; }
+
+        #endregion
+
+        #region Navigations
+
+        public ICollection<ReportParam> Params { get; set; }
+
+        #endregion
+    }
+}
---
+++ src/Datiss.Budget.Entities/System/ReportParam.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datiss.Budget.Entities.AuditableEntity;
+using Datiss.Budget.Enum;
+
+namespace Datiss.Budget.Entities
+{
+    public class ReportParam : IAuditableEntity
+    {
+
+        #region Properties
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+
+        public ReportParamType ParamType { get; set; }
+
+        public int ReportId { get; set; }
+
+        #endregion
+
+        #region Navigations
+
+        public Report Report { get; set; }
+
+        #endregion
+    }
+}
