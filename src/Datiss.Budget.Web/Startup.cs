a397b8fb7667a26e627eecdf2abb9f9389f229f6
Your Name <you@example.com>
2021-10-14 12:40:28 +0330

    AddReportViewerAddPaginationToForms

--- src/Datiss.Budget.Web/Startup.cs
+++ src/Datiss.Budget.Web/Startup.cs
@@ -79,11 +79,12 @@ namespace Datiss.Budget
 
                 endpoints.MapControllerRoute(
                     name: "areaRoute",
-                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
                 endpoints.MapRazorPages();
             });
