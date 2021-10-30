96717a07bd40dc23de5e3f9a30b2a548c38fd934
katayon <katayon.bagherimaki@gmail.com[Bgit git pull origin mastergit config --global user.email katayon.bagherimaki@gmail.com>
2021-10-31 01:47:55 +0330

    modify FinanceYear Service

--- src/Datiss.Budget.Services/FinanceYearService.cs
+++ src/Datiss.Budget.Services/FinanceYearService.cs
@@ -79,7 +79,6 @@ namespace Datiss.Budget.Services
         {
             var result = new PagedResult<FinanceYearViewModel>
             {
-               
             };
 
             var query = Query();
@@ -104,11 +103,11 @@ namespace Datiss.Budget.Services
             return await Task.FromResult(result);
         }
 
-        public async Task<IEnumerable<DropDownItem>> GetDropDownDataAsync() 
-            => await Query().Select(x => new DropDownItem {
-                Id = x.Id,
-                Title = x.Year.ToString()
-            }).ToListAsync();
+        //public async Task<IEnumerable<DropDownItem>> GetDropDownDataAsync() 
+        //    => await Query().Select(x => new DropDownItem {
+        //        Id = x.Id,
+        //        Title = x.Year.ToString()
+        //    }).ToListAsync();
         
     }
 }
