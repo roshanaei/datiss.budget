fffa3ab693e62c533ccc5d12a21673d0cfae9ee6
iman nemati <imun22@gmmail.com>
2021-11-13 03:14:04 +0330

    redirect to login if the user is not authenticated

--- src/Datiss.Budget.Web/Controllers/HomeController.cs
+++ src/Datiss.Budget.Web/Controllers/HomeController.cs
@@ -1,16 +1,27 @@
-﻿using DNTBreadCrumb.Core;
+﻿using System;
+using DNTBreadCrumb.Core;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using DNTCommon.Web.Core;
+using Datiss.Budget.Security;
 
 namespace Datiss.Budget.Controllers
 {
     [BreadCrumb(Title = "خانه", UseDefaultRouteUrl = true, Order = 0)]
     public class HomeController : Controller
     {
+        private readonly IUserContext _userContext;
+
+        public HomeController(IUserContext userContext) {
+            _userContext = userContext ?? throw new ArgumentNullException(nameof(userContext));
+        }
+
         [BreadCrumb(Title = "ایندکس", Order = 1)]
         public IActionResult Index()
         {
+            if (!_userContext.IsAuthenticated)
+                return RedirectToAction("Login", "Index", new { area = "Identity" });
+
             return View();
         }
 
