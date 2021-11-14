5d57ddf3f03cdb3ed43221d52b9701d11e7de24b
Your Name <you@example.com>
2021-11-14 16:37:44 +0330

    Add SalesSplitTotel Migration

--- src/Datiss.Budget.DataLayer/Configurations/SalesSplitTotalConfiguration.cs
+++ src/Datiss.Budget.DataLayer/Configurations/SalesSplitTotalConfiguration.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Enum;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Datiss.Budget.DataLayer.Configurations
+namespace Datiss.Budget.DataLayer.Mappings
 {
-    class SalesSplitTotalConfiguration
+    public class SalesSplitTotalConfiguration : IEntityTypeConfiguration<SalesSplitTotal>
     {
+        public void Configure(EntityTypeBuilder<SalesSplitTotal> builder)
+        {
+            builder.ToTable("SalesSplitTotal");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                    .HasColumnName("SalesSplitTotalId");
+
+
+            builder.HasOne(x => x.FinanceYear)
+                    .WithMany(x => x.SalesSplitTotal)
+                    .HasForeignKey(x => x.YearId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.Organization)
+                    .WithMany(x => x.SalesSplitTotal)
+                    .HasForeignKey(x => x.OrganizationId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.UserType)
+                    .WithMany(x => x.SalesSplitTotal)
+                    .HasForeignKey(x => x.UserTypeId)
+                    .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
