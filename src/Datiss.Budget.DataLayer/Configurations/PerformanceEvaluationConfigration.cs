356afffc4ef73437634c2d504a54d9eb61474cb6
Your Name <you@example.com>
2021-11-14 15:27:39 +0330

    Add PerformanceEvaluation Configuration

---
+++ src/Datiss.Budget.DataLayer/Configurations/PerformanceEvaluationConfigration.cs
@@ -0,0 +1,35 @@
+﻿using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Enum;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Datiss.Budget.DataLayer.Mappings
+{
+    public class PerformanceEvaluationConfigration :IEntityTypeConfiguration<PerformanceEvaluation>
+    {
+        public void Configure(EntityTypeBuilder<PerformanceEvaluation> builder)
+        {
+            builder.ToTable("PerformanceEvaluation");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Status)
+                    .HasDefaultValue(EntityStatus.Enabled);
+
+            builder.HasOne(x => x.FinanceYear)
+                    .WithMany(x => x.PerformanceEvaluation)
+                    .HasForeignKey(x => x.YearId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.Organization)
+                    .WithMany(x => x.PerformanceEvaluation)
+                    .HasForeignKey(x => x.OrganizationId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.TablesFiled)
+                    .WithMany(x => x.PerformanceEvaluation)
+                    .HasForeignKey(x => x.TableFieldId)
+                    .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
