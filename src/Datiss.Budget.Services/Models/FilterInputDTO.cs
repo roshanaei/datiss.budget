191045803c65e48d614879611095d7c955110d3c
Erfan <erfan.eftekharizade@gmail.com>
2021-11-15 16:09:07 +0330

    Complate FilterDTO & InterFace

--- src/Datiss.Budget.Services/Models/FilterInputDTO.cs
+++ src/Datiss.Budget.Services/Models/FilterInputDTO.cs
@@ -120,4 +120,11 @@ namespace Datiss.Budget.Services.Models
         public int? OrganizationId { get; set; }
         public int? UserTypeId { get; set; }
     }
+
+    public class IncomeForcastWsFilterDTO : FilterInputDTO
+    {
+        public int? YearId { get; set; }
+        public int? OrganizationId { get; set; }
+        public int? UserTypeId { get; set; }
+    }
 }
