c3f012ea2a9c3cef80cf4464e73ba84a4f33c91c
Erfan <erfan.eftekharizade@gmail.com>
2021-11-15 16:22:39 +0330

    Resolve DTO

---
+++ src/Datiss.Budget.Services/IncomeForcastWsService.cs
@@ -0,0 +1,23 @@
+﻿using Datiss.Budget.Common.GuardToolkit;
+using Datiss.Budget.DataLayer.Context;
+using Datiss.Budget.Entities;
+using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Services.Contracts;
+using Datiss.Budget.Services.Contracts.Identity;
+using Datiss.Budget.Services.Excel;
+using Datiss.Budget.Services.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services
+{
+    public class IncomeForcastWsService
+    {
+        
+    }
+}
+
--- src/Datiss.Budget.Services/Models/IncomeForcastWsDTO.cs
+++ src/Datiss.Budget.Services/Models/IncomeForcastWsDTO.cs
@@ -9,25 +9,14 @@ namespace Datiss.Budget.Services.Models
     public class CreateIncomeForcastWsDTO
     {
         public int YearId { get; set; }
-
         public int OrganizationId { get; set; }
-
         public int UserTypeId { get; set; }
-
         public string UserTypeTitle { get; set; }
-
         public int NumberUser { get; set; }
-
         public int UnitUser { get; set; }
-
-        public int WasteInstllIncome { get; set; }
-
+        public int WasteInstallIncome { get; set; }
         public int WasteBranchIncome { get; set; }
-
-        public int WasteNote2Income { get; set; }
-
         public int WasteNote3Income { get; set; }
-
         public int WsNote11Income { get; set; }
     }
 
@@ -47,9 +36,8 @@ namespace Datiss.Budget.Services.Models
         public string UserTypeDisplay { get; set; }
         public int NumberUser { get; set; }
         public int UnitUser { get; set; }
-        public int WasteInstllIncome { get; set; }
+        public int WasteInstallIncome { get; set; }
         public int WasteBranchIncome { get; set; }
-        public int WasteNote2Income { get; set; }
         public int WasteNote3Income { get; set; }
         public int WsNote11Income { get; set; }
     }
