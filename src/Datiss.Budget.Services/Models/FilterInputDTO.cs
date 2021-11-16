ce276f05dc404fbd85177107c2ba822e730d6632
Erfan <58788132+ErfanEftekhari@users.noreply.github.com>
2021-11-16 14:37:06 +0330

    Erfan/income forcast ws (#28)
    * Complate Index wasteinstallfee
    
    * Fixed WaterInstallFee filter bug
    
    * Comolate Service For WasteInstallFee&WaterSalesSplit&WasteSalesSplit
    
    * Comolate Service For BranchFeeAmount & Constant
    
    * Comolate Service For FinanceYear
    
    * Comolate view
    
    * fix pull req problem
    
    * Resolve MasterItem
    
    * Complate UserTypeCapacity Services
    
    * New Complate UserTypeCapacity Services
    
    * Interface & DTO
    
    * Complate Service
    
    * Complate DTO
    
    * Complate FilterDTO & InterFace
    
    * Resolve DTO
    
    * Complate Service
    
    Co-authored-by: Iman Nemati <imun22@gmail.com>

--- src/Datiss.Budget.Services/Models/FilterInputDTO.cs
+++ src/Datiss.Budget.Services/Models/FilterInputDTO.cs
@@ -114,4 +114,20 @@ namespace Datiss.Budget.Services.Models
         public int? OrganizationId { get; set; }
         public int? UserTypeId { get; set; }
     }
+
+
+    public class IncomeForcastFilterDTO : FilterInputDTO
+    {
+        public int? YearId { get; set; }
+        public int? OrganizationId { get; set; }
+        public int? UserTypeId { get; set; }
+    }
+
+    public class IncomeForcastWsFilterDTO : FilterInputDTO
+    {
+        public int? YearId { get; set; }
+        public int? OrganizationId { get; set; }
+        public int? UserTypeId { get; set; }
+    }
+
 }
---
+++ src/Datiss.Budget.Services/Models/IncomeForcastDTO.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Models
+{
+    public class CreateIncomeForcastDTO
+    {
+        public int YearId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public int UserTypeId { get; set; }
+
+        public string UserTypeTitle { get; set; }
+
+        public int NumberUser { get; set; }
+
+        public int UnitUser { get; set; }
+
+        public int WaterInstllIncome { get; set; }
+
+        public int WaterBranchIncome { get; set; }
+
+        public int WaterNote2Income { get; set; }
+
+        public int WaterNote3Income { get; set; }
+
+        public int WNote11Income { get; set; }
+    }
+
+    public class UpdateIncomeForcastDTO : CreateIncomeForcastDTO
+    {
+        public int Id { get; set; }
+    }
+
+    public class IncomeForcastDTO
+    {
+        public int Id { get; set; }
+        public int YearId { get; set; }
+        public int Year { get; set; }
+        public int OrganizationId { get; set; }
+        public string OrganizationDisplay { get; set; }
+        public int UserTypeId { get; set; }
+        public string UserTypeDisplay { get; set; }
+        public int NumberUser { get; set; }
+        public int UnitUser { get; set; }
+        public int WaterInstllIncome { get; set; }
+        public int WaterBranchIncome { get; set; }
+        public int WaterNote2Income { get; set; }
+        public int WaterNote3Income { get; set; }
+        public int WNote11Income { get; set; }
+    }
+}
