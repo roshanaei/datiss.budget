db5c73e8217c78c3de6936876873536450895837
Erfan <58788132+ErfanEftekhari@users.noreply.github.com>
2021-11-16 14:37:06 +0330

    Erfan/income forcast ws (#28)
    * Complate Index wasteinstallfee
    
    * Fixed WaterInstallFee filter bug
    
    * Comolate Service For WasteInstallFee&WaterSalesSplit&WasteSalesSplit
    
    * Comolate Service For BranchFeeAmount & Constant
    
    * Comolate Service For FinanceYear
    
    * Comolate view
    
    * fix pull req problem
    
    * Resolve MasterItem
    
    * Complate UserTypeCapacity Services
    
    * New Complate UserTypeCapacity Services
    
    * Interface & DTO
    
    * Complate Service
    
    * Complate DTO
    
    * Complate FilterDTO & InterFace
    
    * Resolve DTO
    
    * Complate Service
    
    Co-authored-by: Iman Nemati <imun22@gmail.com>

---
+++ src/Datiss.Budget.Services/Excel/Models/IncomeForcastWsImportModel.cs
@@ -0,0 +1,34 @@
+﻿using Ganss.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Excel.Models
+{
+    public class IncomeForcastWsImportModel
+    {
+        [Column(0, MappingDirections.Both, Letter = "A")]
+        public int YearId { get; set; }
+
+        [Column(1, MappingDirections.Both, Letter = "B")]
+        public int OrganizationId { get; set; }
+
+        [Column(2, MappingDirections.Both, Letter = "C")]
+        public int DWaterTypeId { get; set; }
+
+        [Column(3, MappingDirections.Both, Letter = "D")]
+        public int NumberUser { get; set; }
+        [Column(4, MappingDirections.Both, Letter = "E")]
+        public int UnitUser { get; set; }
+        [Column(5, MappingDirections.Both, Letter = "F")]
+        public int WasteBranchIncome { get; set; }
+        [Column(6, MappingDirections.Both, Letter = "G")]
+        public int WasteInstallIncome { get; set; }
+        [Column(7, MappingDirections.Both, Letter = "H")]
+        public int WasteNote3Income { get; set; }
+        [Column(8, MappingDirections.Both, Letter = "I")]
+        public int WsNote11Income { get; set; }
+    }
+}
