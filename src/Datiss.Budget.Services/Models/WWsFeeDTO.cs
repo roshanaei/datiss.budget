2fb93482e77bd77c710e4ca29ee465f78861149d
Erfan <erfan.eftekharizade@gmail.com>
2021-11-17 09:26:22 +0330

    Complate DTO/WWsFee

---
+++ src/Datiss.Budget.Services/Models/WWsFeeDTO.cs
@@ -0,0 +1,49 @@
+﻿using Datiss.Budget.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Models
+{
+    public class CreateWWsFeeDTO
+    {
+        public int YearId { get; set; }
+        public int OrganizationId { get; set; }
+        public int UserTypeId { get; set; }
+        public string UserTypeTitle { get; set; }
+        public ActivityType ActivityType { get; set; }
+        public int UsageLayerId { get; set; }
+        public int P1Fee { get; set; }
+        public int P2Fee { get; set; }
+        public int P1Note3 { get; set; }
+        public int P2Note3 { get; set; }
+        public int P1Note7 { get; set; }
+        public int P2Note7 { get; set; }
+    }
+
+    public class UpdateWWsFeeDTO : CreateWWsFeeDTO
+    {
+        public int Id { get; set; }
+    }
+
+    public class WWsFeeDTO
+    {
+        public int Id { get; set; }
+        public int YearId { get; set; }
+        public int Year { get; set; }
+        public int OrganizationId { get; set; }
+        public string OrganizationDisplay { get; set; }
+        public int UserTypeId { get; set; }
+        public string UserTypeDisplay { get; set; }
+        public ActivityType ActivityType { get; set; }
+        public int UsageLayerId { get; set; }
+        public int P1Fee { get; set; }
+        public int P2Fee { get; set; }
+        public int P1Note3 { get; set; }
+        public int P2Note3 { get; set; }
+        public int P1Note7 { get; set; }
+        public int P2Note7 { get; set; }
+    }
+}
