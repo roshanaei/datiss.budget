6de7263628ad9c06e93cc9119df4d166c18fc9c0
Erfan <erfan.eftekharizade@gmail.com>
2021-11-17 10:00:37 +0330

    Complate Interface/FeeCity

---
+++ src/Datiss.Budget.Services/Contracts/IFeeCityService.cs
@@ -0,0 +1,37 @@
+﻿using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Services.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Contracts
+{
+    public interface IFeeCityService
+    {
+        Task<FeeCity> GetByIdAsync(int id);
+
+        //Task<ValidationResult> AddAsync(CreateFeeCityDTO model);
+
+        //Task<ValidationResult> UpdateAsync(UpdateFeeCityDTO model);
+
+        Task HardDeleteAsync(int Id);
+
+        Task HardDeleteAsync(int yearId, int organizationId);
+
+        //Task<int> CalculationAsync(int yearId, int organizationId);
+
+        Task<PagedResult<FeeCityDTO>> GetListAsync(FeeCityFilterDTO filter);
+
+        Task CopyAsync(int sourceYearId, int sourceOrgId, int destYearId);
+
+        Task<Stream> ExportExcelAsync(FeeCityFilterDTO filter);
+
+        Task<IEnumerable<FeeCityDTO>> GetExportItemsAsync(FeeCityFilterDTO filter);
+
+        Task ImportExcelAsync(IFormFile fileInfo);
+    }
+}
