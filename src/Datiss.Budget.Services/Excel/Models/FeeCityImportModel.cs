60fd0aed5233042a977e7d35bcd4bed0eed976fd
Erfan <erfan.eftekharizade@gmail.com>
2021-11-17 09:58:35 +0330

    Complate ExelModel/FeeCity

---
+++ src/Datiss.Budget.Services/Excel/Models/FeeCityImportModel.cs
@@ -0,0 +1,24 @@
+﻿using Ganss.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Excel.Models
+{
+    public class FeeCityImportModel
+    {
+        [Column(0, MappingDirections.Both, Letter = "A")]
+        public int YearId { get; set; }
+
+        [Column(1, MappingDirections.Both, Letter = "B")]
+        public int OrganizationId { get; set; }
+
+        [Column(2, MappingDirections.Both, Letter = "C")]
+        public decimal DomesticPrice { get; set; }
+
+        [Column(3, MappingDirections.Both, Letter = "D")]
+        public decimal NDomesticPrice { get; set; }
+    }
+}
