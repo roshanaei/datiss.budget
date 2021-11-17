a6cbc273918261657f3a2ac651f9241189f3c3f8
Erfan <erfan.eftekharizade@gmail.com>
2021-11-17 11:33:47 +0330

    Edit Name ExelModel/IncomeCurrentWH

--- src/Datiss.Budget.Services/Excel/Models/IncomeCurrentWHImportModel.cs
+++ src/Datiss.Budget.Services/Excel/Models/IncomeCurrentWHImportModel.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Datiss.Budget.Services.Excel.Models
 {
-    public class IncomeCurrentImportModel
+    public class IncomeCurrentWHImportModel
     {
         [Column(0, MappingDirections.Both, Letter = "A")]
         public int YearId { get; set; }
