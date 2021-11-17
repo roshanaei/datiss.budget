a3e682e8ebc5d3b3c7b5d3d0acef53146614c98c
Erfan <erfan.eftekharizade@gmail.com>
2021-11-17 12:46:32 +0330

    Complate DTO/IncomeCurrentWNH

---
+++ src/Datiss.Budget.Services/Models/IncomeCurrentWNHDTO.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Models
+{
+    public class CreateIncomeCurrentWNHDTO
+    {
+        public int YearId { get; set; }
+        public int OrganizationId { get; set; }
+        public int UserTypeId { get; set; }
+        public string UserTypeTitle { get; set; }
+        public int UsageLayerId { get; set; }
+        public int NumberUser { get; set; }
+        public int UnitUser { get; set; }
+        public decimal AvgConsumeUser { get; set; }
+        public int ConsumptionUser { get; set; }
+        public decimal Capacity { get; set; }
+        public int Cost { get; set; }
+        public int Income { get; set; }
+        public int ExcessIncome { get; set; }
+        public int SeasonalIncome { get; set; }
+        public int Note3Price { get; set; }
+        public int Note3Income { get; set; }
+        public int SubscriptionIncome { get; set; }
+        public int TotalIncome { get; set; }
+        public int Diff_ConsWsVolume { get; set; }
+        public int Note2Income { get; set; }
+        public int WasteVolume { get; set; }
+        public int Note7Price { get; set; }
+        public int Note7Income { get; set; }
+    }
+    public class UpdateIncomeCurrentWNHDTO : CreateIncomeCurrentWNHDTO
+    {
+        public int Id { get; set; }
+    }
+    public class IncomeCurrentWNHDTO
+    {
+        public int Id { get; set; }
+        public int YearId { get; set; }
+        public int Year { get; set; }
+        public int OrganizationId { get; set; }
+        public string OrganizationDisplay { get; set; }
+        public int UserTypeId { get; set; }
+        public string UserTypeDisplay { get; set; }
+        public int UsageLayerId { get; set; }
+        public string UsageLayerDisplay { get; set; }
+        public int NumberUser { get; set; }
+        public int UnitUser { get; set; }
+        public decimal AvgConsumeUser { get; set; }
+        public int ConsumptionUser { get; set; }
+        public decimal Capacity { get; set; }
+        public int Cost { get; set; }
+        public int Income { get; set; }
+        public int ExcessIncome { get; set; }
+        public int SeasonalIncome { get; set; }
+        public int Note3Price { get; set; }
+        public int Note3Income { get; set; }
+        public int SubscriptionIncome { get; set; }
+        public int TotalIncome { get; set; }
+        public int Diff_ConsWsVolume { get; set; }
+        public int Note2Income { get; set; }
+        public int WasteVolume { get; set; }
+        public int Note7Price { get; set; }
+        public int Note7Income { get; set; }
+    }
+}
