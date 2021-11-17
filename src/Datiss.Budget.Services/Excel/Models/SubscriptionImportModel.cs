71e5a62ec5ec412c19998fb09777569efef363f3
Erfan <erfan.eftekharizade@gmail.com>
2021-11-17 10:27:26 +0330

    Complate ExelModel/Subscription

---
+++ src/Datiss.Budget.Services/Excel/Models/SubscriptionImportModel.cs
@@ -0,0 +1,24 @@
+﻿using Ganss.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Excel.Models
+{
+    public class SubscriptionImportModel
+    {
+        [Column(0, MappingDirections.Both, Letter = "A")]
+        public int YearId { get; set; }
+
+        [Column(1, MappingDirections.Both, Letter = "B")]
+        public int UserTypeId { get; set; }
+
+        [Column(2, MappingDirections.Both, Letter = "C")]
+        public int SubW { get; set; }
+
+        [Column(3, MappingDirections.Both, Letter = "D")]
+        public int SubWs { get; set; }
+    }
+}
