952c8c77380ece1b2329d1347b66530eb1b4136b
Erfan <erfan.eftekharizade@gmail.com>
2021-11-17 10:31:20 +0330

    Complate Interface/Subscription

---
+++ src/Datiss.Budget.Services/Contracts/ISubscriptionService.cs
@@ -0,0 +1,38 @@
+﻿using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Services.Infrastructure;
+using Datiss.Budget.Services.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Contracts
+{
+    public interface ISubscriptionService
+    {
+        Task<Subscription> GetByIdAsync(int id);
+
+        //Task<ValidationResult> AddAsync(CreateSubscriptionDTO model);
+
+        //Task<ValidationResult> UpdateAsync(UpdateSubscriptionDTO model);
+
+        Task HardDeleteAsync(int Id);
+
+        Task HardDeleteAsync(int yearId, int organizationId);
+
+        //Task<int> CalculationAsync(int yearId, int organizationId);
+
+        Task<PagedResult<SubscriptionDTO>> GetListAsync(SubscriptionFilterDTO filter);
+
+        Task CopyAsync(int sourceYearId, int sourceOrgId, int destYearId);
+
+        Task<Stream> ExportExcelAsync(SubscriptionFilterDTO filter);
+
+        Task<IEnumerable<SubscriptionDTO>> GetExportItemsAsync(SubscriptionFilterDTO filter);
+
+        Task ImportExcelAsync(IFormFile fileInfo);
+    }
+}
