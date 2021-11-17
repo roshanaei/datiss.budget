2ce779c5955c09abbab5d8b1682f23f1d57a7c34
Your Name <you@example.com>
2021-11-17 16:38:25 +0330

    Add                 .WithMany(x => x.IncomeCurrentWsNH)

---
+++ src/Datiss.Budget.DataLayer/Configurations/IncomeCurrentWsNHConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Datiss.Budget.Entities;
+using Datiss.Budget.Enum;
+using Datiss.Budget.Entities.DWH;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Datiss.Budget.DataLayer.Mappings
+{
+    public class IncomeCurrentWsNHConfiguration : IEntityTypeConfiguration<IncomeCurrentWsNH>
+    {
+        public void Configure(EntityTypeBuilder<IncomeCurrentWsNH> builder)
+        {
+            builder.ToTable("IncomeCurrentWsNH").HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).HasColumnName("IncomeCurrentWsNHId");
+
+            builder.Property(x => x.AvgConsumeUser).HasColumnType("decimal(18,6)");
+
+            builder.Property(x => x.Capacity).HasColumnType("decimal(18,6)");
+
+
+            builder.HasOne(x => x.FinanceYear)
+                    .WithMany(x => x.IncomeCurrentWsNH)
+                    .HasForeignKey(x => x.YearId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.Organization)
+                    .WithMany(x => x.IncomeCurrentWsNH)
+                    .HasForeignKey(x => x.OrganizationId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.UserType)
+                .WithMany(x => x.IncomeCurrentWsNH)
+                .HasForeignKey(x => x.UserTypeId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+}
