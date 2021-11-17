d72dba51a7fbfa22a8e8d8091ef1fd9e77a45f3d
Erfan <erfan.eftekharizade@gmail.com>
2021-11-17 13:11:14 +0330

    Complate ModelExel/IncomeCurrentWNH

---
+++ src/Datiss.Budget.Services/Excel/Models/IncomeCurrentWNHImportModel.cs
@@ -0,0 +1,55 @@
+﻿using Ganss.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Excel.Models
+{
+    class IncomeCurrentWNHImportModel
+    {
+        [Column(0, MappingDirections.Both, Letter = "A")]
+        public int YearId { get; set; }
+        [Column(1, MappingDirections.Both, Letter = "B")]
+        public int OrganizationId { get; set; }
+        [Column(2, MappingDirections.Both, Letter = "C")]
+        public int UserTypeId { get; set; }
+        [Column(3, MappingDirections.Both, Letter = "D")]
+        public int NumberUser { get; set; }
+        [Column(4, MappingDirections.Both, Letter = "E")]
+        public int UnitUser { get; set; }
+        [Column(5, MappingDirections.Both, Letter = "F")]
+        public decimal AvgConsumeUser { get; set; }
+        [Column(6, MappingDirections.Both, Letter = "G")]
+        public int ConsumptionUser { get; set; }
+        [Column(7, MappingDirections.Both, Letter = "H")]
+        public decimal Capacity { get; set; }
+        [Column(8, MappingDirections.Both, Letter = "I")]
+        public int Cost { get; set; }
+        [Column(9, MappingDirections.Both, Letter = "J")]
+        public int Income { get; set; }
+        [Column(10, MappingDirections.Both, Letter = "K")]
+        public int ExcessIncome { get; set; }
+        [Column(11, MappingDirections.Both, Letter = "L")]
+        public int SeasonalIncome { get; set; }
+        [Column(12, MappingDirections.Both, Letter = "M")]
+        public int Note3Price { get; set; }
+        [Column(13, MappingDirections.Both, Letter = "N")]
+        public int Note3Income { get; set; }
+        [Column(14, MappingDirections.Both, Letter = "O")]
+        public int SubscriptionIncome { get; set; }
+        [Column(15, MappingDirections.Both, Letter = "P")]
+        public int TotalIncome { get; set; }
+        [Column(16, MappingDirections.Both, Letter = "Q")]
+        public int Diff_ConsWsVolume { get; set; }
+        [Column(17, MappingDirections.Both, Letter = "R")]
+        public int Note2Income { get; set; }
+        [Column(18, MappingDirections.Both, Letter = "S")]
+        public int WasteVolume { get; set; }
+        [Column(19, MappingDirections.Both, Letter = "T")]
+        public int Note7Price { get; set; }
+        [Column(20, MappingDirections.Both, Letter = "U")]
+        public int Note7Income { get; set; }
+    }
+}
