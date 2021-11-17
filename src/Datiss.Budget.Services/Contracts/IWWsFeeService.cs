e689b2d10ef73638a0f960f7fb59d74db4221033
Erfan <erfan.eftekharizade@gmail.com>
2021-11-17 09:31:03 +0330

    Complate Interface/WWsFee

---
+++ src/Datiss.Budget.Services/Contracts/IWWsFeeService.cs
@@ -0,0 +1,37 @@
+﻿using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Services.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Contracts
+{
+    public interface IWWsFeeService
+    { 
+        Task<WWsFee> GetByIdAsync(int id);
+
+        //Task<ValidationResult> AddAsync(CreateWWsFeeDTO model);
+
+        //Task<ValidationResult> UpdateAsync(UpdateWWsFeeDTO model);
+
+        Task HardDeleteAsync(int Id);
+
+        Task HardDeleteAsync(int yearId, int organizationId);
+
+        //Task<int> CalculationAsync(int yearId, int organizationId);
+
+        Task<PagedResult<WWsFeeDTO>> GetListAsync(WWsFeeFilterDTO filter);
+
+        Task CopyAsync(int sourceYearId, int sourceOrgId, int destYearId);
+
+        Task<Stream> ExportExcelAsync(WWsFeeFilterDTO filter);
+
+        Task<IEnumerable<WWsFeeDTO>> GetExportItemsAsync(WWsFeeFilterDTO filter);
+
+        Task ImportExcelAsync(IFormFile fileInfo);
+    }
+}
