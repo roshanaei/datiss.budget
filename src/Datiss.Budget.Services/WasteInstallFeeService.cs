0c7bd3851a965ce5a06df305cbf7d4bff4920aaf
Erfan <erfan.eftekharizade@gmail.com>
2021-11-20 13:05:42 +0330

    Resolve HardDelete Problem

--- src/Datiss.Budget.Services/WasteInstallFeeService.cs
+++ src/Datiss.Budget.Services/WasteInstallFeeService.cs
@@ -112,10 +112,13 @@ namespace Datiss.Budget.Services
         }
         public async Task HardDeleteAsync(int yearId, int organizationId)
         {
-            var items = await _dbSet.Where(_ => _.YearId == yearId)
-                                    .Where(_ => _.OrganizationId == organizationId)
-                                    .ToListAsync();
 
+            var items = await _dbSet.Where(_ => _.YearId == yearId)
+                                    .Where(_ => _.OrganizationId == organizationId ||
+                                                _.Organization.ParentId==organizationId ||
+                                                _.Organization.Parent.ParentId == organizationId ||
+                                                _.Organization.Parent.Parent.ParentId == organizationId)
+                                    .ToListAsync();
             _dbSet.RemoveRange(items);
 
             await _uow.SaveChangesAsync();
