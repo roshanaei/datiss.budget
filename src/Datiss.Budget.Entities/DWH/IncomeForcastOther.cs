91cd473518a1f26ae4e54e59e39544f96cfdf27c
Your Name <you@example.com>
2021-11-21 11:49:48 +0330

    Edit IncomeForcastOther

--- src/Datiss.Budget.Entities/DWH/IncomeForcastOther.cs
+++ src/Datiss.Budget.Entities/DWH/IncomeForcastOther.cs
@@ -18,6 +18,8 @@ namespace Datiss.Budget.Entities.DWH
 
         public int OIFTypeId { get; set; }
 
+        public int OIFCount { get; set; }
+
         public int OIFPrice { get; set; }
 
         #endregion
