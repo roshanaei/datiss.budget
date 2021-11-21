b7a37a072a41d8cabe7836992568ecf2f18e4a99
iman nemati <imun22@gmmail.com>
2021-11-21 18:44:26 +0330

    add enumdisplayprovider

---
+++ src/Datiss.Budget.ViewModels/Extensions/EnumDisplayProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datiss.Budget.Enum;
+using Datiss.Budget.Resources;
+
+namespace Datiss.Budget.ViewModels
+{
+
+    public static class EnumDisplayProvider {
+
+        public static string ToDisplay(this EntityStatus status)
+            => status switch {
+                EntityStatus.Deleted => EnumText.EntityStatus_Deleted,
+                EntityStatus.Disbaled => EnumText.EntityStatus_Disabled,
+                EntityStatus.Enabled => EnumText.EntityStatus_Enabled,
+                _=> EnumText.Unknown
+            };
+
+
+    }
+}
