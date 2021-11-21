ea9da9eb2c6611464b11aca9159ce1646f7c72cc
Erfan <erfan.eftekharizade@gmail.com>
2021-11-21 10:40:52 +0330

    Complete DTO

---
+++ src/Datiss.Budget.Services/Models/PerformanceEvalutionDTO.cs
@@ -0,0 +1,43 @@
+﻿using Datiss.Budget.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Models
+{
+    public class CreatePerformanceEvaluationDTO
+    {
+        public int YearId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public EntityStatus Status { get; set; }
+
+        public int TableFieldId { get; set; }
+
+        public decimal Target { get; set; }
+
+        public decimal Operation { get; set; }
+    }
+
+    public class UpdatePerformanceEvaluationDTO : CreatePerformanceEvaluationDTO
+    {
+        public int Id { get; set; }
+
+    }
+
+    public class PerformanceEvaluationDTO
+    {
+        public int Id { get; set; }
+        public int YearId { get; set; }
+        public int Year { get; set; }
+        public int OrganizationId { get; set; }
+        public string OrganizationDisplay { get; set; }
+        public EntityStatus Status { get; set; }
+        public int TableFieldId { get; set; }
+        public decimal Target { get; set; }
+        public decimal Operation { get; set; }
+    }
+}
