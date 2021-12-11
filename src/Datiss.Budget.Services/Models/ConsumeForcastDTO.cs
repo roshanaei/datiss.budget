5d308836ec986c6311a221f39d99cad43c20a93d
Your Name <you@example.com>
2021-12-11 16:00:02 +0330

    ConsumeForcastService

---
+++ src/Datiss.Budget.Services/Models/ConsumeForcastDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Datiss.Budget.Services.Models
+{
+    public class CreateConsumeForcastDTO
+    {
+
+    }
+
+    public class UpdateConsumeForcastDTO : CreateConsumeForcastDTO
+    {
+
+    }
+
+    public class ConsumeForcastDTO
+    {
+
+    }
+}
