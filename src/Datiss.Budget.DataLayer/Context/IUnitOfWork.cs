088189a8668a527ee8a0ea9c290e7236325408a6
iman nemati <imun22@gmmail.com>
2021-12-12 17:11:54 +0330

    Add ExecuteSqlRawCommandAsync to DbContext interface to support Sp execute

--- src/Datiss.Budget.DataLayer/Context/IUnitOfWork.cs
+++ src/Datiss.Budget.DataLayer/Context/IUnitOfWork.cs
@@ -25,6 +25,7 @@ namespace Datiss.Budget.DataLayer.Context
         void ExecuteSqlInterpolatedCommand(FormattableString query);
         void ExecuteSqlRawCommand(string query, params object[] parameters);
 
+        Task ExecuteSqlRawCommandAsync(string query, params object[] parameters);
         void BeginTransaction();
         void RollbackTransaction();
         void CommitTransaction();
