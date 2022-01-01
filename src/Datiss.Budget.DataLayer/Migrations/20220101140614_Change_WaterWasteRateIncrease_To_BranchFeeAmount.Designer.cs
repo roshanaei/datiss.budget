5ef0520ef270684c26f784cd069b23938fb8ece9
Your Name <you@example.com>
2022-01-01 17:37:45 +0330

    Add Migration For BranchFeeAmount

--- src/Datiss.Budget.DataLayer/Migrations/20220101140614_Change_WaterWasteRateIncrease_To_BranchFeeAmount.Designer.cs
+++ src/Datiss.Budget.DataLayer/Migrations/20220101140614_Change_WaterWasteRateIncrease_To_BranchFeeAmount.Designer.cs
@@ -4,14 +4,16 @@ using Datiss.Budget.DataLayer.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Datiss.Budget.DataLayer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220101140614_Change_WaterWasteRateIncrease_To_BranchFeeAmount")]
+    partial class Change_WaterWasteRateIncrease_To_BranchFeeAmount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
