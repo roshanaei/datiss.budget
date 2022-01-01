6fe85037164654df558d61d9e65dfe4b2272e58a
Your Name <you@example.com>
2022-01-01 18:54:23 +0330

    Add WNHCo Migration

--- src/Datiss.Budget.DataLayer/Migrations/20220101152245_Add WNHCo.Designer.cs
+++ src/Datiss.Budget.DataLayer/Migrations/20220101152245_Add WNHCo.Designer.cs
@@ -4,14 +4,16 @@ using Datiss.Budget.DataLayer.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Datiss.Budget.DataLayer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220101152245_Add WNHCo")]
+    partial class AddWNHCo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
