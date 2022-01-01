2a97828dc7b2c04b30cd6a93c8b480a1464d9a35
Your Name <you@example.com>
2022-01-01 18:51:38 +0330

    Add WNHCo Configuration

---
+++ src/Datiss.Budget.DataLayer/Configurations/WNHCoConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Enum;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Datiss.Budget.DataLayer.Mappings
+{
+    public class WNHCoConfiguration : IEntityTypeConfiguration<WNHCo>
+    {
+        public void Configure(EntityTypeBuilder<WNHCo> builder)
+        {
+            builder.ToTable("WNHCo");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                    .HasColumnName("WNHCoId");
+
+            builder.HasOne(x => x.Organization)
+                    .WithMany(x => x.WNHCo)
+                    .HasForeignKey(x => x.OrganizationId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.FinanceYear)
+                    .WithMany(x => x.WNHCo)
+                    .HasForeignKey(x => x.YearId)
+                    .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
