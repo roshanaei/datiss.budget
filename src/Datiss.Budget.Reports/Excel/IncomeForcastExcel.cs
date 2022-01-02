e6da062351227b6ff9363a507aa3e3574a224fab
Erfan <erfan.eftekharizade@gmail.com>
2022-01-02 11:17:56 +0330

    Complete Controller

---
+++ src/Datiss.Budget.Reports/Excel/IncomeForcastExcel.cs
@@ -0,0 +1,107 @@
+﻿using ClosedXML.Excel;
+using Datiss.Budget.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Reports.Excel
+{
+    public static class IncomeForcastExcel
+    {
+        private const string _sheetName = "IncomeForcast";
+
+        public static XLWorkbook ExportExcel(this IEnumerable<IncomeForcastDTO> items)
+        {
+            if (items == null || !items.Any())
+                return null;
+
+            var workbook = new XLWorkbook();
+            var sheet = workbook.Worksheets.Add(_sheetName);
+
+            sheet.RightToLeft = true;
+            sheet.Cell(1, 1).Value = "سال";
+            sheet.Cell(1, 2).Value = "سازمان";
+            sheet.Cell(1, 3).Value = "کاربری";
+            sheet.Cell(1, 4).Value = "تعداد انشعاب";
+            sheet.Cell(1, 5).Value = "آحاد انشعاب";
+            sheet.Cell(1, 6).Value = "درآمد هزینه لوله گذاری آب";
+            sheet.Cell(1, 7).Value = "درآمد حق انشعاب آب";
+            sheet.Cell(1, 8).Value = "درآمد تبصره 2 ماده واحده آب";
+            sheet.Cell(1, 9).Value = "درآمد تبصره 3 ماده واحده آب";
+            sheet.Cell(1, 10).Value = "درآمد ماده 11 آب";
+
+            var totalCount = items.Count();
+            int row = 2;
+            for (int i = 0; i < totalCount; i++)
+            {
+                var item = items.ElementAt(i);
+                sheet.Cell(row, 1).Value = item.Year.ToString();
+                sheet.Cell(row, 2).Value = item.OrganizationDisplay;
+                sheet.Cell(row, 3).Value = item.UserTypeDisplay;
+                sheet.Cell(row, 4).Value = item.NumberUser;
+                sheet.Cell(row, 5).Value = item.UnitUser;
+                sheet.Cell(row, 6).Value = item.WaterInstllIncome;
+                sheet.Cell(row, 7).Value = item.WaterBranchIncome;
+                sheet.Cell(row, 8).Value = item.WaterNote2Income;
+                sheet.Cell(row, 9).Value = item.WaterNote3Income;
+                sheet.Cell(row, 10).Value = item.WNote11Income;
+                sheet.Cell(row, 11).DataType = XLDataType.Number;
+                row++;
+            }
+            var range = sheet.Range(1, 1, row - 1, 4);
+            var table = range.CreateTable($"{_sheetName}_Table");
+            table.Theme = XLTableTheme.TableStyleMedium16;
+            sheet.Columns().AdjustToContents();
+
+            return workbook;
+        }
+
+        public static XLWorkbook GetImportTemplate(this IEnumerable<IncomeForcastDTO> items, int year)
+        {
+            if (items == null || !items.Any())
+                return null;
+
+            var workbook = new XLWorkbook();
+            var sheet = workbook.Worksheets.Add(_sheetName);
+
+            sheet.RightToLeft = true;
+            sheet.Cell(1, 1).Value = "ورود اطلاعات برای سال مالی : " + year;
+            sheet.Range(1, 1, 1, 5).Merge();
+
+            sheet.Cell(2, 1).Value = "عنوان سازمان";
+            sheet.Cell(2, 2).Value = "کد سازمان";
+            sheet.Cell(2, 3).Value = "عنوان کاربری";
+            sheet.Cell(2, 4).Value = "کد کاربری";
+            sheet.Cell(1, 5).Value = "تعداد انشعاب";
+            sheet.Cell(1, 6).Value = "آحاد انشعاب";
+            sheet.Cell(1, 7).Value = "درآمد هزینه لوله گذاری آب";
+            sheet.Cell(1, 8).Value = "درآمد حق انشعاب آب";
+            sheet.Cell(1, 9).Value = "درآمد تبصره 2 ماده واحده آب";
+            sheet.Cell(1, 10).Value = "درآمد تبصره 3 ماده واحده آب";
+            sheet.Cell(1, 11).Value = "درآمد ماده 11 آب";
+
+            var totalCount = items.Count();
+            int row = 3;
+            for (int i = 0; i < totalCount; i++)
+            {
+                var item = items.ElementAt(i);
+                sheet.Cell(row, 1).Value = item.OrganizationDisplay;
+                sheet.Cell(row, 2).Value = item.OrganizationId;
+                sheet.Cell(row, 3).Value = item.UserTypeDisplay;
+                sheet.Cell(row, 4).Value = item.UserTypeId;
+                row++; //for keeping index in table records
+            }
+
+            var range = sheet.Range(2, 1, row - 1, 5);
+            range.Column(5).Style.NumberFormat.Format = "#,##0";
+            range.Column(6).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            var table = range.CreateTable($"{_sheetName}_Table");
+            table.Theme = XLTableTheme.TableStyleMedium16;
+            sheet.Columns().AdjustToContents();
+
+            return workbook;
+        }
+    }
+}
