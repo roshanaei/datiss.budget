7edb6c28f8a139f00c5335c2eb5f83895ca8b559
Your Name <you@example.com>
2022-01-02 17:21:01 +0330

    Add Migtration SalesSplit

--- src/Datiss.Budget.DataLayer/Migrations/20220102134008_ADDWInstalationToSalesSplitW&Ws.Designer.cs
+++ src/Datiss.Budget.DataLayer/Migrations/20220102134008_ADDWInstalationToSalesSplitW&Ws.Designer.cs
@@ -4,14 +4,16 @@ using Datiss.Budget.DataLayer.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Datiss.Budget.DataLayer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220102134008_ADDWInstalationToSalesSplitW&Ws")]
+    partial class ADDWInstalationToSalesSplitWWs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
