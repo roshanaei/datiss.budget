c2dcd367ed5a48c5575c271d295ee14a72386e25
Your Name <you@example.com>
2022-01-05 15:32:37 +0330

    Add ConsumeForcastWs DTO

---
+++ src/Datiss.Budget.Services/Models/ConsumeForcastWs.cs
@@ -0,0 +1,54 @@
+﻿namespace Datiss.Budget.Services.Models
+{
+    public class CreateConsumeForcastWsDTO
+    {
+        public int YearId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public int UserTypeId { get; set; }
+
+        public int UsageLayerId { get; set; }
+
+        public decimal CountUser { get; set; }
+
+        public decimal UnitUser { get; set; }
+
+        public decimal ConsumeUser { get; set; }
+
+        public decimal AvgConsumeUser { get; set; }
+    }
+
+    public class UpdateConsumeForcastWsDTO : CreateConsumeForcastWsDTO
+    {
+        public int Id { get; set; }
+
+        public decimal ConsumeUserForcast { get; set; }
+    }
+
+    public class ConsumeForcastWsDTO
+    {
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+        public int Year { get; set; }
+
+        public int OrganizationId { get; set; }
+        public string OrganizationDisplay { get; set; }
+
+        public int UserTypeId { get; set; }
+        public string UserTypeDisplay { get; set; }
+
+        public int UsageLayerId { get; set; }
+
+        public decimal CountUser { get; set; }
+
+        public decimal UnitUser { get; set; }
+
+        public decimal ConsumeUser { get; set; }
+
+        public decimal AvgConsumeUser { get; set; }
+
+        public decimal ConsumeUserForcast { get; set; }
+    }
+}
