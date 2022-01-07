6954dac013c45e78e4c9f6d55b6e3ab8bc4d2e90
iman nemati <imun22@gmmail.com>
2022-01-08 00:44:30 +0330

    Add DateService

---
+++ src/Datiss.Budget.Services/System/DataEntryTimeLimitService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Datiss.Budget.Entities;
+using Datiss.Budget.DataLayer.Context;
+using Datiss.Budget.Services.Contracts;
+using Datiss.Budget.Common.GuardToolkit;
+
+namespace Datiss.Budget.Services.System
+{
+    public class DataEntryTimeLimitService
+    {
+
+        private readonly IUnitOfWork _uow;
+        private readonly DbSet<DataEntryTimeLimit> _dbSet;
+
+        public DataEntryTimeLimitService(IUnitOfWork uow) 
+        {
+            _uow = uow ?? throw new ArgumentNullException(nameof(uow));
+            _dbSet = _uow.Set<DataEntryTimeLimit>();
+        }
+
+
+    }
+}
