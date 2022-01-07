3becfdc09b5563b06cd530c9fe58a8f44d621b68
iman nemati <imun22@gmmail.com>
2022-01-08 00:05:10 +0330

    add dataentrytime entity

---
+++ src/Datiss.Budget.Entities/System/DataEntryTimeLimit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Datiss.Budget.Entities.AuditableEntity;
+
+namespace Datiss.Budget.Entities 
+{
+
+    public class DataEntryTimeLimit : IAuditableEntity 
+    {
+
+        public DataEntryTimeLimit() 
+        { 
+        
+        }
+
+        #region Properties
+
+        public int Id { get; set; }
+
+        public int? OrganizationId { get; set; }
+
+        public int? YearId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime FinishDate { get; set; }
+
+        public string Description { get; set; }
+
+        #endregion
+
+        #region Navigations
+
+        public Organization Organization { get; set; }
+
+        public FinanceYear Year { get; set; }
+
+        #endregion
+    }
+}
