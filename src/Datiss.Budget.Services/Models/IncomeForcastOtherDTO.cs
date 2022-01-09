1f23de52de6e167c91e143c170f1d70bd96680ab
Erfan <erfan.eftekharizade@gmail.com>
2022-01-09 12:15:34 +0330

    Complete Interface & DTO

---
+++ src/Datiss.Budget.Services/Models/IncomeForcastOtherDTO.cs
@@ -0,0 +1,40 @@
+﻿using Datiss.Budget.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Models
+{
+    public class CreateIncomeForcastOtherDTO
+    {
+        public int YearId { get; set; }
+        public int OrganizationId { get; set; }
+        public int OIFTypeId{ get; set; }
+        public string OIFTypeTitle { get; set; }
+        public ActivityType ActivityId { get; set; }
+        public int OIFCount { get; set; }
+        public int OIFPrice { get; set; }
+    }
+
+    public class UpdateIncomeForcastOtherDTO : CreateIncomeForcastOtherDTO
+    {
+        public int Id { get; set; }
+
+    }
+
+    public class IncomeForcastOtherDTO
+    {
+        public int Id { get; set; }
+        public int YearId { get; set; }
+        public int Year { get; set; }
+        public int OrganizationId { get; set; }
+        public string OrganizationDisplay { get; set; }
+        public int OIFTypeId{ get; set; }
+        public string OIFTypeDisplay { get; set; }
+        public ActivityType ActivityId { get; set; }
+        public int OIFCount { get; set; }
+        public int OIFPrice { get; set; }
+    }
+}
