e26400e06505b36b6460d024b57a6a1e46ed9bed
Erfan <58788132+ErfanEftekhari@users.noreply.github.com>
2022-01-10 12:13:24 +0330

    Erfan/income forcast other (#97)

---
+++ src/Datiss.Budget.Services/Contracts/IIncomeForcastOtherService.cs
@@ -0,0 +1,38 @@
+﻿using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Services.Infrastructure;
+using Datiss.Budget.Services.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Contracts
+{
+    public interface IIncomeForcastOtherService
+    {
+        Task<IncomeForcastOther> GetByIdAsync(int id);
+
+        Task<ValidationResult<IncomeForcastOtherDTO>> CreateAsync(CreateIncomeForcastOtherDTO model);
+
+        Task<ValidationResult<IncomeForcastOtherDTO>> UpdateAsync(UpdateIncomeForcastOtherDTO model);
+
+        Task HardDeleteAsync(int Id);
+
+        Task<OrganizationDeleteDataResult> HardDeleteAsync(int yearId, int organizationId);
+
+        Task<IEnumerable<CalculationItemData>> CalculationAsync(int yearId, int organizationId);
+
+        Task<PagedResult<IncomeForcastOtherDTO>> GetListAsync(IncomeForcastOtherFilterDTO filter);
+
+        Task CopyAsync(int sourceYearId, int sourceOrgId, int destYearId);
+
+        Task<Stream> ExportExcelAsync(IncomeForcastOtherFilterDTO filter);
+
+        Task<IEnumerable<IncomeForcastOtherDTO>> GetExportItemsAsync(int yearId, int organizationId);
+
+        Task<ImportResult> ImportExcelAsync(IFormFile fileInfo, int yearId, bool continueIfAnyOrgMissing = false);
+    }
+}
---
+++ src/Datiss.Budget.Services/Excel/Models/IncomeForcastOtherImportModel.cs
@@ -0,0 +1,37 @@
+﻿using Datiss.Budget.Enum;
+using Ganss.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Excel.Models
+{
+    public class IncomeForcastOtherImportModel
+    {
+        [Column(MappingDirections.Both, Letter = "A")]
+        public string OrganizationDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "B")]
+        public int OrganizationId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public string OIFTypeDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "D")]
+        public int OIFTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "E")]
+        public string ActivityDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "F")]
+        public int ActivityId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "G")]
+        public int OIFCount { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "H")]
+        public int OIFPrice { get; set; }
+    }
+}
