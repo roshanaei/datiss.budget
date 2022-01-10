6bf9316aaad50ea286b82ecbd00cd2bf9df5b061
Your Name <you@example.com>
2022-01-10 18:47:44 +0330

    Add Get Excell Template

--- src/Datiss.Budget.Reports/Excel/ConsumeForcastWsExcel.cs
+++ src/Datiss.Budget.Reports/Excel/ConsumeForcastWsExcel.cs
@@ -53,5 +53,50 @@ namespace Datiss.Budget.Reports.Excel
 
             return workbook;
         }
+
+        public static XLWorkbook GetImportTemplate(this IEnumerable<ConsumeForcastWsDTO> items, int year)
+        {
+            if (items == null || !items.Any())
+                return null;
+
+            var workbook = new XLWorkbook();
+            var sheet = workbook.Worksheets.Add(_sheetName);
+
+            sheet.RightToLeft = true;
+            sheet.Cell(1, 1).Value = "ورود اطلاعات برای سال مالی : " + year;
+            sheet.Range(1, 1, 1, 5).Merge();
+
+            sheet.Cell(2, 1).Value = "عنوان سازمان";
+            sheet.Cell(2, 2).Value = "کد سازمان";
+            sheet.Cell(2, 3).Value = "قطر انشعاب";
+            sheet.Cell(2, 4).Value = "کد قطر انشعاب";
+            sheet.Cell(2, 5).Value = "تعرفه نصب انشعاب";
+
+            var totalCount = items.Count();
+            int row = 3;
+            for (int i = 0; i < totalCount; i++)
+            {
+                var item = items.ElementAt(i);
+                sheet.Cell(row, 1).Value = item.OrganizationDisplay;
+                sheet.Cell(row, 2).Value = item.OrganizationId;
+                sheet.Cell(row, 3).Value = item.UserTypeTitle;
+                sheet.Cell(row, 4).Value = item.UserTypeId;
+                sheet.Cell(row, 5).Value = item.UsageLayerTitle;
+                sheet.Cell(row, 6).Value = item.UsageLayerId;
+                row++; //for keeping index in table records
+            }
+
+            var range = sheet.Range(2, 1, row - 1, 5);
+            range.Column(3).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            range.Column(4).Style.NumberFormat.Format = "#,##0";
+            range.Column(5).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            range.Column(6).Style.NumberFormat.Format = "#,##0";
+            var table = range.CreateTable($"{_sheetName}_Table");
+            table.Theme = XLTableTheme.TableStyleMedium16;
+            sheet.Columns().AdjustToContents();
+
+            return workbook;
+        }
+
     }
 }
