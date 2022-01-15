bf1c1671567da39426fe65270e5677708a5697ce
Erfan <58788132+ErfanEftekhari@users.noreply.github.com>
2022-01-15 18:07:14 +0330

    Organization (#99)

--- src/Datiss.Budget.Services/Contracts/IOrganizationService.cs
+++ src/Datiss.Budget.Services/Contracts/IOrganizationService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Datiss.Budget.Entities;
+using Datiss.Budget.Enum;
 using Datiss.Budget.Services.Infrastructure;
 using Datiss.Budget.Services.Models;
 
@@ -8,17 +9,21 @@ namespace Datiss.Budget.Services.Contracts
 {
     public interface IOrganizationService
     {
+        Task<Organization> GetByIdAsync(int id);
+
         Task<ValidationResult> CreateAsync(CreateOrganizationDTO model);
 
         Task<ValidationResult> UpdateAsync(UpdateOrganizationDTO model);
 
-        Task<ValidationResult> SoftDeleteAsync(int id);
+        Task SoftDeleteAsync(int id);
 
         Task<IEnumerable<DropDownItem>> GetParentsAsync();
 
         Task<IEnumerable<Organization>> GetWithChildrenAsync(int? organizationId, bool input = false);
 
         Task<IEnumerable<DropDownItem>> GetDropDownDataAsync(bool input = false);
+
+        Task<IEnumerable<DropDownItem>> GetDropDownTypeOrgDataAsync(OrganizationType type);
 
         Task<bool> IsDescendentOfAsync(int parentId, int targetOrganizationId);
 
--- src/Datiss.Budget.ViewModels/Extensions/EnumSelectListProvider.cs
+++ src/Datiss.Budget.ViewModels/Extensions/EnumSelectListProvider.cs
@@ -22,13 +22,13 @@ namespace Datiss.Budget.Extensions
                         Text = EnumText.EntityStatus_Disabled,
                         Value = ((int)EntityStatus.Disbaled).ToString(),
                         Selected = status == EntityStatus.Disbaled
-                    },
-                    new SelectListItem
-                    {
-                        Text = EnumText.EntityStatus_Deleted,
-                        Value = ((int)EntityStatus.Deleted).ToString(),
-                        Selected = status == EntityStatus.Deleted
                     }
+                    //new SelectListItem
+                    //{
+                    //    Text = EnumText.EntityStatus_Deleted,
+                    //    Value = ((int)EntityStatus.Deleted).ToString(),
+                    //    Selected = status == EntityStatus.Deleted
+                    //}
                 };
         public static IEnumerable<SelectListItem> GetActivityTypeItems(ActivityType? activity = null)
             => new List<SelectListItem>
@@ -44,7 +44,29 @@ namespace Datiss.Budget.Extensions
                         Text = EnumText.ActivityType_Waste,
                         Value = ((int)ActivityType.Waste).ToString(),
                         Selected = activity == ActivityType.Waste
-                    },
+                    }
+                };
+        public static IEnumerable<SelectListItem> GetOrganizationTypeItem(OrganizationType? type = null)
+            => new List<SelectListItem>
+                {
+                    new SelectListItem
+                    {
+                        Text = EnumText.OrganizationType_County,
+                        Value = ((int)OrganizationType.County).ToString(),
+                        Selected = type == OrganizationType.County
+                    },                    
+                    new SelectListItem
+                    {
+                        Text = EnumText.OrganizationType_City,
+                        Value = ((int)OrganizationType.City).ToString(),
+                        Selected = type == OrganizationType.City
+                    },                    
+                    new SelectListItem
+                    {
+                        Text = EnumText.OrganizationType_Village,
+                        Value = ((int)OrganizationType.Village).ToString(),
+                        Selected = type == OrganizationType.Village
+                    }                    
                 };
     }
 }
