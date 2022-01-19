942c772a768acc68b3425190fb0b6e830d97cc63
Erfan <erfan.eftekharizade@gmail.com>
2022-01-19 12:36:02 +0330

    Complete DTO & Interface

---
+++ src/Datiss.Budget.Services/Contracts/ISalesSplitTotal.cs
@@ -0,0 +1,38 @@
+﻿using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Services.Infrastructure;
+using Datiss.Budget.Services.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Contracts
+{
+    public interface ISalesSplitTotal
+    {
+        Task<SalesSplitTotal> GetByIdAsync(int id);
+
+        Task<ValidationResult<SalesSplitTotalDTO>> CreateAsync(CreateSalesSplitTotalDTO model);
+
+        Task<ValidationResult<SalesSplitTotalDTO>> UpdateAsync(UpdateSalesSplitTotalDTO model);
+
+        Task HardDeleteAsync(int Id);
+
+        Task<OrganizationDeleteDataResult> HardDeleteAsync(int yearId, int organizationId);
+
+        Task<IEnumerable<CalculationItemData>> CalculationAsync(int yearId, int organizationId);
+
+        Task<PagedResult<SalesSplitTotalDTO>> GetListAsync(SalesSplitTotalFilterDTO filter);
+
+        Task CopyAsync(int sourceYearId, int sourceOrgId, int destYearId);
+
+        Task<Stream> ExportExcelAsync(SalesSplitTotalFilterDTO filter);
+
+        Task<IEnumerable<SalesSplitTotalDTO>> GetExportItemsAsync(int yearId, int organizationId);
+
+        Task<ImportResult> ImportExcelAsync(IFormFile fileInfo, int yearId, bool continueIfAnyOrgMissing = false);
+    }
+}
