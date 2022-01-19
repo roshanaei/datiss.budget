069b7617c36a1ffd9add5c923b0ae3e62fd7ca5d
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-19 10:41:40 +0330

    Create SubscriptionViewModel

---
+++ src/Datiss.Budget.ViewModels/SubscriptionViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.ViewModels
+{
+    public class SubscriptionViewModel
+    {
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+
+        public int Year { get; set; }
+
+        public int UserTypeId { get; set; }
+
+        public string UserTypeDisplay { get; set; }
+
+        public int SubW { get; set; }
+
+        public string SubWDisplay => SubW.ToString("N0");
+
+        public int SubWs { get; set; }
+
+        public string SubWsDisplay => SubWs.ToString("N0");
+
+    }
+}
