8337a2b817680223ed7d144d564b06084c8fe6ba
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-19 10:56:38 +0330

    Add Create SubscriptionViewModel

--- src/Datiss.Budget.ViewModels/SubscriptionViewModel.cs
+++ src/Datiss.Budget.ViewModels/SubscriptionViewModel.cs
@@ -1,11 +1,41 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Datiss.Budget.ViewModels
 {
+    public class CreateSubscriptionViewModel : BaseViewModel
+    {
+        public int YearId { get; set; }
+
+        public string YearDisplay { get; set; }
+
+        public int UserTypeId { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Range(0, int.MaxValue, ErrorMessage = "لطفاً آبونمان آب را بصورت صحیح وارد نمایید.")]
+        public int SubW { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Range(0, int.MaxValue, ErrorMessage = "لطفاً آبونمان فاضلاب را بصورت صحیح وارد نمایید.")]
+        public int SubWs { get; set; }
+
+        public IEnumerable<SelectListItem> UserTypeSource { get; set; }
+
+        public string UserTypeTitle { 
+            get {
+                if (UserTypeSource == null || !UserTypeSource.Any())
+                    return string.Empty;
+
+                return UserTypeSource.FirstOrDefault(x => x.Value.ToString() == UserTypeId.ToString()).Text;
+            } 
+        }
+    }
+
     public class SubscriptionViewModel
     {
         public int Id { get; set; }
@@ -25,6 +55,5 @@ namespace Datiss.Budget.ViewModels
         public int SubWs { get; set; }
 
         public string SubWsDisplay => SubWs.ToString("N0");
-
     }
 }
