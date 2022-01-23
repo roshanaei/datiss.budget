2798a59e5a4c198f9a74069cf799f465c231342c
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-23 11:07:33 +0330

    Add Create to ViewModel

--- src/Datiss.Budget.ViewModels/WWsFeeViewModel.cs
+++ src/Datiss.Budget.ViewModels/WWsFeeViewModel.cs
@@ -1,12 +1,67 @@
 ﻿using Datiss.Budget.Enum;
+using Datiss.Budget.Extensions;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Datiss.Budget.ViewModels
 {
+    public class CreateWWsFeeViewModel : BaseViewModel
+    {
+        public int YearId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public ActivityType ActivityType { get; set; }
+
+        public int UserTypeId { get; set; }
+
+        public int UsageLayerId { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Range(0, int.MaxValue, ErrorMessage = "پارامتر اول تعرفه بهای خدمات باید به صورت عددی وارد شود")]
+        public int P1Fee { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Range(0, int.MaxValue, ErrorMessage = "پارامتر دوم تعرفه بهای خدمات باید به صورت عددی وارد شود")]
+        public int P2Fee { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Range(0, int.MaxValue, ErrorMessage = "پارامتر اول خدمات تبصره 3 باید به صورت عددی وارد شود")]
+        public int P1Note3 { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Range(0, int.MaxValue, ErrorMessage = "پارامتر دوم خدمات تبصره 3 باید به صورت عددی وارد شود")]
+        public int P2Note3 { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Range(0, int.MaxValue, ErrorMessage = "پارامتر اول خدمات تبصره 7 باید به صورت عددی وارد شود")]
+        public int P1Note7 { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Range(0, int.MaxValue, ErrorMessage = "پارامتر اول خدمات تبصره 7 باید به صورت عددی وارد شود")]
+        public int P2Note7 { get; set; }
+
+        public IEnumerable<SelectListItem> UserTypeSource { get; set; }
+
+        public string UserTypeTitle
+        {
+            get
+            {
+                if (UserTypeSource == null || !UserTypeSource.Any())
+                    return string.Empty;
+                return UserTypeSource.FirstOrDefault(x => x.Value.ToString() == UserTypeId.ToString()).Text;
+            }
+        }
+
+        public IEnumerable<SelectListItem> ActivityTypeSource
+            => EnumSelectListProvider.GetActivityTypeItems(ActivityType);
+    }
+
     public class WWsFeeViewModel
     {
         public int Id { get; set; }
