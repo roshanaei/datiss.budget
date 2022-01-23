7c93e4f6fe825f6f0688f1ce9a51277aeafbbbc5
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-23 11:52:24 +0330

    Add GetExportTemplate in WWsFeeExcel

--- src/Datiss.Budget.Reports/Excel/WWsFeeExcel.cs
+++ src/Datiss.Budget.Reports/Excel/WWsFeeExcel.cs
@@ -75,5 +75,63 @@ namespace Datiss.Budget.Reports.Excel
             return workbook;
         }
 
+        public static XLWorkbook GetImportTemplate(this IEnumerable<WWsFeeDTO> items, int year)
+        {
+            if (items == null || !items.Any())
+                return null;
+
+            var workbook = new XLWorkbook();
+            var sheet = workbook.Worksheets.Add(_sheetName);
+
+            sheet.RightToLeft = true;
+            sheet.Cell(1, 1).Value = "ورود اطلاعات برای سال مالی : " + year;
+            sheet.Range(1, 1, 1, 14).Merge();
+
+            sheet.Cell(2, 1).Value = "عنوان سازمان";
+            sheet.Cell(2, 2).Value = "کد سازمان";
+            sheet.Cell(2, 3).Value = "عنوان حوزه فعالیت";
+            sheet.Cell(2, 4).Value = "کد حوزه فعالیت";
+            sheet.Cell(2, 5).Value = "عنوان کاربری";
+            sheet.Cell(2, 6).Value = "کد کاربری";
+            sheet.Cell(2, 7).Value = "عنوان طبقات مصرف مسکونی";
+            sheet.Cell(2, 8).Value = "کد طبقات مصرف مسکونی";
+            sheet.Cell(2, 9).Value = "پارامتر اول تعرفه بهای خدمات";
+            sheet.Cell(2, 10).Value = "پارامتر دوم تعرفه بهای خدمات";
+            sheet.Cell(2, 11).Value = "پارامتر اول خدمات تبصره 3";
+            sheet.Cell(2, 12).Value = "پارامتر دوم خدمات تبصره 3";
+            sheet.Cell(2, 13).Value = "پارامتر اول خدمات تبصره 7";
+            sheet.Cell(2, 14).Value = "پارامتر دوم خدمات تبصره 7";
+
+            var totalCount = items.Count();
+            int row = 3;
+            for (int i = 0; i < totalCount; i++)
+            {
+                var item = items.ElementAt(i);
+                sheet.Cell(row, 1).Value = item.OrganizationDisplay;
+                sheet.Cell(row, 2).Value = item.OrganizationId;
+                sheet.Cell(row, 3).Value = item.ActivityTypeDisplay;
+                sheet.Cell(row, 4).Value = (int)item.ActivityType;
+                sheet.Cell(row, 5).Value = item.UserTypeDisplay;
+                sheet.Cell(row, 6).Value = item.UserTypeId;
+                sheet.Cell(row, 7).Value = item.UsageLayerDisplay;
+                sheet.Cell(row, 8).Value = item.UsageLayerId;
+                row++;
+            }
+
+            var range = sheet.Range(2, 1, row - 1, 14);
+            range.Column(9).Style.NumberFormat.Format = "#,##0";
+            range.Column(10).Style.NumberFormat.Format = "#,##0";
+            range.Column(11).Style.NumberFormat.Format = "#,##0";
+            range.Column(12).Style.NumberFormat.Format = "#,##0";
+            range.Column(13).Style.NumberFormat.Format = "#,##0";
+            range.Column(14).Style.NumberFormat.Format = "#,##0";
+            range.Column(15).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            var table = range.CreateTable($"{_sheetName}_Table");
+            table.Theme = XLTableTheme.TableStyleMedium16;
+            sheet.Columns().AdjustToContents();
+
+            return workbook;
+        }
     }
+}
 }
