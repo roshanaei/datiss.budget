343cb53023aae7ad31aafa3d2f585d6bc9e9afbc
Erfan <erfan.eftekharizade@gmail.com>
2022-01-24 13:03:27 +0330

    Complete Service

---
+++ src/Datiss.Budget.Services/Excel/Models/CofficientImportModel.cs
@@ -0,0 +1,27 @@
+﻿using Ganss.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Excel.Models
+{
+    public class CofficientImportModel
+    {
+        [Column(MappingDirections.Both, Letter = "A")]
+        public string OrganizationDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "B")]
+        public int OrganizationId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public string CofficientTypeDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "D")]
+        public int CofficientTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "E")]
+        public int Fee { get; set; }
+    }
+}
