127113e3bb06210272e78c88e8810a4064237194
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-26 12:32:28 +0330

    Create IncomeCurrentWsHService

---
+++ src/Datiss.Budget.Services/IncomeCurrentWsHService.cs
@@ -0,0 +1,49 @@
+﻿using Datiss.Budget.DataLayer.Context;
+using Datiss.Budget.Entities;
+using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Security;
+using Datiss.Budget.Services.Contracts;
+using Datiss.Budget.Services.Contracts.Identity;
+using Datiss.Budget.Services.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Datiss.Budget.Services
+{
+    public class IncomeCurrentWsHService : IIncomeCurrentWsHService
+    {
+        private readonly IUserContext _userContext;
+        private readonly IUnitOfWork _uow;
+        private readonly IExcelService _excelService;
+        private readonly IUserService _userService;
+        private readonly IOrganizationService _organizationService;
+
+        private readonly DbSet<IncomeCurrentWsH> _dbSet;
+        private readonly DbSet<Organization> _orgDbSet;
+        private readonly DbSet<FinanceYear> _yearSet;
+        private readonly DbSet<Constant> _constSet;
+
+        public IncomeCurrentWsHService(
+            IUserContext userContext,
+            IUnitOfWork uow,
+            IExcelService excelService,
+            IUserService userService,
+            IOrganizationService organizationService)
+        {
+            _userContext = userContext ?? throw new ArgumentNullException(nameof(userContext));
+            _uow = uow ?? throw new ArgumentNullException(nameof(uow));
+            _dbSet = _uow.Set<IncomeCurrentWsH>();
+            _orgDbSet = _uow.Set<Organization>();
+            _yearSet = _uow.Set<FinanceYear>();
+            _constSet = _uow.Set<Constant>();
+            _excelService = excelService ?? throw new ArgumentNullException(nameof(excelService));
+            _userService = userService ?? throw new ArgumentNullException(nameof(userService));
+            _organizationService = organizationService ?? throw new ArgumentNullException(nameof(organizationService));
+        }
+    }
+}
