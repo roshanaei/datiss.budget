2b702ba3e7a2c718278acc68974a7852422f010f
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-26 11:30:09 +0330

    IncomeForcastOther entity

--- src/Datiss.Budget.Services/IncomeForcastOtherService.cs
+++ src/Datiss.Budget.Services/IncomeForcastOtherService.cs
@@ -600,10 +600,7 @@ namespace Datiss.Budget.Services
             if (filter.Search.IsNotNullOrEmpty())
             {
                 filter.Search = filter.Search.ToUpper().CorrectYeKe();
-                query = query.Where(_ => _.Organization.Title.ToUpper().Contains(filter.Search) ||
-                                    _.OIFType.Title.ToUpper().Contains(filter.Search) ||
-                                    _.OIFPrice.ToString().Contains(filter.Search) ||
-                                    _.OIFPrice.ToString().Contains(filter.Search));
+                query = query.Where(_ => _.OIFType.Title.ToUpper().Contains(filter.Search));
             }
 
             return query;
