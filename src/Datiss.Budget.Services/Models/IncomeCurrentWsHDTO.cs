953a535bee050491115ac53344a698b0abb58e1f
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-26 12:15:42 +0330

    add Create to DTO

--- src/Datiss.Budget.Services/Models/IncomeCurrentWsHDTO.cs
+++ src/Datiss.Budget.Services/Models/IncomeCurrentWsHDTO.cs
@@ -6,6 +6,47 @@ using System.Threading.Tasks;
 
 namespace Datiss.Budget.Services.Models
 {
+    public class CreateIncomeCurrentWsHDTO 
+    {
+        public int YearId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public int UserTypeId { get; set; }
+
+        public string UserTypeTitle { get; set; }
+
+        public int UsageLayerId { get; set; }
+
+        public string UsageLayerTitle { get; set; }
+
+        public int NumberUser { get; set; }
+
+        public int UnitUser { get; set; }
+
+        public decimal AvgConsumeUser { get; set; }
+
+        public int ConsumptionUser { get; set; }
+
+        public int Cost { get; set; }
+
+        public int Income { get; set; }
+
+        public int SubscriptionIncome { get; set; }
+
+        public int Note3Price { get; set; }
+
+        public int Note3Income { get; set; }
+
+        public int SeasonalIncome { get; set; }
+
+        public int TIncome { get; set; }
+
+        public int Note7Price { get; set; }
+
+        public int Note7Income { get; set; }
+    }
+
     public class IncomeCurrentWsHDTO
     {
         public int Id { get; set; }
