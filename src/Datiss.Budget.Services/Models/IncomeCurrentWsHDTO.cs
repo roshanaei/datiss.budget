5ed9e29e934b97d2e970b23ae0f7cbff6cd97f1c
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-26 12:08:55 +0330

    Create IncomeCurrentWsHDTO

---
+++ src/Datiss.Budget.Services/Models/IncomeCurrentWsHDTO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Models
+{
+    public class IncomeCurrentWsHDTO
+    {
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+
+        public int Year { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string OrganizationDisaplay { get; set; }
+
+        public int UserTypeId { get; set; }
+
+        public string UserTypeDisplay { get; set; }
+
+        public int UsageLayerId { get; set; }
+
+        public string UsageLayerDisplay { get; set; }
+
+        public int NumberUser { get; set; }
+
+        public int UnitUser { get; set; }
+
+        public decimal AvgConsumeUser { get; set; }
+
+        public int ConsumptionUser { get; set; }
+
+        public int Cost { get; set; }
+
+        public int Income { get; set; }
+
+        public int SubscriptionIncome { get; set; }
+
+        public int Note3Price { get; set; }
+
+        public int Note3Income { get; set; }
+
+        public int SeasonalIncome { get; set; }
+
+        public int TIncome { get; set; }
+
+        public int Note7Price { get; set; }
+
+        public int Note7Income { get; set; }
+    }
+}
