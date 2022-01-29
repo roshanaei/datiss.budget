6dbe183e8e5ee078cd68154b5c502832f8f9af66
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-29 16:39:56 +0330

    Create IIncomeCurrentWsNHService

---
+++ src/Datiss.Budget.Services/Contracts/IIncomeCurrentWsNHService.cs
@@ -0,0 +1,39 @@
+﻿using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Services.Infrastructure;
+using Datiss.Budget.Services.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Contracts
+{
+    public interface IIncomeCurrentWsNHService
+    {
+        Task<IncomeCurrentWsNH> GetByIdAsync(int id);
+
+        Task<ValidationResult<IncomeCurrentWsNHDTO>> CreateAsync(CreateIncomeCurrentWsNHDTO model);
+
+        Task<ValidationResult<IncomeCurrentWsNHDTO>> UpdateAsync(UpdateIncomeCurrentWsNHDTO model);
+
+        Task HardDeleteAsync(int Id);
+
+        Task<PagedResult<IncomeCurrentWsNHDTO>> GetListAsync(IncomeCurrentWsNHFilterDTO filter);
+
+        Task<OrganizationDeleteDataResult> HardDeleteAsync(int yearId, int organizationId);
+
+        Task<IEnumerable<CalculationItemData>> CalculationAsync(int yearId, int organizationId);
+
+        Task CopyAsync(int sourceYearId, int sourceOrgId, int destYearId);
+
+        Task<Stream> ExportExcelAsync(IncomeCurrentWsNHFilterDTO filter);
+
+        Task<IEnumerable<IncomeCurrentWsNHDTO>> GetExportItemsAsync(int yearId, int organizationId);
+
+        Task<ImportResult> ImportExcelAsync(IFormFile fileInfo, int yearId, bool continueIfAnyOrgMissing = false);
+
+    }
+}
