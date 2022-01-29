9cb5a703343e39c986c1297c37f5192a6e6e53f7
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-29 16:44:15 +0330

    Create IncomeCurrentWsNHService

---
+++ src/Datiss.Budget.Services/IncomeCurrentWsNHService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Datiss.Budget.Services.Contracts;
+using Datiss.Budget.Common.GuardToolkit;
+using Datiss.Budget.Services.Infrastructure;
+using Datiss.Budget.Services.Models;
+using Datiss.Budget.Resources;
+using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Services.Excel;
+using Datiss.Budget.Entities;
+using Datiss.Budget.Common.Exceptions;
+using Datiss.Budget.DataLayer.Context;
+using Datiss.Budget.Services.Contracts.Identity;
+using Mapster;
+using LinqKit;
+using Datiss.Budget.Services.Excel.Models;
+using Datiss.Budget.Security;
+using Datiss.Budget.Enum;
+using Microsoft.Data.SqlClient;
+using Datiss.Budget.Common;
+using Datiss.Budget.Extensions;
+
+namespace Datiss.Budget.Services
+{
+    public class IncomeCurrentWsNHService : IIncomeCurrentWsNHService
+    {
+        private readonly IUserContext _userContext;
+        private readonly IUnitOfWork _uow;
+        private readonly IExcelService _excelService;
+        private readonly IUserService _userService;
+        private readonly IOrganizationService _organizationService;
+
+        private readonly DbSet<IncomeCurrentWsNH> _dbSet;
+        private readonly DbSet<Organization> _orgDbSet;
+        private readonly DbSet<FinanceYear> _yearSet;
+        private readonly DbSet<Constant> _constSet;
+
+        public IncomeCurrentWsNHService(
+            IUserContext userContext,
+            IUnitOfWork uow,
+            IExcelService excelService,
+            IUserService userService,
+            IOrganizationService organizationService)
+        {
+            _userContext = userContext ?? throw new ArgumentNullException(nameof(userContext));
+            _uow = uow ?? throw new ArgumentNullException(nameof(uow));
+            _dbSet = _uow.Set<IncomeCurrentWsNH>();
+            _orgDbSet = _uow.Set<Organization>();
+            _yearSet = _uow.Set<FinanceYear>();
+            _constSet = _uow.Set<Constant>();
+            _excelService = excelService ?? throw new ArgumentNullException(nameof(excelService));
+            _userService = userService ?? throw new ArgumentNullException(nameof(userService));
+            _organizationService = organizationService ?? throw new ArgumentNullException(nameof(organizationService));
+        }
+    }
+}
