ffdbffa460b96e474490a71fa810226b5c09c001
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-29 09:29:49 +0330

    Add Copy[Post] to Controller

--- src/Datiss.Budget.Web/Controllers/IncomeCurrentWsHController.cs
+++ src/Datiss.Budget.Web/Controllers/IncomeCurrentWsHController.cs
@@ -393,6 +393,43 @@ namespace Datiss.Budget.Web.Controllers
             return PartialView("_copyModal", model);
         }
 
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Copy(CopyViewModel model)
+        {
+            model.CheckArgumentIsNull(nameof(model));
+
+            try
+            {
+                await _incomeCurrentWsHService.CopyAsync(
+                                                    model.SourceYearId,
+                                                    model.SourceOrgId,
+                                                    model.TargetYearId);
+                model.Succeed(ViewMessages.CopySuccess);
+            }
+            catch (CopySameYearException)
+            {
+                model.AddError(ViewMessages.CopySameYear);
+            }
+            catch (CopyDestYearExxeption)
+            {
+                model.AddError(ViewMessages.CopyErrorDestYear);
+            }
+            catch (CopyOrgNullDataException)
+            {
+                model.AddError(ViewMessages.CopySourceOrgNullData);
+            }
+            catch (CopyDestYearHasDataException)
+            {
+                model.AddError(ViewMessages.CopyDestYearHasData);
+            }
+            catch (Exception ex)
+            {
+                model.AddError(ViewMessages.SystemError);
+            }
+
+            return Json(model);
+        }
+
 
         #region Private Helper Methods
         private string getCalcTitle(string key)
