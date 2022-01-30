6ac5a54e80b64faa61e5a98cd3347db97084849f
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-30 09:32:30 +0330

    Create IncomeCurrentWsNHExcel

---
+++ src/Datiss.Budget.Reports/Excel/IncomeCurrentWsNHExcel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Reports.Excel
+{
+    public static class IncomeCurrentWsNHExcel
+    {
+        private const string _sheetName = "IncomeCurrentWsNH";
+    }
+}
