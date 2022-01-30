8451dcf1077c8892c95516831465f26a71ca7e4c
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-30 10:48:42 +0330

    Create IncomeCurrentWsNHController

---
+++ src/Datiss.Budget.Web/Controllers/IncomeCurrentWsNHController.cs
@@ -0,0 +1,54 @@
+﻿using Datiss.Budget.Services.Contracts;
+using Datiss.Budget.Services.Contracts.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Web.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    public class IncomeCurrentWsNHController : Controller
+    {
+        public const string Name = "WaterInstallFee";
+        private readonly ILogger<IncomeCurrentWsNHController> _logger;
+        private readonly IWebHostEnvironment _env;
+        private readonly IIncomeCurrentWsNHService _incomeCurrentWsNHService;
+        private readonly IConstantService _constantService;
+        private readonly IOrganizationService _organizationService;
+        private readonly IFinanceYearService _financeYearService;
+        private readonly ISecurityTrimmingService _securityTrimmingService;
+
+        public IncomeCurrentWsNHController(
+            ILogger<IncomeCurrentWsNHController> logger,
+            IWebHostEnvironment environment,
+            IIncomeCurrentWsNHService incomeCurrentWsNHService,
+            IOrganizationService organizationService,
+            IFinanceYearService financeYearService,
+            IConstantService constantService,
+            ISecurityTrimmingService securityTrimmingService)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _env = environment ?? throw new ArgumentNullException(nameof(environment));
+            _incomeCurrentWsNHService = incomeCurrentWsNHService ?? throw new ArgumentNullException(nameof(incomeCurrentWsNHService));
+            _organizationService = organizationService ?? throw new ArgumentNullException(nameof(organizationService));
+            _financeYearService = financeYearService ?? throw new ArgumentNullException(nameof(financeYearService));
+            _constantService = constantService ?? throw new ArgumentNullException(nameof(constantService));
+            _securityTrimmingService = securityTrimmingService ?? throw new ArgumentNullException(nameof(securityTrimmingService));
+        }
+
+
+        private void showMessage(string type, string message)
+        {
+            ViewData["type"] = type;
+            ViewData["message"] = message;
+        }
+
+
+    }
+}
