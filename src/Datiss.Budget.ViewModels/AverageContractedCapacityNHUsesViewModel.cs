03ee67fc751e903c4428ba72ed7682870f188c17
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-31 14:50:17 +0330

    Create ViewModel

---
+++ src/Datiss.Budget.ViewModels/AverageContractedCapacityNHUsesViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.ViewModels
+{
+    public class AverageContractedCapacityNHUsesViewModel
+    {
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+
+        public int Year { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string OrganizationDisplay { get; set; }
+
+        public int UserTypeId { get; set; }
+
+        public string UserTypeDisplay { get; set; }
+
+        public decimal AverageCapacity { get; set; }
+
+        public string AverageCapacityDisplay => AverageCapacity.ToString("N2");
+
+        public decimal AverageCapacityWs { get; set; }
+
+        public string AverageCapacityWsDisplay => AverageCapacityWs.ToString("N2");
+
+        public decimal AverageCapacityIncome { get; set; }
+
+        public string AverageCapacityIncomeDisplay => AverageCapacityIncome.ToString("N2");
+
+        public decimal AverageCapacityWsIncome { get; set; }
+
+        public string AverageCapacityWsIncomeDisplay => AverageCapacityWsIncome.ToString("N2");
+    }
+}
