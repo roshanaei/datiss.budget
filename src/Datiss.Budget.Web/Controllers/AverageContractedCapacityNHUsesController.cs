79ab33b65bac7b13062a1ff0b51e9733022d18f2
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-31 15:33:36 +0330

    Create Controller

---
+++ src/Datiss.Budget.Web/Controllers/AverageContractedCapacityNHUsesController.cs
@@ -0,0 +1,69 @@
+﻿using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Services.Contracts;
+using Datiss.Budget.Services.Contracts.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Web.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    public class AverageContractedCapacityNHUsesController : Controller
+    {
+        public const string Name = "WaterInstallFee";
+        public const string ACTION_Create = nameof(Create);
+        public const string ACTION_Index = nameof(Index);
+        public const string ACTION_Edit = nameof(Edit);
+        public const string ACTION_Copy = nameof(Copy);
+        public const string ACTION_Delete = nameof(Delete);
+        public const string ACTION_DeleteRecords = nameof(DeleteRecords);
+        public const string ACTION_ImportExcel = nameof(ImportExcel);
+        public const string ACTION_Calculation = nameof(Calculation);
+        public const string ACTION_DownloadExcelTemplate = nameof(DownloadExcelTemplate);
+        public const string ACTION_ExportExcel = nameof(ExportExcel);
+        public const string ACTION_GetExcelTemplate = nameof(GetExcelTemplate);
+
+        private string _indexFilterKey = $"{Name}_{ACTION_Index}_filter";
+
+        private readonly ILogger<AverageContractedCapacityNHUsesController> _logger;
+        private readonly IWebHostEnvironment _env;
+        private readonly IAverageContractedCapacityNHUsesService _averageContractedCapacityNHUsesService;
+        private readonly IConstantService _constantService;
+        private readonly IOrganizationService _organizationService;
+        private readonly IFinanceYearService _financeYearService;
+        private readonly ISecurityTrimmingService _securityTrimmingService;
+
+        public AverageContractedCapacityNHUsesController(
+            ILogger<AverageContractedCapacityNHUsesController> logger,
+            IWebHostEnvironment environment,
+            IAverageContractedCapacityNHUsesService averageContractedCapacityNHUsesService,
+            IOrganizationService organizationService,
+            IFinanceYearService financeYearService,
+            IConstantService constantService,
+            ISecurityTrimmingService securityTrimmingService)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _env = environment ?? throw new ArgumentNullException(nameof(environment));
+            _averageContractedCapacityNHUsesService = averageContractedCapacityNHUsesService ?? throw new ArgumentNullException(nameof(averageContractedCapacityNHUsesService));
+            _organizationService = organizationService ?? throw new ArgumentNullException(nameof(organizationService));
+            _financeYearService = financeYearService ?? throw new ArgumentNullException(nameof(financeYearService));
+            _constantService = constantService ?? throw new ArgumentNullException(nameof(constantService));
+            _securityTrimmingService = securityTrimmingService ?? throw new ArgumentNullException(nameof(securityTrimmingService));
+        }
+
+
+        private void showMessage(string type, string message)
+        {
+            ViewData["type"] = type;
+            ViewData["message"] = message;
+        }
+
+
+    }
+}
