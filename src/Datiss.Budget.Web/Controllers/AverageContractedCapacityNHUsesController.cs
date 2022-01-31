3545979c80430cad2f1d1f887dfce1bae68dfd89
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-31 16:39:09 +0330

    Add ExportExcel to Controller

--- src/Datiss.Budget.Web/Controllers/AverageContractedCapacityNHUsesController.cs
+++ src/Datiss.Budget.Web/Controllers/AverageContractedCapacityNHUsesController.cs
@@ -487,6 +487,16 @@ namespace Datiss.Budget.Web.Controllers
             return workbook.Deliver("AverageContractedCapacityNHUses-Import-Template.xlsx");
         }
 
+        [HttpGet("[action]/{orgid}/{yearid}")]
+        public async Task<IActionResult> ExportExcel(int orgid, int yearid)
+        {
+            var result = await _averageContractedCapacityNHUsesService.GetExportItemsAsync(yearid, orgid);
+            if (result.Count() == 0)
+                return RedirectToAction("Index");
+            using var workbook = result.ExportExcel();
+            return workbook.Deliver("AverageContractedCapacity.xlsx");
+        }
+
 
         #region Private Helper Methods
         private string getCalcTitle(string key)
