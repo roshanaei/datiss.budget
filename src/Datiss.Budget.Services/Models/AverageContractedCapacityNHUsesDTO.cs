0d2a288b3356a08618d623ceffc6046f46710ee2
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-31 11:14:52 +0330

    Create AverageContractedCapacityNHUsesDTO

---
+++ src/Datiss.Budget.Services/Models/AverageContractedCapacityNHUsesDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Models
+{
+    public class AverageContractedCapacityNHUsesDTO
+    {
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+
+        public int Year { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string OrganizationDisplay { get; set; }
+
+        public int UserTypeId { get; set; }
+
+        public string UserTypeDisplay { get; set; }
+
+        public decimal AverageCapacity { get; set; }
+
+        public decimal AverageCapacityWs { get; set; }
+
+        public decimal AverageCapacityIncome { get; set; }
+
+        public decimal AverageCapacityWsIncome { get; set; }
+    }
+}
