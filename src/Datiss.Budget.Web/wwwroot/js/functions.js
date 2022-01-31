a455d593c44d574521f783c41f2a2446858854ab
Erfan <erfan.eftekharizade@gmail.com>
2022-01-31 13:34:35 +0330

    Fix WaterInstallFee

--- src/Datiss.Budget.Web/wwwroot/js/functions.js
+++ src/Datiss.Budget.Web/wwwroot/js/functions.js
@@ -117,10 +117,24 @@ function askWarningMsg(title, message, cancelText, confirmText) {
     });
 }
 
-function ErorrPermission() {
+function ShowPermissionErorr() {
     Swal.fire({
         title: '<span class="my-font">خطا</span>',
         html: `<span class="my-font">شما مجاز به انجام این عملیات نمی باشید.</span>`,
+        type: 'error',
+        confirmButtonColor: '#2F8BE6',
+        animation: false,
+        customClass: 'animated flipInX',
+        confirmButtonText: 'بستن',
+        confirmButtonClass: 'btn btn-outline-danger mr-1 my-font',
+        buttonsStyling: false,
+    });
+}
+
+function ShowCommonErorr() {
+    Swal.fire({
+        title: '<span class="my-font">خطا</span>',
+        html: `<span class="my-font">مشکلی به وجود آمده است لطفا مجددا تلاش کنید .</span>`,
         type: 'error',
         confirmButtonColor: '#2F8BE6',
         animation: false,
