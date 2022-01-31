b569bc3ad675cdd47b1eae451e5441233e2980fe
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-31 15:24:46 +0330

    Create GetImportTemplate

--- src/Datiss.Budget.Reports/Excel/AverageContractedCapacityNHUsesExcel.cs
+++ src/Datiss.Budget.Reports/Excel/AverageContractedCapacityNHUsesExcel.cs
@@ -57,7 +57,7 @@ namespace Datiss.Budget.Reports.Excel
 
                 row++;
             }
-            var range = sheet.Range(1, 1, row - 1, 15);
+            var range = sheet.Range(1, 1, row - 1, 6);
 
             var table = range.CreateTable($"{_sheetName}_Table");
             table.Theme = XLTableTheme.TableStyleMedium16;
@@ -65,5 +65,61 @@ namespace Datiss.Budget.Reports.Excel
 
             return workbook;
         }
+
+        public static XLWorkbook GetImportTamplate(this IEnumerable<AverageContractedCapacityNHUsesDTO> items, int year)
+        {
+            if (items == null || !items.Any())
+                return null;
+
+            var workbook = new XLWorkbook();
+            var sheet = workbook.Worksheets.Add(_sheetName);
+
+            sheet.RightToLeft = true;
+            sheet.Cell(1, 1).Value = "ورود اطلاعات برای سال مالی : " + year;
+            sheet.Range(1, 1, 1, 5).Merge();
+
+            sheet.Cell(2, 1).Value = "عنوان سازمان";
+            sheet.Cell(2, 2).Value = "کد سازمان";
+            sheet.Cell(2, 3).Value = "کاربری";
+            sheet.Cell(2, 4).Value = "کد کاربری";
+            sheet.Cell(2, 5).Value = "متوسط ظرفیت قراردادی آب";
+            sheet.Cell(2, 6).Value = "متوسط ظرفیت قراردادی فاضلاب";
+            sheet.Cell(2, 7).Value = "درآمد سرمایه ای متوسط ظرفیت قراردادی آب";
+            sheet.Cell(2, 8).Value = "درآمد سرمایه ای متوسط ظرفیت قراردادی فاضلاب";
+
+            var totalCount = items.Count();
+            int row = 3;
+            for (int i = 0; i < totalCount; i++)
+            {
+                var item = items.ElementAt(i);
+                sheet.Cell(row, 1).Value = item.OrganizationDisplay;
+                sheet.Cell(row, 2).Value = item.OrganizationId;
+                sheet.Cell(row, 3).Value = item.UserTypeDisplay;
+                sheet.Cell(row, 4).Value = item.UserTypeId;
+                row++; //for keeping index in table records
+            }
+
+            var range = sheet.Range(2, 1, row - 1, 8);
+            //Other
+            range.Column(5).Style.NumberFormat.Format = "#,##0.00";
+            range.Column(5).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+            range.Column(6).Style.NumberFormat.Format = "#,##0.00";
+            range.Column(6).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+            range.Column(7).Style.NumberFormat.Format = "#,##0";
+            range.Column(7).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+            range.Column(8).Style.NumberFormat.Format = "#,##0";
+            range.Column(8).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+            //
+            var table = range.CreateTable($"{_sheetName}_Table");
+            table.Theme = XLTableTheme.TableStyleMedium16;
+            sheet.Columns().AdjustToContents();
+
+            return workbook;
+        }
+
     }
 }
