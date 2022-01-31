6cfbf2c72fba7c411af7f5aa98a90e78dab35675
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-01-31 11:53:36 +0330

    Add CreateAsync to Service

--- src/Datiss.Budget.Services/AverageContractedCapacityNHUsesService.cs
+++ src/Datiss.Budget.Services/AverageContractedCapacityNHUsesService.cs
@@ -12,6 +12,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Datiss.Budget.Common.GuardToolkit;
+using Datiss.Budget.Enum;
+using Datiss.Budget.Common.Exceptions;
+using Datiss.Budget.Services.Infrastructure;
+using Datiss.Budget.Services.Models;
+using Mapster;
+using Datiss.Budget.Resources;
 
 namespace Datiss.Budget.Services
 {
@@ -55,5 +62,79 @@ namespace Datiss.Budget.Services
             return await Task.FromResult(entity);
         }
 
+        public async Task<ValidationResult<AverageContractedCapacityNHUsesDTO>> CreateAsync(CreateAverageContractedCapacityNHUsesDTO model)
+        {
+            model.CheckArgumentIsNull(nameof(model));
+
+            var entity = new AverageContractedCapacityNHUses
+            {
+                YearId = model.YearId,
+                OrganizationId = model.OrganizationId,
+                UserTypeId = model.UserTypeId,
+                AverageCapacity = model.AverageCapacity,
+                AverageCapacityWs = model.AverageCapacityWs,
+                AverageCapacityIncome = model.AverageCapacityIncome,
+                AverageCapacityWsIncome = model.AverageCapacityWsIncome
+            };
+
+            model.UserTypeTitle = (await _constSet.FindAsync(model.UserTypeId)).Title;
+            var organizationDisplay = (await _orgDbSet.FindAsync(model.OrganizationId)).Title;
+            try
+            {
+                if (await checkLogicAsync(model.YearId, model.OrganizationId, model.UserTypeId))
+                {
+                    await _dbSet.AddAsync(entity);
+                    await _uow.SaveChangesAsync();
+
+                    var result = entity.Adapt<AverageContractedCapacityNHUsesDTO>();
+                    result.UserTypeDisplay = model.UserTypeTitle;
+                    result.OrganizationDisplay = organizationDisplay;
+                    result.Year = (await _yearSet.FindAsync(model.YearId)).Year;
+                    result.AverageCapacity = entity.AverageCapacity;
+                    result.AverageCapacityWs = entity.AverageCapacityWs;
+                    result.AverageCapacityIncome = entity.AverageCapacityIncome;
+                    result.AverageCapacityWsIncome = entity.AverageCapacityWsIncome;
+
+                    return ValidationResult<AverageContractedCapacityNHUsesDTO>.Success(result);
+                }
+            }
+            catch (DisbaledYearDataInputException)
+            {
+                return ValidationResult<AverageContractedCapacityNHUsesDTO>.Failed(ServiceMessages.Logic_InputDisableYearData);
+            }
+
+            return ValidationResult<AverageContractedCapacityNHUsesDTO>.Failed(
+                string.Format(ServiceMessages.Logic_UserTypeDuplicate,
+                model.UserTypeTitle, organizationDisplay)
+                );
+
+        }
+
+        #region Logics
+        private async Task<bool> checkLogicAsync(
+            int yearId,
+            int organizationId,
+            int dwaterTypeId,
+            int? id = null)
+        {
+            var year = await _yearSet.FindAsync(yearId);
+            year.CheckReferenceIsNull(nameof(year));
+
+            if (year.Status == EntityStatus.Disbaled)
+                throw new DisbaledYearDataInputException();
+
+            var result = id == null
+                ? await Query().AnyAsync(x => x.YearId == yearId &&
+                                                x.OrganizationId == organizationId &&
+                                                x.UserTypeId == dwaterTypeId)
+
+                : await Query().AnyAsync(x => x.YearId == yearId &&
+                                            x.OrganizationId == organizationId &&
+                                            x.UserTypeId == dwaterTypeId &&
+                                            x.Id != id);
+            return !result;
+        }
+
+        #endregion
     }
 }
