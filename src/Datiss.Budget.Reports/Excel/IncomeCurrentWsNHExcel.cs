6d27b2e3ec4d0278ff476a296ad888ce14bbf08d
Eshaq Jabbari <86953413+eshaqjabbarii@users.noreply.github.com>
2022-02-02 17:36:19 +0330

    Feature/IncomeCurrentWsNH (#121)

---
+++ src/Datiss.Budget.Reports/Excel/IncomeCurrentWsNHExcel.cs
@@ -0,0 +1,203 @@
+﻿using ClosedXML.Excel;
+using Datiss.Budget.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Reports.Excel
+{
+    public static class IncomeCurrentWsNHExcel
+    {
+        private const string _sheetName = "IncomeCurrentWsNH";
+
+        public static XLWorkbook ExportExcel(this IEnumerable<IncomeCurrentWsNHDTO> items)
+        {
+            if (items == null || !items.Any())
+                return null;
+
+            var workbook = new XLWorkbook();
+            var sheet = workbook.Worksheets.Add(_sheetName);
+
+            sheet.RightToLeft = true;
+            sheet.Cell(1, 1).Value = "سال";
+            sheet.Cell(1, 2).Value = "سازمان";
+            sheet.Cell(1, 3).Value = "کاربری";
+            sheet.Cell(1, 4).Value = "تعداد مشترک";
+            sheet.Cell(1, 5).Value = "آحاد مشترک";
+            sheet.Cell(1, 6).Value = "متوسط حجم دفع";
+            sheet.Cell(1, 7).Value = "حجم دفع";
+            sheet.Cell(1, 8).Value = "قیمت هر کاربری";
+            sheet.Cell(1, 9).Value = "درآمد کارمزد دفع";
+            sheet.Cell(1, 10).Value = "درآمد آبونمان";
+            sheet.Cell(1, 11).Value = "درآمد فاضلاب بهاء مازاد بر ظرفیت";
+            sheet.Cell(1, 12).Value = "درآمد فاضلاب بهاء فصلی";
+            sheet.Cell(1, 13).Value = "درآمد کل فاضلاب بهاء";
+            sheet.Cell(1, 14).Value = "ظرفیت قراردادی";
+            sheet.Cell(1, 15).Value = "قیمت فاضلاب تبصره 3";
+            sheet.Cell(1, 16).Value = "درآمد تبصره 3 فاضلاب بهاء";
+
+            var totalCount = items.Count();
+            int row = 2;
+            for (int i = 0; i < totalCount; i++)
+            {
+                var item = items.ElementAt(i);
+                sheet.Cell(row, 1).Value = item.Year.ToString();
+                sheet.Cell(row, 2).Value = item.OrganizationDisplay;
+                sheet.Cell(row, 3).Value = item.UserTypeDisplay;
+                sheet.Cell(row, 3).DataType = XLDataType.Text;
+                
+                sheet.Cell(row, 4).Value = item.NumberUser;
+                sheet.Cell(row, 4).Style.NumberFormat.Format = "#,##0";
+                sheet.Cell(row, 4).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                
+                sheet.Cell(row, 5).Value = item.UnitUser;
+                sheet.Cell(row, 5).Style.NumberFormat.Format = "#,##0";
+                sheet.Cell(row, 5).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                
+                sheet.Cell(row, 6).Value = item.AvgConsumeUser;
+                sheet.Cell(row, 6).Style.NumberFormat.Format = "#,##0.00";
+                sheet.Cell(row, 6).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+                sheet.Cell(row, 7).Value = item.ConsumptionUser;
+                sheet.Cell(row, 7).Style.NumberFormat.Format = "#,##0";
+                sheet.Cell(row, 7).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+                sheet.Cell(row, 8).Value = item.Capacity;
+                sheet.Cell(row, 8).Style.NumberFormat.Format = "#,##0.00";
+                sheet.Cell(row, 8).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+                sheet.Cell(row, 9).Value = item.Cost;
+                sheet.Cell(row, 9).Style.NumberFormat.Format = "#,##0";
+                sheet.Cell(row, 9).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                
+                sheet.Cell(row, 10).Value = item.Income;
+                sheet.Cell(row, 10).Style.NumberFormat.Format = "#,##0";
+                sheet.Cell(row, 10).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+                sheet.Cell(row, 11).Value = item.SubscriptionIncome;
+                sheet.Cell(row, 11).Style.NumberFormat.Format = "#,##0";
+                sheet.Cell(row, 11).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+                sheet.Cell(row, 12).Value = item.ExcessIncome;
+                sheet.Cell(row, 12).Style.NumberFormat.Format = "#,##0";
+                sheet.Cell(row, 12).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                
+                sheet.Cell(row, 13).Value = item.SeasonalIncome;
+                sheet.Cell(row, 13).Style.NumberFormat.Format = "#,##0";
+                sheet.Cell(row, 13).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                
+                sheet.Cell(row, 14).Value = item.Note3Price;
+                sheet.Cell(row, 14).Style.NumberFormat.Format = "#,##0";
+                sheet.Cell(row, 14).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                
+                sheet.Cell(row, 15).Value = item.Note3Income;
+                sheet.Cell(row, 15).Style.NumberFormat.Format = "#,##0";
+                sheet.Cell(row, 15).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                                
+                sheet.Cell(row, 16).Value = item.TotalIncome;
+                sheet.Cell(row, 16).Style.NumberFormat.Format = "#,##0";
+                sheet.Cell(row, 16).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+                row++;
+            }
+            var range = sheet.Range(1, 1, row - 1, 16);
+            var table = range.CreateTable($"{_sheetName}_Table");
+            table.Theme = XLTableTheme.TableStyleMedium16;
+            sheet.Columns().AdjustToContents();
+
+            return workbook;
+        }
+
+        public static XLWorkbook GetImportTemplate(this IEnumerable<IncomeCurrentWsNHDTO> items, int year)
+        {
+            if (items == null || !items.Any())
+                return null;
+
+            var workbook = new XLWorkbook();
+            var sheet = workbook.Worksheets.Add(_sheetName);
+
+            sheet.RightToLeft = true;
+            sheet.Cell(1, 1).Value = "ورود اطلاعات برای سال مالی : " + year;
+            sheet.Range(1, 1, 1, 17).Merge();
+
+            sheet.Cell(2, 1).Value = "عنوان سازمان";
+            sheet.Cell(2, 2).Value = "کد سازمان";
+            sheet.Cell(2, 3).Value = "عنوان کاربری";
+            sheet.Cell(2, 4).Value = "کد کاربری";
+            sheet.Cell(2, 5).Value = "تعداد مشترک";
+            sheet.Cell(2, 6).Value = "آحاد مشترک";
+            sheet.Cell(2, 7).Value = "متوسط حجم دفع";
+            sheet.Cell(2, 8).Value = "حجم دفع";
+            sheet.Cell(2, 9).Value = "قیمت هر کاربری";
+            sheet.Cell(2, 10).Value = "درآمد کارمزد دفع";
+            sheet.Cell(2, 11).Value = "درآمد آبونمان";
+            sheet.Cell(2, 12).Value = "درآمد فاضلاب بهاء مازاد بر ظرفیت";
+            sheet.Cell(2, 13).Value = "درآمد فاضلاب بهاء فصلی";
+            sheet.Cell(2, 14).Value = "درآمد کل فاضلاب بهاء";
+            sheet.Cell(2, 15).Value = "ظرفیت قراردادی";
+            sheet.Cell(2, 16).Value = "قیمت فاضلاب تبصره 3";
+            sheet.Cell(2, 17).Value = "درآمد تبصره 3 فاضلاب بهاء";
+
+            var totalCount = items.Count();
+            int row = 3;
+            for (int i = 0; i < totalCount; i++)
+            {
+                var item = items.ElementAt(i);
+                sheet.Cell(row, 1).Value = item.OrganizationDisplay;
+                sheet.Cell(row, 2).Value = item.OrganizationId;
+                sheet.Cell(row, 3).Value = item.UserTypeDisplay;
+                sheet.Cell(row, 4).Value = item.UserTypeId;
+                row++; //for keeping index in table records
+            }
+
+            var range = sheet.Range(2, 1, row - 1, 19);
+            range.Column(4).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Right;
+            //Other
+            range.Column(6).Style.NumberFormat.Format = "#,##0";
+            range.Column(6).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+            range.Column(7).Style.NumberFormat.Format = "#,##0";
+            range.Column(7).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            
+            range.Column(8).Style.NumberFormat.Format = "#,##0";
+            range.Column(8).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            
+            range.Column(9).Style.NumberFormat.Format = "#,##0.00";
+            range.Column(9).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            
+            range.Column(10).Style.NumberFormat.Format = "#,##0";
+            range.Column(10).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            
+            range.Column(11).Style.NumberFormat.Format = "#,##0";
+            range.Column(11).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            
+            range.Column(12).Style.NumberFormat.Format = "#,##0";
+            range.Column(12).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            
+            range.Column(13).Style.NumberFormat.Format = "#,##0";
+            range.Column(13).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            
+            range.Column(14).Style.NumberFormat.Format = "#,##0";
+            range.Column(14).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            
+            range.Column(15).Style.NumberFormat.Format = "#,##0";
+            range.Column(15).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            
+            range.Column(16).Style.NumberFormat.Format = "#,##0";
+            range.Column(16).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            
+            range.Column(17).Style.NumberFormat.Format = "#,##0";
+            range.Column(17).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            
+            //
+
+            var table = range.CreateTable($"{_sheetName}_Table");
+            table.Theme = XLTableTheme.TableStyleMedium16;
+            sheet.Columns().AdjustToContents();
+
+            return workbook;
+        }
+    }
+}
---
+++ src/Datiss.Budget.ViewModels/IncomeCurrentWsNHViewModel.cs
@@ -0,0 +1,239 @@
+﻿using Datiss.Budget.Resources;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.ViewModels
+{
+    public class CreateIncomeCurrentWsNHViewModel : BaseViewModel
+    {
+        public int YearId { get; set; }
+
+        public string YearDisplay { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string OrganizationDisplay { get; set; }
+
+        public int UserTypeId { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "Required")]
+        [Range(0, int.MaxValue, ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "isNumber")]
+        public int NumberUser { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "Required")]
+        [Range(0, int.MaxValue, ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "isNumber")]
+        public int UnitUser { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "Required")]
+        [Range(0, int.MaxValue, ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "isNumber")]
+        public decimal AvgConsumeUser { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "Required")]
+        [Range(0, int.MaxValue, ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "isNumber")]
+        public decimal Capacity { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "Required")]
+        [Range(0, int.MaxValue, ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "isNumber")]
+        public int ConsumptionUser { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "Required")]
+        [Range(0, int.MaxValue, ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "isNumber")]
+        public int Cost { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "Required")]
+        [Range(0, int.MaxValue, ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "isNumber")]
+        public int Income { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "Required")]
+        [Range(0, int.MaxValue, ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "isNumber")]
+        public int SubscriptionIncome { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "Required")]
+        [Range(0, int.MaxValue, ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "isNumber")]
+        public int ExcessIncome { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "Required")]
+        [Range(0, int.MaxValue, ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "isNumber")]
+        public int SeasonalIncome { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "Required")]
+        [Range(0, int.MaxValue, ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "isNumber")]
+        public int Note3Price { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "Required")]
+        [Range(0, int.MaxValue, ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "isNumber")]
+        public int Note3Income { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "Required")]
+        [Range(0, int.MaxValue, ErrorMessageResourceType = typeof(ViewModelString), ErrorMessageResourceName = "isNumber")]
+        public int TotalIncome { get; set; }
+
+        public IEnumerable<SelectListItem> UserTypeSource { get; set; }
+
+        public string UserTypeTitle
+        {
+            get
+            {
+                if (UserTypeSource == null || !UserTypeSource.Any())
+                    return string.Empty;
+
+                return UserTypeSource.FirstOrDefault(x => x.Value.ToString() == UserTypeId.ToString()).Text;
+            }
+        }
+    }
+
+    public class UpdateIncomeCurrentWsNHViewModel : CreateIncomeCurrentWsNHViewModel
+    {
+        public int Id { get; set; }
+    }
+
+    public class IncomeCurrentWsNHViewModel
+    {
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+
+        public int Year { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string OrganizationDisplay { get; set; }
+
+        public int UserTypeId { get; set; }
+
+        public string UserTypeDisplay { get; set; }
+
+        public int NumberUser { get; set; }
+
+        public string NumberUserDisplay => NumberUser.ToString("N0");
+
+        public int UnitUser { get; set; }
+
+        public string UnitUserDisplay => UnitUser.ToString("N0");
+
+        public decimal AvgConsumeUser { get; set; }
+
+        public string AvgConsumeUserDisplay => AvgConsumeUser.ToString("N2");
+
+        public decimal Capacity { get; set; }
+
+        public string CapacityDisplay => Capacity.ToString("N2");
+
+        public int ConsumptionUser { get; set; }
+
+        public string ConsumptionUserDisplay => ConsumptionUser.ToString("N0");
+        
+        public int Cost { get; set; }
+
+        public string CostDisplay => Cost.ToString("N0");
+
+        public int Income { get; set; }
+
+        public string IncomeDisplay => Income.ToString("N0");
+
+        public int SubscriptionIncome { get; set; }
+
+        public string SubscriptionIncomeDisplay => SubscriptionIncome.ToString("N0");
+
+        public int ExcessIncome { get; set; }
+
+        public string ExcessIncomeDisplay => ExcessIncome.ToString("N0");
+
+        public int SeasonalIncome { get; set; }
+
+        public string SeasonalIncomeDisplay => SeasonalIncome.ToString("N0");
+
+        public int Note3Price { get; set; }
+
+        public string Note3PriceDisplay => Note3Price.ToString("N0");
+       
+        public int Note3Income { get; set; }
+
+        public string Note3IncomeDisplay => Note3Income.ToString("N0");
+
+        public int TotalIncome { get; set; }
+
+        public string TotalIncomeDisplay => TotalIncome.ToString("N0");
+    }
+
+    public class IncomeCurrentWsNHFilterViewModel : FilterViewModel
+    {
+        public int? YearId { get; set; }
+
+        public int? OrganizationId { get; set; }
+
+        public IList<SelectListItem> YearSource { get; set; }
+
+        public IList<SelectListItem> OrganizationSource { get; set; }
+    }
+
+    public class IncomeCurrentWsNHIndexViewModel : PagedViewModel<IncomeCurrentWsNHViewModel>
+    {
+        public IncomeCurrentWsNHIndexViewModel()
+        {
+            Filter = new IncomeCurrentWsNHFilterViewModel();
+        }
+
+        public IncomeCurrentWsNHFilterViewModel Filter { get; set; }
+
+        public IList<SelectListItem> YearSource { get; set; }
+
+        public IList<SelectListItem> OrganizationSource { get; set; }
+
+        public IList<SelectListItem> InputOrganizationSource { get; set; }
+
+        public IList<SelectListItem> UserTypeSource { get; set; }
+
+        public IFormFile ExcelFile { get; set; }
+
+        public void SetYearSource(IEnumerable<DropDownItemViewModel> source)
+            => YearSource = source.Select(x => new SelectListItem
+            {
+                Text = x.Title,
+                Value = x.Id.ToString()
+            }).ToList();
+
+        public void SetOrganizationSource(IEnumerable<DropDownItemViewModel> source)
+            => OrganizationSource = source.Select(x => new SelectListItem
+            {
+                Text = x.Title,
+                Value = x.Id.ToString()
+            }).ToList();
+
+        public void SetInputOrganizationSource(IEnumerable<DropDownItemViewModel> source)
+            => InputOrganizationSource = source.Select(x => new SelectListItem
+            {
+                Text = x.Title,
+                Value = x.Id.ToString()
+            }).ToList();
+
+        public void SetUserTypeSource(IEnumerable<DropDownItemViewModel> source)
+            => UserTypeSource = source.Select(x => new SelectListItem
+            {
+                Text = x.Title,
+                Value = x.Id.ToString()
+            }).ToList();
+
+        public void SetOrganizationFilterSource(IEnumerable<DropDownItemViewModel> source, int? selectedOrgId = null)
+            => Filter.OrganizationSource = source.Select(x => new SelectListItem
+            {
+                Selected = x.Id == selectedOrgId,
+                Text = x.Title,
+                Value = x.Id.ToString()
+            }).ToList();
+
+        public void SetFinanceYearFilterSource(IEnumerable<DropDownItemViewModel> source, int? selectedYearId = null)
+            => Filter.YearSource = source.Select(x => new SelectListItem
+            {
+                Selected = x.Id == selectedYearId,
+                Text = x.Title,
+                Value = x.Id.ToString()
+            }).ToList();
+    }
+}
