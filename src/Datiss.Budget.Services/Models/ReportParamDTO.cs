231acca0304a57292e534dc7de4421e5aa105335
iman nemati <imun22@gmmail.com>
2022-02-05 20:32:28 +0330

    Add ReportService methods

---
+++ src/Datiss.Budget.Services/Models/ReportParamDTO.cs
@@ -0,0 +1,29 @@
+﻿using Datiss.Budget.Enum;
+
+namespace Datiss.Budget.Services.Models
+{
+    public class ReportParamDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public ReportParamType ParamType { get; set; }
+        public int ReportId { get; set; }
+        public string ConstantKey { get; set; }
+        public string ReportName { get; set; }
+        public string ReportTitle { get; set; }
+    }
+
+    public class CreateReportParamDTO
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public ReportParamType ParamType { get; set; }
+    }
+
+    public class EditReportParamDTO : CreateReportParamDTO 
+    { 
+        public int Id { get; set; }
+    }
+
+}
---
+++ src/Datiss.Budget.Web/Areas/Admin/Controllers/ReportsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Datiss.Budget.Services.Identity;
+
+namespace Datiss.Budget.Web.Admin.Controllers
+{
+
+    [Authorize(Roles = ConstantRoles.Admin)]
+    [Area(AreaConstants.AdminArea)]
+    [Route("[area]/[controller]")]
+    public class ReportsController : Controller
+    {
+
+        public const string Name = "Reports";
+        public const string ACTION_Index = nameof(Index);
+
+        [HttpGet("{page?}")]
+        public IActionResult Index(int page = 1) {
+            
+            return View();
+        }
+    }
+}
