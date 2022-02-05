32c9f9b3a16fa815c0f628028f21d3c13df1620d
iman nemati <imun22@gmmail.com>
2022-02-05 20:32:28 +0330

    Add ReportService methods

---
+++ src/Datiss.Budget.Services/Models/ReportDTO.cs
@@ -0,0 +1,54 @@
+﻿using Datiss.Budget.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Models
+{
+    public class ReportDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public EntityStatus Status { get; set; }
+        public byte[] FileData { get; set; }
+        public string FilePath { get; set; }
+    }
+
+    public class CreateReportData
+    {
+        public CreateReportData() 
+            => Parameters = new List<CreateReportParamDTO>();
+        
+        public void AddParam(CreateReportParamDTO p)
+            => Parameters.Add(p);
+
+        public void AddParam(string name, string title, ReportParamType paramType) 
+            => AddParam(new CreateReportParamDTO
+                {
+                    Name = name,
+                    Title = title,
+                    ParamType = paramType
+                });
+        
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public byte[] FileData { get; set; }
+        public IList<CreateReportParamDTO> Parameters { get; set; }
+    }
+
+    public class UpdateReportData : CreateReportData
+    {
+        public int Id { get; set; }
+        public EntityStatus Status { get; set; }
+    }
+
+    public class ReportFilterDTO : FilterInputDTO
+    {
+    }
+
+}
