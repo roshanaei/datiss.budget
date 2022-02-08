788a00e16fdd69784de0f5b0c19d62c812a25786
Erfan <erfan.eftekharizade@gmail.com>
2022-02-08 18:26:24 +0330

    Complete Interface

---
+++ src/Datiss.Budget.Services/IIncomeCurrentReportService.cs
@@ -0,0 +1,25 @@
+﻿using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Services.Infrastructure;
+using Datiss.Budget.Services.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services
+{
+    public interface IIncomeCurrentReportService
+    {
+        Task<IncomeCurrentReport> GetByIdAsync(int id);
+        Task<ValidationResult<IncomeCurrentReportDTO>> UpdateAsync(UpdateIncomeCurrentReportDTO model);
+        Task<OrganizationDeleteDataResult> HardDeleteAsync(int yearId, int organizationId);
+        Task<PagedResult<IncomeCurrentReportDTO>> GetListAsync(IncomeCurrentReportFilterDTO filter);
+        Task CopyAsync(int sourceYearId, int sourceOrgId, int destYearId);
+        Task<Stream> ExportExcelAsync(IncomeCurrentReportFilterDTO filter);
+        Task<IEnumerable<IncomeCurrentReportDTO>> GetExportItemsAsync(int yearId, int organizationId);
+        Task<ImportResult> ImportExcelAsync(IFormFile fileInfo, int yearId, bool continueIfAnyOrgMissing = false);
+    }
+}
