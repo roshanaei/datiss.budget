e16953fcd270519d33344e044e0ebe91fd996cc0
Erfan <58788132+ErfanEftekhari@users.noreply.github.com>
2022-02-12 13:41:10 +0330

    Erfan/pagging (#140)

--- src/Datiss.Budget.ViewModels/Base/PagedViewModel.cs
+++ src/Datiss.Budget.ViewModels/Base/PagedViewModel.cs
@@ -9,11 +9,20 @@ namespace Datiss.Budget.ViewModels
         }
 
         public IEnumerable<T> Items { get; set; }
-        public int PageSize { get; set; } = 10;
+        public int PageSize { get; set; }
         public int PreviousPage => PageNumber - 1;
-        public int PageNumber { get; set; } = 1;
+        public int PageNumber { get; set; }
         public int NextPage => PageNumber + 1;
         public int TotalCount { get; set; }
         public int PagesCount { get; set; }
+        public int PageLimit { get; set; } = 5;
+        public int StartPage { get; set; } = 1;
+        public int Lastpage => PagesCount;
+        public int TotalLimit => PagesCount % PageLimit == 0
+                               ? PagesCount / PageLimit
+                               : (PagesCount / PageLimit) + 1;
+        public int LimitNumber => PageNumber % PageLimit == 0
+                               ? PageNumber / PageLimit
+                               : (PageNumber / PageLimit) + 1;
     }
 }
