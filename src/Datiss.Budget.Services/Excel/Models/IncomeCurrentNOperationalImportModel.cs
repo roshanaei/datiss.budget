c3fbdf74c5091bd5f18977f9011184cfd47d8d88
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-02-13 08:42:41 +0330

    Create IncomeCurrentNOperationalImportModel

---
+++ src/Datiss.Budget.Services/Excel/Models/IncomeCurrentNOperationalImportModel.cs
@@ -0,0 +1,28 @@
+﻿using Ganss.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Excel.Models
+{
+    public class IncomeCurrentNOperationalImportModel
+    {
+        [Column(MappingDirections.Both, Letter = "A")]
+        public string OrganizationDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "B")]
+        public int OrganizationId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public string DWaterTypeDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "D")]
+        public int NOICTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "E")]
+        public int NOICPrice { get; set; }
+
+    }
+}
