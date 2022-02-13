04fa5c72011fa5dd9c61d1fde81ba6bfc0c7f758
Erfan <erfan.eftekharizade@gmail.com>
2022-02-13 10:47:48 +0330

    Commit

---
+++ src/Datiss.Budget.Web/ViewComponents/PagerViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Datiss.Budget.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Datiss.Budget.Common.GuardToolkit;
+
+namespace Datiss.Budget.Web
+{
+    public class PagerViewComponent : ViewComponent
+    {
+
+        public PagerViewComponent()
+        {
+
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(PagerViewModel model)
+        {
+            model.CheckArgumentIsNull(nameof(model));
+
+            return View(model);
+        }
+    }
+}
