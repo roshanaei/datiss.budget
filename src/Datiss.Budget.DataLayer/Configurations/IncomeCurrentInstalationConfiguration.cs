c1b80aab601a65f20f96d8ddcc0df4b617aa1a88
Your Name <you@example.com>
2022-02-13 17:24:34 +0330

    Add Configuration

---
+++ src/Datiss.Budget.DataLayer/Configurations/IncomeCurrentInstalationConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Datiss.Budget.Entities.DWH;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Datiss.Budget.DataLayer.Mappings
+{
+   public class IncomeCurrentInstalationConfiguration :IEntityTypeConfiguration<IncomeCurrentInstalation>
+    {
+        public void Configure(EntityTypeBuilder<IncomeCurrentInstalation> builder)
+        {
+            builder.ToTable("IncomeCurrentInstalation");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                    .HasColumnName("ICInstalationId");
+
+            builder.Property(x => x.OrganizationId).IsRequired();
+
+            builder.Property(x => x.YearId).IsRequired();
+
+            builder.Property(x => x.ICInstalationTypeId).IsRequired();
+
+            builder.HasOne(x => x.FinanceYear)
+                    .WithMany(x => x.IncomeCurrentInstalations)
+                    .HasForeignKey(x => x.YearId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.Organization)
+                    .WithMany(x => x.IncomeCurrentInstalations)
+                    .HasForeignKey(x => x.OrganizationId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.ICInstalationType)
+                    .WithMany(x => x.IncomeCurrentInstalations)
+                    .HasForeignKey(x => x.ICInstalationTypeId)
+                    .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
--- src/Datiss.Budget.Entities/DWH/IncomeCurrentInstalation.cs
+++ src/Datiss.Budget.Entities/DWH/IncomeCurrentInstalation.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Datiss.Budget.Entities.AuditableEntity;
+using Datiss.Budget.Enum;
 
 namespace Datiss.Budget.Entities.DWH
 {
-    class IncomeCurrentInstalation
+    public class IncomeCurrentInstalation : IAuditableEntity
     {
+        public IncomeCurrentInstalation() { }
+
+        #region Properties
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public ActivityType ActivityType { get; set; }
+
+        public int ICInstalationTypeId { get; set; }
+
+        public int NumberUser { get; set; }
+
+        public int Cost { get; set; }
+
+        public long Income { get; set; }
+
+        #endregion
+
+        #region Navigations
+        public FinanceYear FinanceYear { get; set; }
+
+        public Organization Organization { get; set; }
+
+        public Constant ICInstalationType { get; set; }
+
+        #endregion
     }
 }
