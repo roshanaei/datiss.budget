63c8973a7045a875f4a6ba8b767934d0c47a806e
Eshaq Jabbari <86953413+eshaqjabbarii@users.noreply.github.com>
2022-02-13 16:02:50 +0330

    Feature/income current n operational (#143)

--- src/Datiss.Budget.IocConfig/AddDatissBudgetServicesExtensions.cs
+++ src/Datiss.Budget.IocConfig/AddDatissBudgetServicesExtensions.cs
@@ -57,6 +57,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddScoped<IReportEngine, ReportEngine>();
             services.AddScoped<IRoleService, RoleService>();
             services.AddScoped<IAppClaimTypeService, AppClaimTypeService>();
+            services.AddScoped<IIncomeCurrentNOperationalService, IncomeCurrentNOperationalService>();
 
             return services;
         }
