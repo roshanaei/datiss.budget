acee3a47336f6ee94b00e9355c489a3cd12d3d2b
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-02-13 08:34:18 +0330

    Create IncomeCurrentNOpratiopnalDTO

---
+++ src/Datiss.Budget.Services/Models/IncomeCurrentNOperationalDTO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Models
+{
+    public class CreateIncomeCurrentNOperationalDTO
+    {
+        public int YearId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public int NOICTypeId { get; set; }
+
+        public int NOICPrice { get; set; }
+
+        public string NOICTypeTitle { get; set; }
+    }
+
+    public class UpdateIncomeCurrentNOperationalDTO : CreateIncomeCurrentNOperationalDTO
+    {
+        public int Id { get; set; }
+
+    }
+
+    public class IncomeCurrentNOperationalDTO
+    {
+        public int Id { get; set; }
+        public int YearId { get; set; }
+        public int Year { get; set; }
+        public int OrganizationId { get; set; }
+        public string OrganizationDisplay { get; set; }
+        public int NOICTypeId { get; set; }
+        public string NOICTypeDisplay { get; set; }
+        public int NOICPrice { get; set; }
+    }
+
+}
