ce6f14a0cd944ce6bf31d3e0f30c1232f21cf2ca
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-02-14 17:43:02 +0330

    Add ImportTemplate to Excel

--- src/Datiss.Budget.Reports/Excel/CostCurrentInstallationExcel.cs
+++ src/Datiss.Budget.Reports/Excel/CostCurrentInstallationExcel.cs
@@ -1,5 +1,7 @@
 ﻿using ClosedXML.Excel;
+using Datiss.Budget.Enum;
 using Datiss.Budget.Services.Models;
+using Datiss.Budget.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -56,5 +58,60 @@ namespace Datiss.Budget.Reports.Excel
 
             return workbook;
         }
+
+        public static XLWorkbook GetImportTemplate(this IEnumerable<CostCurrentInstalationDTO> items, int year)
+        {
+            if (items == null || !items.Any())
+                return null;
+
+            var workbook = new XLWorkbook();
+            var sheet = workbook.Worksheets.Add(_sheetName);
+
+            sheet.RightToLeft = true;
+            sheet.Cell(1, 1).Value = "ورود اطلاعات برای سال مالی : " + year;
+            sheet.Range(1, 1, 1, 9).Merge();
+
+            sheet.Cell(2, 1).Value = "عنوان سازمان";
+            sheet.Cell(2, 2).Value = "کد سازمان";
+            sheet.Cell(2, 3).Value = "عنوان بخش";
+            sheet.Cell(2, 4).Value = "کد بخش";
+            sheet.Cell(2, 5).Value = "عنوان";
+            sheet.Cell(2, 6).Value = "کد عنوان";
+            sheet.Cell(2, 7).Value = "تعداد انشعاب طی دوره";
+            sheet.Cell(2, 8).Value = "بهاء واحد";
+            sheet.Cell(2, 9).Value = "جمع کل هزینه";
+
+            var totalCount = items.Count();
+            int row = 3;
+            for (int i = 0; i < totalCount; i++)
+            {
+                var item = items.ElementAt(i);
+                sheet.Cell(row, 1).Value = item.OrganizationDisplay;
+                sheet.Cell(row, 2).Value = item.OrganizationId;
+                sheet.Cell(row, 3).Value = item.CCInstalationTypeTitle;
+                sheet.Cell(row, 4).Value = item.CCInstalationTypeId;
+                sheet.Cell(row, 5).Value = item.ActivityType.ToDisplay();
+                sheet.Cell(row, 6).Value = item.ActivityType == ActivityType.Water ? 0 : 1;
+                row++; //for keeping index in table records
+            }
+
+            var range = sheet.Range(2, 1, row - 1, 9);
+            range.Column(5).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Right;
+            //Other
+            range.Column(7).Style.NumberFormat.Format = "#,##0";
+            range.Column(7).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            range.Column(8).Style.NumberFormat.Format = "#,##0";
+            range.Column(8).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            range.Column(9).Style.NumberFormat.Format = "#,##0";
+            range.Column(9).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            //
+            var table = range.CreateTable($"{_sheetName}_Table");
+            table.Theme = XLTableTheme.TableStyleMedium16;
+            sheet.Columns().AdjustToContents();
+
+            return workbook;
+        }
     }
+
+}
 }
