b56d2343c7040d7b93585ce33874a916a6178c99
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-02-14 17:57:34 +0330

    Create Controller

---
+++ src/Datiss.Budget.Web/Controllers/CostCurrentInstallationController.cs
@@ -0,0 +1,64 @@
+﻿using Datiss.Budget.Services.Contracts;
+using Datiss.Budget.Services.Contracts.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Web.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    public class CostCurrentInstallationController : Controller
+    {
+        public const string Name = "CostCurrentInstallation";
+
+        public const string ACTION_Create = nameof(Create);
+        public const string ACTION_Index = nameof(Index);
+        public const string ACTION_Edit = nameof(Edit);
+        public const string ACTION_Copy = nameof(Copy);
+        public const string ACTION_Delete = nameof(Delete);
+        public const string ACTION_DeleteRecords = nameof(DeleteRecords);
+        public const string ACTION_ImportExcel = nameof(ImportExcel);
+        public const string ACTION_Calculation = nameof(Calculation);
+        public const string ACTION_ExportExcel = nameof(ExportExcel);
+        public const string ACTION_GetExcelTemplate = nameof(GetExcelTemplate);
+
+        private string _indexFilterKey = $"{Name}_{ACTION_Index}_filter";
+
+        private readonly ILogger<CostCurrentInstallationController> _logger;
+        private readonly IWebHostEnvironment _env;
+        private readonly ICostCurrentInstallationService _costCurrentInstallationService;
+        private readonly IConstantService _constantService;
+        private readonly IOrganizationService _organizationService;
+        private readonly IFinanceYearService _financeYearService;
+        private readonly ISecurityTrimmingService _securityTrimmingService;
+
+        public CostCurrentInstallationController(
+            ILogger<CostCurrentInstallationController> logger,
+            IWebHostEnvironment environment,
+            ICostCurrentInstallationService costCurrentInstallationService,
+            IOrganizationService organizationService,
+            IFinanceYearService financeYearService,
+            IConstantService constantService,
+            ISecurityTrimmingService securityTrimmingService)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _env = environment ?? throw new ArgumentNullException(nameof(environment));
+            _costCurrentInstallationService = costCurrentInstallationService ?? throw new ArgumentNullException(nameof(costCurrentInstallationService));
+            _organizationService = organizationService ?? throw new ArgumentNullException(nameof(organizationService));
+            _financeYearService = financeYearService ?? throw new ArgumentNullException(nameof(financeYearService));
+            _constantService = constantService ?? throw new ArgumentNullException(nameof(constantService));
+            _securityTrimmingService = securityTrimmingService ?? throw new ArgumentNullException(nameof(securityTrimmingService));
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
