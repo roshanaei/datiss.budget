842793bdae2353d7504ad5e85d7b0daec2a6a4dc
EshaqJabbari93 <eshaqjabbari_dev@outlook.com>
2022-02-14 12:29:11 +0330

    Create CCI DTO

---
+++ src/Datiss.Budget.Services/Models/CostCurrentInstalationDTO.cs
@@ -0,0 +1,59 @@
+﻿using Datiss.Budget.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Models
+{
+    public class CreateCostCurrentInstalationDTO
+    {
+        public int YearId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public ActivityType ActivityType { get; set; }
+
+        public int CCInstalationTypeId { get; set; }
+
+        public string CCInstalationTypeTitle { get; set; }
+
+        public int NumberUser { get; set; }
+
+        public int Cost { get; set; }
+
+        public long Income { get; set; }
+    }
+
+    public class UpdateCostCurrentInstalationDTO : CreateCostCurrentInstalationDTO
+    {
+        public int Id { get; set; }
+    }
+
+    public class CostCurrentInstalationDTO
+    {
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+
+        public int Year { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string OrganizationDisplay { get; set; }
+
+        public ActivityType ActivityType { get; set; }
+
+        public int CCInstalationTypeId { get; set; }
+
+        public string CCInstalationTypeTitle { get; set; }
+
+        public int NumberUser { get; set; }
+
+        public int Cost { get; set; }
+
+        public long Income { get; set; }
+    }
+
+}
