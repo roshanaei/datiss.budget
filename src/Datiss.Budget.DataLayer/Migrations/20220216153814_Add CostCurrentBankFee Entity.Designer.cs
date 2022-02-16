926fdb5d4b8b9aa0ffd4fcc61714e6c38203c68b
Your Name <you@example.com>
2022-02-16 19:10:10 +0330

    Add COstCenterBankFee Migration

--- src/Datiss.Budget.DataLayer/Migrations/20220216153814_Add CostCurrentBankFee Entity.Designer.cs
+++ src/Datiss.Budget.DataLayer/Migrations/20220216153814_Add CostCurrentBankFee Entity.Designer.cs
@@ -4,14 +4,16 @@ using Datiss.Budget.DataLayer.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Datiss.Budget.DataLayer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220216153814_Add CostCurrentBankFee Entity")]
+    partial class AddCostCurrentBankFeeEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
