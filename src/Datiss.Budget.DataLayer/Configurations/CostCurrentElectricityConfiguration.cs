50a744af2d3cd77e851f9628a3600ff3e9b43e9d
Your Name <you@example.com>
2022-02-16 15:25:03 +0330

    Add CCElectricity Configuration

---
+++ src/Datiss.Budget.DataLayer/Configurations/CostCurrentElectricityConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Datiss.Budget.Entities.DWH;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Datiss.Budget.DataLayer.Mappings
+{
+    public class CostCurrentElectricityConfiguration :IEntityTypeConfiguration<CostCurrentElectricity>
+    {
+        public void Configure(EntityTypeBuilder<CostCurrentElectricity> builder)
+        {
+            builder.ToTable("CostCurrentElectricity");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                   .HasColumnName("CCElectricityId");
+
+            builder.Property(x => x.YearId)
+                    .IsRequired();
+
+            builder.Property(x => x.OrganizationId)
+                    .IsRequired();
+
+            builder.Property(x => x.ActivityType)
+                    .IsRequired();
+
+            builder.HasOne(x => x.FinanceYear)
+                    .WithMany(x => x.CostCurrentElectricity)
+                    .HasForeignKey(x => x.YearId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.Organization)
+                    .WithMany(x => x.CostCurrentElectricity)
+                    .HasForeignKey(x => x.OrganizationId)
+                    .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
