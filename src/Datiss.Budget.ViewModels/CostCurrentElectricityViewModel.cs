4edf25569e5636107adefe8d2731a9ff1ef5c580
eshaqjabbarii <eshaqjabbari_dev@outlook.com>
2022-02-22 09:47:03 +0330

    Create ViewModel

---
+++ src/Datiss.Budget.ViewModels/CostCurrentElectricityViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Datiss.Budget.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.ViewModels
+{
+    public class CostCurrentElectricityViewModel
+    {
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+
+        public int Year { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string OrganizationDisplay { get; set; }
+
+        public ActivityType ActivityType { get; set; }
+
+        public string ActivityTypeDisplay => ActivityType.ToDisplay();
+
+        public int ElectricityAmount { get; set; }
+
+        public string ElectricityAmountDisplay => ElectricityAmount.ToString("N0");
+
+        public long ElectricityCost { get; set; }
+
+        public string ElectricityCostDisplay => ElectricityCost.ToString("N0");
+    }
+}
