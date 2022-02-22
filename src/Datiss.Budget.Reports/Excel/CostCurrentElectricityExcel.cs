c8d6c0dcaeb50eb198d75a3769ec9413179c8dbb
eshaqjabbarii <eshaqjabbari_dev@outlook.com>
2022-02-22 10:13:02 +0330

    Add ExcelExport in CCEExcel

---
+++ src/Datiss.Budget.Reports/Excel/CostCurrentElectricityExcel.cs
@@ -0,0 +1,59 @@
+﻿using ClosedXML.Excel;
+using Datiss.Budget.Services.Models;
+using Datiss.Budget.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Reports.Excel
+{
+    public static class CostCurrentElectricityExcel
+    {
+        private const string _sheetName = "CostCurrentElectricity";
+
+        public static XLWorkbook ExportExcel(this IEnumerable<CostCurrentElectricityDTO> items)
+        {
+            if (items == null || !items.Any())
+                return null;
+
+            var workbook = new XLWorkbook();
+            var sheet = workbook.Worksheets.Add(_sheetName);
+
+            sheet.RightToLeft = true;
+            sheet.Cell(1, 1).Value = "سال";
+            sheet.Cell(1, 2).Value = "سازمان";
+            sheet.Cell(1, 3).Value = "نوع فعالیت";
+            sheet.Cell(1, 4).Value = "برق مصرفی کیلو وات ساعت";
+            sheet.Cell(1, 5).Value = "میلیون ریال";
+
+            var totalCount = items.Count();
+            int row = 2;
+
+            for (int i = 0; i < totalCount; i++)
+            {
+                var item = items.ElementAt(i);
+                sheet.Cell(row, 1).Value = item.Year.ToString();
+                sheet.Cell(row, 2).Value = item.OrganizationDisplay;
+                sheet.Cell(row, 3).Value = item.ActivityType.ToDisplay();
+
+                sheet.Cell(row, 4).Value = item.ElectricityAmount;
+                sheet.Cell(row, 4).Style.NumberFormat.Format = "#,##0";
+                sheet.Cell(row, 4).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+                sheet.Cell(row, 5).Value = item.ElectricityCost;
+                sheet.Cell(row, 5).Style.NumberFormat.Format = "#,##0";
+                sheet.Cell(row, 5).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+                row++;
+            }
+            var range = sheet.Range(1, 1, row - 1, 7);
+            var table = range.CreateTable($"{_sheetName}_Table");
+            table.Theme = XLTableTheme.TableStyleMedium16;
+            sheet.Columns().AdjustToContents();
+
+            return workbook;
+        }
+    }
+}
