c80390d74120395c726190e685e6d6c41950ab70
eshaqjabbarii <eshaqjabbari_dev@outlook.com>
2022-02-22 10:52:00 +0330

    Create CostCurrentElectricityService

--- src/Datiss.Budget.Services/Contracts/ICostCurrentElectricityService.cs
+++ src/Datiss.Budget.Services/Contracts/ICostCurrentElectricityService.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Datiss.Budget.Services.Excel
+namespace Datiss.Budget.Services.Contracts
 {
     public interface ICostCurrentElectricityService
     {
