780f977c45fd7d3365b9e2c4ab1e326eaccebaac
eshaqjabbarii <eshaqjabbari_dev@outlook.com>
2022-02-22 09:50:01 +0330

    Add Create to ViewModel

--- src/Datiss.Budget.ViewModels/CostCurrentElectricityViewModel.cs
+++ src/Datiss.Budget.ViewModels/CostCurrentElectricityViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Datiss.Budget.Enum;
+using Datiss.Budget.Extensions;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +9,24 @@ using System.Threading.Tasks;
 
 namespace Datiss.Budget.ViewModels
 {
+    public class CreateCostCurrentElectricityViewModel : BaseViewModel
+    {
+        public int YearId { get; set; }
+
+        public string YearDisplay { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string OrganizationDisplay { get; set; }
+
+        public ActivityType ActivityType { get; set; }
+
+        public int ElectricityAmount { get; set; }
+
+        public long ElectricityCost { get; set; }
+
+        public IEnumerable<SelectListItem> ActivityTypeSource => EnumSelectListProvider.GetActivityTypeItems(ActivityType);
+    }
     public class CostCurrentElectricityViewModel
     {
         public int Id { get; set; }
