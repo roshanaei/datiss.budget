32ba51962c59882c61b7ccba985b08b1e1c789eb
eshaqjabbarii <eshaqjabbari_dev@outlook.com>
2022-02-23 15:00:57 +0330

    Add Create to DTO

--- src/Datiss.Budget.Services/Models/CostCurrentConsumableDTO.cs
+++ src/Datiss.Budget.Services/Models/CostCurrentConsumableDTO.cs
@@ -7,6 +7,21 @@ using System.Threading.Tasks;
 
 namespace Datiss.Budget.Services.Models
 {
+    public class CreateCostCurrentConsumableDTO
+    {
+        public int YearId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public ActivityType ActivityType { get; set; }
+
+        public int ConsumableTypeId { get; set; }
+
+        public int ConsumableAmount { get; set; }
+
+        public long ConsumableCost { get; set; }
+    }
+
     public class CostCurrentConsumableDTO
     {
         public int Id { get; set; }
