093d9a06b0c67b850860547f1e0d533e31a7c2ba
eshaqjabbarii <eshaqjabbari_dev@outlook.com>
2022-02-23 19:38:10 +0330

    Add Calc to Service

--- src/Datiss.Budget.Services/CostCurrentConsumableService.cs
+++ src/Datiss.Budget.Services/CostCurrentConsumableService.cs
@@ -18,6 +18,7 @@ using Mapster;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -218,6 +219,35 @@ namespace Datiss.Budget.Services
 
             return await Task.FromResult(result);
         }
+
+        public async Task<IEnumerable<CalculationItemData>> CalculationAsync(int yearId, int organizationId)
+        {
+            List<SqlParameter> sqlParams = new List<SqlParameter>
+            {
+                new SqlParameter("YearId", yearId),
+                new SqlParameter("OrganizationId", organizationId)
+            };
+            var result = new List<CalculationItemData>();
+
+            result.Add(new CalculationItemData
+            {
+                Key = "CostCurrentConsumable_Cal1",
+                Value = await _uow.ExecuteScalar<int>(
+                        "[dbo].[CostCurrentConsumable_Cal1] @YearId, @OrganizationId",
+                        parameters: sqlParams.ToArray())
+            });
+
+            result.Add(new CalculationItemData
+            {
+                Key = "CostCurrentConsumable_Cal2",
+                Value = await _uow.ExecuteScalar<int>(
+                        "[dbo].[CostCurrentConsumable_Cal2] @YearId, @OrganizationId",
+                        parameters: sqlParams.ToArray())
+            });
+
+            return await Task.FromResult(result);
+        }
+
 
         #region Private Helper Methods
         private async Task<IQueryable<CostCurrentConsumable>> setFilter(
