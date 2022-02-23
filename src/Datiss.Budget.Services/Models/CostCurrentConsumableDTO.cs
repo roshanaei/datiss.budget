bce2a6a5d874c6c8623d06a82d5dc443aa444546
eshaqjabbarii <eshaqjabbari_dev@outlook.com>
2022-02-23 14:55:36 +0330

    Create CostCurrentConsumableDTO

---
+++ src/Datiss.Budget.Services/Models/CostCurrentConsumableDTO.cs
@@ -0,0 +1,32 @@
+﻿using Datiss.Budget.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Models
+{
+    public class CostCurrentConsumableDTO
+    {
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+
+        public int Year { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string OrganizationDisplay { get; set; }
+
+        public ActivityType ActivityType { get; set; }
+
+        public int ConsumableTypeId { get; set; }
+
+        public string ConsumableTypeDisplay { get; set; }
+
+        public int ConsumableAmount { get; set; }
+
+        public long ConsumableCost { get; set; }
+    }
+}
