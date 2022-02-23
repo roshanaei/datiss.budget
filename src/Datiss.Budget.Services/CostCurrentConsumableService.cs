ee3e24def63804772339a476e5746cce16519db1
eshaqjabbarii <eshaqjabbari_dev@outlook.com>
2022-02-23 16:45:51 +0330

    Create Servcie

---
+++ src/Datiss.Budget.Services/CostCurrentConsumableService.cs
@@ -0,0 +1,257 @@
+﻿using Datiss.Budget.Common.Exceptions;
+using Datiss.Budget.Common.GuardToolkit;
+using Datiss.Budget.DataLayer.Context;
+using Datiss.Budget.Entities;
+using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Enum;
+using Datiss.Budget.Extensions;
+using Datiss.Budget.Security;
+using Datiss.Budget.Services.Contracts;
+using Datiss.Budget.Services.Contracts.Identity;
+using Datiss.Budget.Services.Excel;
+using Datiss.Budget.Services.Models;
+using LinqKit;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services
+{
+    public class CostCurrentConsumableService : ICostCurrentConsumableService
+    {
+        private readonly IUserContext _userContext;
+        private readonly IUnitOfWork _uow;
+        private readonly IExcelService _excelService;
+        private readonly IUserService _userService;
+        private readonly IOrganizationService _organizationService;
+
+        private readonly DbSet<CostCurrentConsumable> _dbSet;
+        private readonly DbSet<Organization> _orgDbSet;
+        private readonly DbSet<FinanceYear> _yearSet;
+
+        public CostCurrentConsumableService(
+            IUserContext userContext,
+            IUnitOfWork uow,
+            IExcelService excelService,
+            IUserService userService,
+            IOrganizationService organizationService)
+        {
+            _userContext = userContext ?? throw new ArgumentNullException(nameof(userContext));
+            _uow = uow ?? throw new ArgumentNullException(nameof(uow));
+            _dbSet = _uow.Set<CostCurrentConsumable>();
+            _orgDbSet = _uow.Set<Organization>();
+            _yearSet = _uow.Set<FinanceYear>();
+            _excelService = excelService ?? throw new ArgumentNullException(nameof(excelService));
+            _userService = userService ?? throw new ArgumentNullException(nameof(userService));
+            _organizationService = organizationService ?? throw new ArgumentNullException(nameof(organizationService));
+        }
+        private IQueryable<CostCurrentConsumable> Query()
+            => _dbSet.AsNoTracking();
+
+        #region Private Helper Methods
+        private async Task<IQueryable<CostCurrentConsumable>> setFilter(
+            IQueryable<CostCurrentConsumable> query,
+            CostCurrentConsumableFilterDTO filter)
+        {
+            query.CheckArgumentIsNull(nameof(query));
+            filter.CheckArgumentIsNull(nameof(filter));
+
+            var predicate = PredicateBuilder.New<CostCurrentConsumable>();
+
+            if (filter.YearId.HasValue)
+                query = query.Where(x => x.YearId == filter.YearId.Value);
+
+            if (filter.OrganizationId.HasValue)
+            {
+                var organizations = await _organizationService
+                    .GetWithChildrenAsync(filter.OrganizationId.Value);
+
+                foreach (var org in organizations)
+                {
+                    predicate.Or(_ => _.OrganizationId == org.Id);
+                }
+
+                query = query.Where(predicate);
+            }
+            if (filter.ActivityType.HasValue)
+                query = query.Where(x => x.ActivityType == filter.ActivityType.Value);
+
+            if (filter.Search.IsNotNullOrEmpty())
+            {
+                filter.Search = filter.Search.ToUpper().CorrectYeKe();
+                query = query.Where(_ => _.Organization.Title.ToUpper().Contains(filter.Search) ||
+                                         _.ConsumableType.Title.ToUpper().Contains(filter.Search));
+            }
+
+            return query;
+        }
+
+        private IQueryable<CostCurrentConsumable> setOrder(
+           IQueryable<CostCurrentConsumable> query,
+           string orderBy = "id",
+           bool desc = false)
+        {
+            if (string.IsNullOrWhiteSpace(orderBy))
+                orderBy = "id";
+
+            orderBy = orderBy.ToLower();
+            switch (orderBy)
+            {
+
+                case "organization":
+                    return desc
+                        ? query.OrderByDescending(x => x.Organization.Title)
+                        : query.OrderBy(x => x.Organization.Title);
+
+                case "consumabletype":
+                    return desc
+                        ? query.OrderByDescending(x => x.ConsumableType.DisplayOrder)
+                        : query.OrderBy(x => x.ConsumableType.DisplayOrder);
+
+                case "getexport":
+                    return query.Include(x => x.Organization)
+                                .OrderBy(x => x.ActivityType)
+                                .ThenBy(x => x.Organization.DisplayOrder)
+                                .ThenBy(x => x.Organization.Type)
+                                .ThenBy(x => x.Organization.ParentId);
+                default:
+                    return query.Include(x => x.Organization)
+                                .OrderBy(x => x.Organization.DisplayOrder)
+                                .ThenBy(x => x.Organization.Type)
+                                .ThenBy(x => x.Organization.ParentId)
+                                .ThenBy(x => x.ConsumableType.DisplayOrder);
+            }
+        }
+
+        private async Task<IEnumerable<CostCurrentConsumable>> getChildrenData(
+            int parentOrganizationId,
+            int yearId,
+            int targetYearId,
+            ActivityType activityType)
+        {
+
+            var children = await _orgDbSet
+                .Where(_ => _.Status != EntityStatus.Deleted &&
+                            _.ParentId == parentOrganizationId)
+                .ToListAsync();
+
+            var result = new List<CostCurrentConsumable>();
+
+            foreach (var org in children)
+            {
+                if (await Query()
+                            .Where(_ => _.OrganizationId == org.Id)
+                            .Where(_ => _.YearId == targetYearId)
+                            .Where(_ => _.ActivityType == activityType).AnyAsync())
+                {
+                    throw new CopyDestYearHasDataException();
+                }
+
+                var data = await Query()
+                                .Where(_ => _.YearId == yearId)
+                                .Where(_ => _.OrganizationId == org.Id)
+                                .Where(_ => _.ActivityType == activityType)
+                                .ToListAsync();
+
+                foreach (var item in data)
+                {
+                    if (!await checkLogicAsync(targetYearId, item.OrganizationId, item.ActivityType))
+                        throw new CopyDestYearHasDataException();
+
+                    var entity = new CostCurrentConsumable
+                    {
+                        OrganizationId = item.OrganizationId,
+                        YearId = targetYearId,
+                        ActivityType = item.ActivityType,
+                        ConsumableTypeId = item.ConsumableTypeId,
+                        ConsumableAmount = item.ConsumableAmount,
+                        ConsumableCost = item.ConsumableCost                        
+                    };
+
+                    result.Add(entity);
+                }
+
+                result.AddRange(await getChildrenData(org.Id, yearId, targetYearId, activityType));
+            }
+
+            return result;
+        }
+        private async Task<IEnumerable<CostCurrentConsumable>> getChildren(
+            int parentOrganizationId,
+            int yearId,
+            ActivityType activityType)
+        {
+            var children = await _orgDbSet
+                .Where(_ => _.ParentId == parentOrganizationId)
+                .ToListAsync();
+            var result = new List<CostCurrentConsumable>();
+            foreach (var org in children)
+            {
+                var data = await Query()
+                                .Where(_ => _.YearId == yearId)
+                                .Where(_ => _.OrganizationId == org.Id)
+                                .Where(_ => _.ActivityType == activityType)
+                                .ToListAsync();
+
+                foreach (var item in data)
+                {
+                    result.Add(item);
+                }
+                result.AddRange(await getChildren(org.Id, yearId, activityType));
+            }
+            return result;
+        }
+        private async Task<bool> hasAnyDataAsync(int orgid, int yearid, ActivityType activityType)
+        {
+            bool any = await Query().AnyAsync(x => x.OrganizationId == orgid &&
+                                                x.YearId == yearid &&
+                                                x.ActivityType == activityType);
+            if (any)
+            {
+                return true;
+            }
+            else
+            {
+                var childs = await _organizationService.GetWithChildrenAsync(orgid);
+                foreach (var child in childs)
+                    if (await Query().AnyAsync(x => x.YearId == yearid &&
+                                                    x.OrganizationId == child.Id &&
+                                                    x.ActivityType == activityType))
+                        return true;
+            }
+
+            return false;
+
+        }
+        #endregion
+
+        #region Logics
+        private async Task<bool> checkLogicAsync(
+            int yearId,
+            int organizationId,
+            ActivityType activityType,
+            int? id = null)
+        {
+            var year = await _yearSet.FindAsync(yearId);
+            year.CheckReferenceIsNull(nameof(year));
+
+            if (year.Status == EntityStatus.Disbaled)
+                throw new DisbaledYearDataInputException();
+
+            var result = id == null
+                ? await Query().AnyAsync(x => x.YearId == yearId &&
+                                                x.OrganizationId == organizationId &&
+                                                x.ActivityType == activityType)
+
+                : await Query().AnyAsync(x => x.YearId == yearId &&
+                                            x.OrganizationId == organizationId &&
+                                            x.ActivityType == activityType &&
+                                            x.Id != id);
+            return !result;
+        }
+        #endregion
+    }
+}
