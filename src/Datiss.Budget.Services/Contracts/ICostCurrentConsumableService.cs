d273d97c7759cb03610aa692eef3345ca9d41f1b
eshaqjabbarii <eshaqjabbari_dev@outlook.com>
2022-02-23 16:28:45 +0330

    Create Service Interface

---
+++ src/Datiss.Budget.Services/Contracts/ICostCurrentConsumableService.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Datiss.Budget.Services.Infrastructure;
+using Datiss.Budget.Services.Models;
+using Datiss.Budget.Entities.DWH;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+
+namespace Datiss.Budget.Services.Contracts
+{
+    public interface ICostCurrentConsumableService
+    {
+        Task<CostCurrentConsumable> GetByIdAsync(int id);
+
+        Task<ValidationResult<CostCurrentConsumableDTO>> CreateAsync(CreateCostCurrentConsumableDTO model);
+
+        Task<ValidationResult<CostCurrentConsumableDTO>> UpdateAsync(UpdateCostCurrentConsumableDTO model);
+
+        Task HardDeleteAsync(int Id);
+
+        Task<PagedResult<CostCurrentConsumableDTO>> GetListAsync(CostCurrentConsumableFilterDTO filter);
+
+        Task<OrganizationDeleteDataResult> HardDeleteAsync(int yearId, int organizationId);
+
+        Task CopyAsync(int sourceYearId, int sourceOrgId, int destYearId);
+
+        Task<IEnumerable<CostCurrentConsumableDTO>> GetExportItemsAsync(int yearId, int organizationId);
+
+        Task<ImportResult> ImportExcelAsync(IFormFile fileInfo, int yearId, bool continueIfAnyOrgMissing = false);
+    }
+}
