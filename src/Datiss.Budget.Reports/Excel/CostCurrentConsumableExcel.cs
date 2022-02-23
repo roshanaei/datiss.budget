0e8919da4476a6e77dd5fae9282ada7459d46503
eshaqjabbarii <eshaqjabbari_dev@outlook.com>
2022-02-23 16:00:08 +0330

    Add ExcelExport to CCCExcel

---
+++ src/Datiss.Budget.Reports/Excel/CostCurrentConsumableExcel.cs
@@ -0,0 +1,64 @@
+﻿using ClosedXML.Excel;
+using Datiss.Budget.Services.Models;
+using Datiss.Budget.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Reports.Excel
+{
+    public static class CostCurrentConsumableExcel
+    {
+        private const string _sheetName = "CostCurrentConsumable";
+
+        public static XLWorkbook ExportExcel(this IEnumerable<CostCurrentConsumableDTO> items)
+        {
+            if (items == null || !items.Any())
+                return null;
+
+            var workbook = new XLWorkbook();
+            var sheet = workbook.Worksheets.Add(_sheetName);
+
+            sheet.RightToLeft = true;
+            sheet.Cell(1, 1).Value = "سال";
+            sheet.Cell(1, 2).Value = "سازمان";
+            sheet.Cell(1, 3).Value = "نوع فعالیت";
+            sheet.Cell(1, 4).Value = "ماده مصرفی";
+            sheet.Cell(1, 5).Value = "مقدار";
+            sheet.Cell(1, 6).Value = "مبلغ";
+
+            var totalCount = items.Count();
+            int row = 2;
+
+            for (int i = 0; i < totalCount; i++)
+            {
+                var item = items.ElementAt(i);
+                sheet.Cell(row, 1).Value = item.Year.ToString();
+                sheet.Cell(row, 2).Value = item.OrganizationDisplay;
+                sheet.Cell(row, 3).Value = item.ActivityType.ToDisplay();
+
+                sheet.Cell(row, 4).Value = item.ConsumableTypeDisplay;
+                sheet.Cell(row, 4).Style.NumberFormat.Format = ConstantReport.__NumberFormat;
+                sheet.Cell(row, 4).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+                sheet.Cell(row, 5).Value = item.ConsumableAmount;
+                sheet.Cell(row, 5).Style.NumberFormat.Format = ConstantReport.__NumberFormat;
+                sheet.Cell(row, 5).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                
+                sheet.Cell(row, 6).Value = item.ConsumableCost;
+                sheet.Cell(row, 6).Style.NumberFormat.Format = ConstantReport.__NumberFormat;
+                sheet.Cell(row, 6).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                
+                row++;
+            }
+            var range = sheet.Range(1, 1, row - 1, 6);
+            var table = range.CreateTable($"{_sheetName}_Table");
+            table.Theme = XLTableTheme.TableStyleMedium16;
+            sheet.Columns().AdjustToContents();
+
+            return workbook;
+        }
+    }
+}
