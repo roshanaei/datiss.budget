bb528a71065b03c70215ddca25874dfeecb82042
eshaqjabbarii <eshaqjabbari_dev@outlook.com>
2022-02-23 15:51:28 +0330

    Add Index to ViewModel

--- src/Datiss.Budget.ViewModels/CostCurrentConsumableViewModel.cs
+++ src/Datiss.Budget.ViewModels/CostCurrentConsumableViewModel.cs
@@ -84,4 +84,69 @@ namespace Datiss.Budget.ViewModels
         public IList<SelectListItem> OrganizationSource { get; set; }
         public IList<SelectListItem> ActivityTypeSource => EnumSelectListProvider.GetActivityTypeItems(ActivityType).ToList();
     }
+
+    public class CostCurrentConsumableIndexViewModel : PagedViewModel<CostCurrentConsumableViewModel>
+    {
+        public CostCurrentConsumableIndexViewModel()
+        {
+            Filter = new CostCurrentConsumableFilterViewModel();
+        }
+        public CostCurrentConsumableFilterViewModel Filter { get; set; }
+
+        public ActivityType activityType { get; set; }
+
+        public IList<SelectListItem> YearSource { get; set; }
+
+        public IList<SelectListItem> OrganizationSource { get; set; }
+
+        public IList<SelectListItem> InputOrganizationSource { get; set; }
+
+        public IList<SelectListItem> ConsumableTypeSource { get; set; }
+
+        public void SetYearSource(IEnumerable<DropDownItemViewModel> source)
+            => YearSource = source.Select(x => new SelectListItem
+            {
+                Text = x.Title,
+                Value = x.Id.ToString()
+            }).ToList();
+
+        public void SetOrganizationSource(IEnumerable<DropDownItemViewModel> source)
+            => OrganizationSource = source.Select(x => new SelectListItem
+            {
+                Text = x.Title,
+                Value = x.Id.ToString()
+            }).ToList();
+
+        public void SetInputOrganizationSource(IEnumerable<DropDownItemViewModel> source)
+            => InputOrganizationSource = source.Select(x => new SelectListItem
+            {
+                Text = x.Title,
+                Value = x.Id.ToString()
+            }).ToList();
+
+        public void SetConsumableTypeSource(IEnumerable<DropDownItemViewModel> source)
+            => ConsumableTypeSource = source.Select(x => new SelectListItem
+            {
+                Text = x.Title,
+                Value = x.Id.ToString()
+            }).ToList();
+
+
+        public void SetOrganizationFilterSource(IEnumerable<DropDownItemViewModel> source, int? selectedOrgId = null)
+            => Filter.OrganizationSource = source.Select(x => new SelectListItem
+            {
+                Selected = x.Id == selectedOrgId,
+                Text = x.Title,
+                Value = x.Id.ToString()
+            }).ToList();
+
+        public void SetFinanceYearFilterSource(IEnumerable<DropDownItemViewModel> source, int? selectedYearId = null)
+            => Filter.YearSource = source.Select(x => new SelectListItem
+            {
+                Selected = x.Id == selectedYearId,
+                Text = x.Title,
+                Value = x.Id.ToString()
+            }).ToList();
+    }
+
 }
