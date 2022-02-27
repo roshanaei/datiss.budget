b9e3c828503033fc5ca6dedba711f5a4fc1f3fee
Your Name <you@example.com>
2022-02-27 18:29:03 +0330

    AddOrgFilterIncomeForcastWs

--- src/Datiss.Budget.Web/Controllers/IncomeForcastWsController.cs
+++ src/Datiss.Budget.Web/Controllers/IncomeForcastWsController.cs
@@ -133,11 +133,11 @@ namespace Datiss.Budget.Web.Controllers
             var userTypeSource = (await _constantService.GetByConstantKeyAsync(ConstantKeys.__UserType))
                 .Adapt<IEnumerable<DropDownItemViewModel>>();
 
-            var inputOrgSource = (await _organizationService.GetDropDownDataAsync(true))
-               .Adapt<List<DropDownItemViewModel>>();
-
             filter.YearId = maxYear;
             filter.OrganizationId = firstOrgId;
+
+            var inputOrgSource = (await _organizationService.GetDropDownInputDataAsync(filter.OrganizationId))
+                 .Adapt<List<DropDownItemViewModel>>();
 
             var myfilter = TempData.Get<IncomeForcastWsFilterViewModel>(_indexFilterKey);
             if (myfilter != null)
@@ -188,8 +188,8 @@ namespace Datiss.Budget.Web.Controllers
             var userSource = (await _constantService.GetByConstantKeyAsync(ConstantKeys.__UserType))
                 .Adapt<IEnumerable<DropDownItemViewModel>>();
 
-            var inputOrgSource = (await _organizationService.GetDropDownDataAsync(true))
-               .Adapt<List<DropDownItemViewModel>>();
+            var inputOrgSource = (await _organizationService.GetDropDownInputDataAsync(filter.OrganizationId))
+                .Adapt<List<DropDownItemViewModel>>();
 
             model.SetYearSource(yearSource);
             model.SetOrganizationSource(orgSource);
