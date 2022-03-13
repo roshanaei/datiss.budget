2c2da44a21fdc13578ae5c20cfa888c5068c9738
erfaneftekhari <Erfan.Eftekharizade@Gmail.com>
2022-03-13 18:13:12 +0330

    Complete Servie And View

---
+++ src/Datiss.Budget.Services/Excel/Models/UserTypeAverageCapacityCostImportModel.cs
@@ -0,0 +1,25 @@
+﻿using Ganss.Excel;
+
+namespace Datiss.Budget.Services.Excel.Models
+{
+    public class UserTypeAverageCapacityCostImportModel
+    {
+        [Column(MappingDirections.Both, Letter = "A")]
+        public string OrganizationDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "B")]
+        public int OrganizationId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public string UserTypeDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "D")]
+        public int UserTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "E")]
+        public decimal AverageCapacityWIncome { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "F")]
+        public decimal AverageCapacityWsIncome { get; set; }
+    }
+}
