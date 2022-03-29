3a73aeb08225b05bf215c2590bb22d2d55084a79
Erfan <58788132+ErfanEftekhari@users.noreply.github.com>
2022-03-30 02:51:19 +0430

    Erfan/delete entities (#180)

--- src/Datiss.Budget.Services/ConsumeForcastService.cs
+++ src/Datiss.Budget.Services/ConsumeForcastService.cs
@@ -226,13 +226,20 @@ namespace Datiss.Budget.Services
                                    .Where(x => x.OrganizationId == organizationId)
                                    .ToListAsync();
 
-            var childrens = await getChildren(organizationId, yearId);
+            IEnumerable<ConsumeForcast> childrens = new ConsumeForcast[] { };
+
+            if (organization.Type == OrganizationType.County || organization.Type == OrganizationType.Root)
+            {
+                childrens = await getChildren(organizationId, yearId);
+            }
 
             if (self.Count() == 0 && childrens.Count() == 0)
                 throw new DeleteNullRecordException();
 
             _dbSet.RemoveRange(self);
-            _dbSet.RemoveRange(childrens);
+
+            if (childrens.Any())
+                _dbSet.RemoveRange(childrens);
 
             var result = new OrganizationDeleteDataResult
             {
--- src/Datiss.Budget.Services/NHCoService.cs
+++ src/Datiss.Budget.Services/NHCoService.cs
@@ -210,13 +210,21 @@ namespace Datiss.Budget.Services
                                    .Where(_ => _.OrganizationId == organizationId)
                                    .Where(_ => _.ActivityType == activityType)
                                    .ToListAsync();
-            var childrens = await getChildren(organizationId, yearId, activityType);
+
+            IEnumerable<NHCo> childrens = new NHCo[] { };
+
+            if (organization.Type == OrganizationType.County || organization.Type == OrganizationType.Root)
+            {
+                childrens = await getChildren(organizationId, yearId, activityType);
+            }
+
 
             if (self.Count() == 0 && childrens.Count() == 0)
                 throw new DeleteNullRecordException();
             _dbSet.RemoveRange(self);
 
-            _dbSet.RemoveRange(childrens);
+            if (childrens.Any())
+                _dbSet.RemoveRange(childrens);
 
             var result = new OrganizationDeleteDataResult
             {
