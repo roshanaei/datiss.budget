73d52d5e401e079556ef178838aeb42cf5f333a0
Your Name <you@example.com>
2022-03-30 16:41:58 +0430

    Add CostCurrentFinancing Configuration

--- src/Datiss.Budget.Entities/FinanceYear.cs
+++ src/Datiss.Budget.Entities/FinanceYear.cs
@@ -112,7 +112,7 @@ namespace Datiss.Budget.Entities
 
         public ICollection<CostCurrentNO> CostCurrentNO { get; set; }
 
-        public ICollection<CostCurrentFinancing> costCurrentFinancing { get; set; }
+        public ICollection<CostCurrentFinancing> CostCurrentFinancing { get; set; }
 
         #endregion
     }
