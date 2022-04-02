9a81cf7888a00c668f1c23285a898e973f6f6317
Erfan <erfan.eftekharizade@gmail.com>
2022-04-03 00:48:22 +0430

    Complete Controller

---
+++ src/Datiss.Budget.Reports/Excel/CostCurrentContractualExcel.cs
@@ -0,0 +1,101 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using ClosedXML.Excel;
+using Datiss.Budget.Services.Models;
+
+namespace Datiss.Budget.Reports.Excel
+{
+
+    public static class CostCurrentContractualExcel
+    {
+        private const string _sheetName = "CostCurrentContractual";
+
+        public static XLWorkbook ExportExcel(this IEnumerable<CostCurrentContractualDTO> items)
+        {
+            if (items == null || !items.Any())
+                return null;
+
+            var workbook = new XLWorkbook();
+            var sheet = workbook.Worksheets.Add(_sheetName);
+
+            sheet.RightToLeft = true;
+            sheet.Cell(1, 1).Value = "سال";
+            sheet.Cell(1, 2).Value = "سازمان";
+            sheet.Cell(1, 3).Value = "مراکز هزینه";
+            sheet.Cell(1, 4).Value = "هزینه کارمزد بانکی سال گذشته";
+            sheet.Cell(1, 5).Value = "هزینه کارمزد بانکی";
+
+            var totalCount = items.Count();
+            int row = 2;
+            for (int i = 0; i < totalCount; i++)
+            {
+                var item = items.ElementAt(i);
+                sheet.Cell(row, 1).Value = item.Year.ToString();
+                sheet.Cell(row, 2).Value = item.OrganizationDisplay;
+                sheet.Cell(row, 3).Value = item.CostCenterTypeDisplay;
+                sheet.Cell(row, 3).DataType = XLDataType.Text;
+                sheet.Cell(row, 4).Value = item.ContractLastYear;
+                sheet.Cell(row, 4).Style.NumberFormat.Format = ConstantReport.__NumberFormat;
+                sheet.Cell(row, 4).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                sheet.Cell(row, 5).Value = item.ContractForcast;
+                sheet.Cell(row, 5).Style.NumberFormat.Format = ConstantReport.__NumberFormat;
+                sheet.Cell(row, 5).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+                row++;
+            }
+            var range = sheet.Range(1, 1, row - 1, 5);
+            var table = range.CreateTable($"{_sheetName}_Table");
+            table.Theme = XLTableTheme.TableStyleMedium16;
+            sheet.Columns().AdjustToContents();
+
+            return workbook;
+        }
+
+        public static XLWorkbook GetImportTemplate(this IEnumerable<CostCurrentContractualDTO> items, int year)
+        {
+            if (items == null || !items.Any())
+                return null;
+
+            var workbook = new XLWorkbook();
+            var sheet = workbook.Worksheets.Add(_sheetName);
+
+            sheet.RightToLeft = true;
+            sheet.Cell(1, 1).Value = "ورود اطلاعات برای سال مالی : " + year;
+            sheet.Range(1, 1, 1, 6).Merge();
+
+            sheet.Cell(2, 1).Value = "عنوان سازمان";
+            sheet.Cell(2, 2).Value = "کد سازمان";
+            sheet.Cell(2, 3).Value = "مراکز هزینه";
+            sheet.Cell(2, 4).Value = "کد مراکز هزینه";
+            sheet.Cell(2, 5).Value = "هزینه کارمزد بانکی سال گذشته";
+            sheet.Cell(2, 6).Value = "هزینه کارمزد بانکی";
+
+            var totalCount = items.Count();
+            int row = 3;
+            for (int i = 0; i < totalCount; i++)
+            {
+                var item = items.ElementAt(i);
+                sheet.Cell(row, 1).Value = item.OrganizationDisplay;
+                sheet.Cell(row, 2).Value = item.OrganizationId;
+                sheet.Cell(row, 3).Value = item.CostCenterTypeDisplay;
+                sheet.Cell(row, 4).Value = item.CostCenterTypeId;
+                row++; //for keeping index in table records
+            }
+
+            var range = sheet.Range(2, 1, row - 1, 6);
+            range.Column(3).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Right;
+            //Other
+            range.Column(5).Style.NumberFormat.Format = ConstantReport.__NumberFormat;
+            range.Column(5).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            range.Column(6).Style.NumberFormat.Format = ConstantReport.__NumberFormat;
+            range.Column(6).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            //
+            var table = range.CreateTable($"{_sheetName}_Table");
+            table.Theme = XLTableTheme.TableStyleMedium16;
+            sheet.Columns().AdjustToContents();
+
+            return workbook;
+        }
+
+    }
+}
---
+++ src/Datiss.Budget.ViewModels/CostCurrentContractualViewModel.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace Datiss.Budget.ViewModels
+{
+    public class CreateCostCurrentContractualViewModel : BaseViewModel
+    {
+        public int YearId { get; set; }
+        public string YearDisplay { get; set; }
+
+        public int OrganizationId { get; set; }
+        public string OrganizationDisplay { get; set; }
+
+        public int CostCenterTypeId { get; set; }
+
+        public string ContractDescription { get; set; }
+
+        public bool ExtensionId { get; set; }
+
+
+        [Required(ErrorMessage = "*")]
+        public long ContractLastYear { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public long ContractForcast { get; set; }
+
+        public IEnumerable<SelectListItem> CostCenterTypeSource { get; set; }
+
+        public string CostCenterTypeTitle
+        {
+            get
+            {
+                if (CostCenterTypeSource == null || !CostCenterTypeSource.Any())
+                    return string.Empty;
+
+                return CostCenterTypeSource.FirstOrDefault(x => x.Value.ToString() == CostCenterTypeId.ToString()).Text;
+            }
+        }
+
+    }
+
+    public class UpdateCostCurrentContractualViewModel : CreateCostCurrentContractualViewModel
+    {
+        public int Id { get; set; }
+
+    }
+
+    public class CostCurrentContractualViewModel
+    {
+        public int Id { get; set; }
+        public int YearId { get; set; }
+        public int Year { get; set; }
+        public int OrganizationId { get; set; }
+        public string OrganizationDisplay { get; set; }
+        public int CostCenterTypeId { get; set; }
+        public string CostCenterTypeDisplay { get; set; }
+        public string ContractDescription { get; set; }
+        public bool ExtensionId { get; set; }
+        public long ContractLastYear { get; set; }
+        public string ContractLastYearDisplay => ContractLastYear.ToString("N0");
+        public long ContractForcast { get; set; }
+        public string ContractForcastDisplay => ContractForcast.ToString("N0");
+    }
+
+    public class CostCurrentContractualFilterViewModel : FilterViewModel
+    {
+        public int? YearId { get; set; }
+        public int? OrganizationId { get; set; }
+        public int? CostCenterTypeId { get; set; }
+        public bool? ExtensionId { get; set; }
+        
+        public IList<SelectListItem> YearSource { get; set; }
+
+        public IList<SelectListItem> OrganizationSource { get; set; }
+    }
+
+    public class CostCurrentContractualIndexViewModel : PagedViewModel<CostCurrentContractualViewModel>
+    {
+
+        public CostCurrentContractualIndexViewModel()
+        {
+            Filter = new CostCurrentContractualFilterViewModel();
+        }
+
+        public CostCurrentContractualFilterViewModel Filter { get; set; }
+
+        public IList<SelectListItem> YearSource { get; set; }
+
+        public IList<SelectListItem> OrganizationSource { get; set; }
+
+        public IList<SelectListItem> InputOrganizationSource { get; set; }
+
+        public IList<SelectListItem> CostCenterTypeSource { get; set; }
+
+
+        public void SetYearSource(IEnumerable<DropDownItemViewModel> source)
+            => YearSource = source.Select(x => new SelectListItem
+            {
+                Text = x.Title,
+                Value = x.Id.ToString()
+            }).ToList();
+
+        public void SetOrganizationSource(IEnumerable<DropDownItemViewModel> source)
+            => OrganizationSource = source.Select(x => new SelectListItem
+            {
+                Text = x.Title,
+                Value = x.Id.ToString()
+            }).ToList();
+
+        public void SetInputOrganizationSource(IEnumerable<DropDownItemViewModel> source)
+            => InputOrganizationSource = source.Select(x => new SelectListItem
+            {
+                Text = x.Title,
+                Value = x.Id.ToString()
+            }).ToList();
+
+        public void SetCostCenterTypeSource(IEnumerable<DropDownItemViewModel> source)
+            => CostCenterTypeSource = source.Select(x => new SelectListItem
+            {
+                Text = x.Title,
+                Value = x.Id.ToString()
+            }).ToList();
+
+        public void SetOrganizationFilterSource(IEnumerable<DropDownItemViewModel> source, int? selectedOrgId = null)
+            => Filter.OrganizationSource = source.Select(x => new SelectListItem
+            {
+                Selected = x.Id == selectedOrgId,
+                Text = x.Title,
+                Value = x.Id.ToString()
+            }).ToList();
+
+        public void SetFinanceYearFilterSource(IEnumerable<DropDownItemViewModel> source, int? selectedYearId = null)
+            => Filter.YearSource = source.Select(x => new SelectListItem
+            {
+                Selected = x.Id == selectedYearId,
+                Text = x.Title,
+                Value = x.Id.ToString()
+            }).ToList();
+
+    }
+
+}
