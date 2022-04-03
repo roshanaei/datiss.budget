2010b7052df7c31c0a25bc25f11f758a8f64efc9
Your Name <you@example.com>
2022-04-03 18:20:42 +0430

    CostCurrent UpdateViewModel

--- src/Datiss.Budget.ViewModels/CostCurrentNOViewModel.cs
+++ src/Datiss.Budget.ViewModels/CostCurrentNOViewModel.cs
@@ -15,6 +15,35 @@ namespace Datiss.Budget.ViewModels
 
         public string OrganizationDisplay { get; set; }
 
+        public int CostCurrentNoTypeId { get; set; }
 
+
+        [Required(ErrorMessage = "*")]
+        public long BaseFee { get; set; }
+        
+        [Required(ErrorMessage = "*")]
+        public long LastYearFee { get; set; }
+
+
+        public IEnumerable<SelectListItem> CostCurrentNOTypeSource { get; set; }
+
+        public string CostCurrentNOTypeTitle
+        {
+            get
+            {
+                if(CostCurrentNOTypeSource == null || !CostCurrentNOTypeSource.Any())
+                    return string.Empty;
+
+                return CostCurrentNOTypeSource.FirstOrDefault(x => x.Value.ToString() == CostCurrentNoTypeId.ToString()).Text;
+            }
+        }
+
+    }
+
+    public class UpdateCostCurrentNOViewModel : CreateCostCurrentNOViewModelViewModel
+    {
+        public int Id { get; set; }
+
+        public long ForcastFee { get; set; }
     }
 }
