afcdc9499e73df1e551c07dd5f0463f7f24ded89
Your Name <you@example.com>
2022-04-03 18:17:06 +0430

    CostCurrent CreateViewModel

---
+++ src/Datiss.Budget.ViewModels/CostCurrentNOViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Datiss.Budget.ViewModels
+{
+    public class CreateCostCurrentNOViewModelViewModel : BaseViewModel
+    {
+        public int YearId { get; set; }
+
+        public string YearDisplay { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string OrganizationDisplay { get; set; }
+
+
+    }
+}
