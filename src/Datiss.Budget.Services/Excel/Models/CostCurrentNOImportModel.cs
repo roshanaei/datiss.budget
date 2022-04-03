175572e229e1602ffaf0f12ddfcf01f19cb1c00a
Your Name <you@example.com>
2022-04-03 17:46:40 +0430

    Add CostCurrent Service

---
+++ src/Datiss.Budget.Services/Excel/Models/CostCurrentNOImportModel.cs
@@ -0,0 +1,26 @@
+﻿using Ganss.Excel;
+
+namespace Datiss.Budget.Services.Excel
+{
+    public class CostCurrentNOImportModel
+    {
+
+        [Column(MappingDirections.Both, Letter = "A")]
+        public string OrganizationDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "B")]
+        public int OrganizationId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public string CostCurrentNOTitle { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "D")]
+        public int CostCurrentNOTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "E")]
+        public long BaseFee { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "F")]
+        public long LastYearFee { get; set; }
+    }
+}
