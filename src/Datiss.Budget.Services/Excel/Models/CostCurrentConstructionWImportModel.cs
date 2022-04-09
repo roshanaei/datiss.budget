4feabaf0f9943428d295847ba2606621bc262bba
erfaneftekhari <Erfan.Eftekharizade@Gmail.com>
2022-04-09 11:10:42 +0430

    Complete Interface

---
+++ src/Datiss.Budget.Services/Excel/Models/CostCurrentConstructionWImportModel.cs
@@ -0,0 +1,52 @@
+﻿using Ganss.Excel;
+
+namespace Datiss.Budget.Services.Excel
+{
+    public class CostCurrentConstructionWImportModel
+    {
+        [Column(MappingDirections.Both, Letter = "A")]
+        public string OrganizationDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "B")]
+        public int OrganizationId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public string ProjectDescription { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public int WaterInvestorsTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public int CostCenterTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public int ExploitationAreaTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public int ProgressPercent { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public long CostDone { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public int Amount { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public int MeasurementTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public long UnitPrice { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public long TotalCost { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public int CreditTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public int ExtensionTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public int SuggestedBudgetTopicTypeId { get; set; }
+    }
+}
---
+++ src/Datiss.Budget.Services/Models/CostCurrentConstructionWDTO.cs
@@ -0,0 +1,85 @@
+﻿namespace Datiss.Budget.Services.Models
+{
+    public class CreateCostCurrentConstructionWDTO
+    {
+        public int YearId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string ProjectDescription { get; set; }
+
+        public int WaterInvestorsTypeId { get; set; }
+
+        public int CostCenterTypeId { get; set; }
+
+        public int ExploitationAreaTypeId { get; set; }
+
+        public int ProgressPercent { get; set; }
+
+        public long CostDone { get; set; }
+
+        public int Amount { get; set; }
+
+        public int MeasurementTypeId { get; set; }
+
+        public long UnitPrice { get; set; }
+
+        public long TotalCost { get; set; }
+
+        public int CreditTypeId { get; set; }
+
+        public int ExtensionTypeId { get; set; }
+
+        public int SuggestedBudgetTopicTypeId { get; set; }
+
+    }
+
+    public class UpdateCostCurrentConstructionWDTO : CreateCostCurrentConstructionWDTO
+    {
+        public int Id { get; set; }
+
+    }
+
+    public class CostCurrentConstructionWDTO
+    {
+        public int Id { get; set; }
+        public int YearId { get; set; }
+
+        public int Year { get; set; }
+        public int OrganizationId { get; set; }
+
+        public string OrganizationDisplay { get; set; }
+        public string ProjectDescription { get; set; }
+
+        public int WaterInvestorsTypeId { get; set; }
+        public string WaterInvestorsDisplay { get; set; }
+
+        public int CostCenterTypeId { get; set; }
+        public string CostCenterDisplay { get; set; }
+
+        public int ExploitationAreaTypeId { get; set; }
+        public string ExploitationAreaDisplay { get; set; }
+
+        public int ProgressPercent { get; set; }
+
+        public long CostDone { get; set; }
+
+        public int Amount { get; set; }
+
+        public int MeasurementTypeId { get; set; }
+        public string MeasurementDisplay { get; set; }
+
+        public long UnitPrice { get; set; }
+
+        public long TotalCost { get; set; }
+
+        public int CreditTypeId { get; set; }
+        public string CreditDisplay { get; set; }
+
+        public int ExtensionTypeId { get; set; }
+        public string ExtensionDisplay { get; set; }
+
+        public int SuggestedBudgetTopicTypeId { get; set; }
+        public string SuggestedBudgetTopicDisplay { get; set; }
+    }
+}
