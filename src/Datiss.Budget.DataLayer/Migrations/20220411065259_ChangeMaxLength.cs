303e158d575283f860174e13eced81de9f2a7655
Erfan <58788132+ErfanEftekhari@users.noreply.github.com>
2022-04-11 13:53:40 +0430

    Erfan/change (#201)

---
+++ src/Datiss.Budget.DataLayer/Migrations/20220411065259_ChangeMaxLength.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Datiss.Budget.DataLayer.Migrations
+{
+    public partial class ChangeMaxLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ConstantKey",
+                table: "Constants",
+                type: "nvarchar(75)",
+                maxLength: 75,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ConstantKey",
+                table: "Constants",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(75)",
+                oldMaxLength: 75);
+        }
+    }
+}
