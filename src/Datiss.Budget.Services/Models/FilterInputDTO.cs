ae89e0ec04eb7b9e6e4345c920d9cabaf95aa21a
erfaneftekhari <Erfan.Eftekharizade@Gmail.com>
2022-04-11 11:50:27 +0430

    Complte DTO

--- src/Datiss.Budget.Services/Models/FilterInputDTO.cs
+++ src/Datiss.Budget.Services/Models/FilterInputDTO.cs
@@ -338,4 +338,11 @@ namespace Datiss.Budget.Services.Models
         public int? OrganizationId { get; set; }
 
     }
+
+    public class CostForcastTransferWFilterDTO : FilterInputDTO
+    {
+        public int? YearId { get; set; }
+        public int? OrganizationId { get; set; }
+
+    }
 }
