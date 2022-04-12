9095a7544a4ce1ced6b09aaac5af402bd9eb6b11
Your Name <you@example.com>
2022-04-12 16:08:59 +0430

    add CostForcastBuy

---
+++ src/Datiss.Budget.Entities/DWH/CostForcastBuy.cs
@@ -0,0 +1,29 @@
+﻿using Datiss.Budget.Entities.AuditableEntity;
+using Datiss.Budget.Enum;
+
+namespace Datiss.Budget.Entities.DWH
+{
+    public class CostForcastBuy : IAuditableEntity
+    {
+        public CostForcastBuy() { }
+
+        #region Properties
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+
+        public int  OrganizationId { get; set; }
+
+        public string BuyDescription { get; set; }  
+
+        public int BuyOrganizationId { get; set; }
+
+        public int CostCenterTypeId { get; set; }
+
+
+        #endregion
+
+        #region Navigation
+        #endregion
+    }
+}
