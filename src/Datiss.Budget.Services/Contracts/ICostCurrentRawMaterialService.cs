98dc9c90e6b7a59e5981db248fa5350d64e4e3ff
erfaneftekhari <Erfan.Eftekharizade@Gmail.com>
2022-04-16 11:01:38 +0430

    Complete Interface

---
+++ src/Datiss.Budget.Services/Contracts/ICostCurrentRawMaterialService.cs
@@ -0,0 +1,32 @@
+﻿using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Services.Infrastructure;
+using Datiss.Budget.Services.Models;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services.Contracts
+{
+    public interface ICostCurrentRawMaterialService
+    {
+        Task<CostCurrentInstalation> GetByIdAsync(int id);
+
+        Task<ValidationResult<CostCurrentInstalationDTO>> CreateAsync(CreateCostCurrentInstalationDTO model);
+
+        Task<ValidationResult<CostCurrentInstalationDTO>> UpdateAsync(UpdateCostCurrentInstalationDTO model);
+
+        Task HardDeleteAsync(int Id);
+
+        Task<OrganizationDeleteDataResult> HardDeleteAsync(int yearId, int organizationId);
+
+        Task<IEnumerable<CalculationItemData>> CalculationAsync(int yearId, int organizationId);
+
+        Task<PagedResult<CostCurrentInstalationDTO>> GetListAsync(CostCurrentInstalationFilterDTO filter);
+
+        Task CopyAsync(int sourceYearId, int sourceOrgId, int destYearId);
+
+        Task<IEnumerable<CostCurrentInstalationDTO>> GetExportItemsAsync(int yearId, int organizationId);
+
+        Task<ImportResult> ImportExcelAsync(IFormFile fileInfo, int yearId, bool continueIfAnyOrgMissing = false);
+    }
+}
---
+++ src/Datiss.Budget.Services/Models/CostCurrentRawMaterialDTO.cs
@@ -0,0 +1,57 @@
+﻿using Datiss.Budget.Enum;
+
+namespace Datiss.Budget.Services.Models
+{
+    public class CreateCostCurrentRawMaterialDTO
+    {
+        public int YearId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public ActivityType ActivityType { get; set; }
+
+        public int RawMaterialTypeTypeId { get; set; }
+
+        public string RawMaterialTypeTypeDisplay { get; set; }
+
+        public long BaseFee { get; set; }
+
+        public long LastYearFee { get; set; }
+
+    }
+
+    public class UpdateCostCurrentRawMaterialDTO : CreateCostCurrentRawMaterialDTO
+    {
+        public int Id { get; set; }
+        public long ForcastFee { get; set; }
+
+    }
+
+    public class CostCurrentRawMaterialDTO
+    {
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+
+        public int Year { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string OrganizationDisplay { get; set; }
+
+        public ActivityType ActivityType { get; set; }
+
+        public string ActivityTypeDisplay { get; set; }
+
+        public int RawMaterialTypeTypeId { get; set; }
+
+        public string RawMaterialTypeTypeDisplay { get; set; }
+
+        public long BaseFee { get; set; }
+
+        public long LastYearFee { get; set; }
+
+        public long ForcastFee { get; set; }
+    }
+
+}
