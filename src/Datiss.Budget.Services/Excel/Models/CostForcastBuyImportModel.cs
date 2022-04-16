5c07e62e11941642a2e5cbe8db530eb88533d24e
erfaneftekhari <Erfan.Eftekharizade@Gmail.com>
2022-04-16 16:52:37 +0430

    Complete Service

---
+++ src/Datiss.Budget.Services/Excel/Models/CostForcastBuyImportModel.cs
@@ -0,0 +1,48 @@
+﻿using Ganss.Excel;
+
+namespace Datiss.Budget.Services.Excel
+{
+    public class CostForcastBuyImportModel
+    {
+        [Column(MappingDirections.Both, Letter = "A")]
+        public string OrganizationDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "B")]
+        public int OrganizationId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public int LocationId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "D")]
+        public string BuyDescription { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "E")]
+        public int BuyDepartmentId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "F")]
+        public int CostCenterTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "G")]
+        public int AssetTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "H")]
+        public int AssetDetailTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "I")]
+        public int Amount { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "J")]
+        public int MeasurementTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "K")]
+        public long UnitPrice { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "L")]
+        public int CreditTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "M")]
+        public long ProposedCost { get; set; }
+
+
+    }
+}
