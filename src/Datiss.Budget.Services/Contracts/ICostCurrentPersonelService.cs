0fa5242fc67f0085072e865e0fb877712ac16aa7
Erfan <58788132+ErfanEftekhari@users.noreply.github.com>
2022-04-17 13:08:49 +0430

    Erfan/personnel (#210)

---
+++ src/Datiss.Budget.Services/Contracts/ICostCurrentPersonelService.cs
@@ -0,0 +1,24 @@
+﻿using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Enum;
+using Datiss.Budget.Services.Infrastructure;
+using Datiss.Budget.Services.Models;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services
+{
+    public interface ICostCurrentPersonelService
+    {
+        Task<CostCurrentPersonel> GetByIdAsync(int id);
+        Task<ValidationResult<CostCurrentPersonelDTO>> UpdateAsync(UpdateCostCurrentPersonelDTO model);
+        Task<OrganizationDeleteDataResult> HardDeleteAsync(int yearId, int organizationId, RecordType recordType);
+        Task HardDeleteAsync(int Id);
+        Task<PagedResult<CostCurrentPersonelDTO>> GetListAsync(CostCurrentPersonelFilterDTO filter);
+        Task CopyAsync(int sourceYearId, int sourceOrgId, int destYearId);
+        Task<ValidationResult> CalculationAsync(int yearId, int organizationId);
+        Task<IEnumerable<CostCurrentPersonelDTO>> GetExportItemsAsync(int yearId, int organizationId);
+        Task<ImportResult> ImportExcelAsync(IFormFile fileInfo, int yearId, bool continueIfAnyOrgMissing = false);
+        Task<IEnumerable<CostCurrentPersonelDTO>> GetLastYearBaseItemsAsync(int yearId);
+    }
+}
