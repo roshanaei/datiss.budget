dcfc912f6e0da9d67ed4eac07e14e61c26c475a7
Erfan <erfan.eftekharizade@gmail.com>
2022-04-24 11:03:35 +0430

    Complete View

--- src/Datiss.Budget.IocConfig/AddDatissBudgetServicesExtensions.cs
+++ src/Datiss.Budget.IocConfig/AddDatissBudgetServicesExtensions.cs
@@ -80,6 +80,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddScoped<ICostForcastConstructionWsService, CostForcastConstructionWsService>();
             services.AddScoped<ICostCurrentRawMaterialService, CostCurrentRawMaterialService>();
             services.AddScoped<ICostCurrentPersonelService, CostCurrentPersonelService>();
+            services.AddScoped<IBudgetSourceReportService, BudgetSourceReportService>();
 
           
             return services;
