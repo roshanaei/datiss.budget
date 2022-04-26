51c7fa1afaeae528f981d32f5ddb5b86529e6462
erfaneftekhari <Erfan.Eftekharizade@Gmail.com>
2022-04-26 15:06:38 +0430

    Complete View

--- src/Datiss.Budget.Services/Excel/Models/IncomeCurrentWsNHImportModel.cs
+++ src/Datiss.Budget.Services/Excel/Models/IncomeCurrentWsNHImportModel.cs
@@ -21,49 +21,5 @@ namespace Datiss.Budget.Services.Excel.Models
         [Column(MappingDirections.Both, Letter = "D")]
         public int UserTypeId { get; set; }
 
-        [Column(MappingDirections.Both, Letter = "E")]
-        public int NumberUser { get; set; }
-
-        [Column(MappingDirections.Both, Letter = "F")]
-        public int UnitUser { get; set; }
-
-        [Column(MappingDirections.Both, Letter = "G")]
-        public decimal AvgConsumeUser { get; set; }
-
-        [Column(MappingDirections.Both, Letter = "H")]
-        public decimal Capacity { get; set; }
-
-        [Column(MappingDirections.Both, Letter = "I")]
-        public int ConsumptionUser { get; set; }
-
-        [Column(MappingDirections.Both, Letter = "J")]
-        public int Cost { get; set; }
-
-        [Column(MappingDirections.Both, Letter = "K")]
-        public int Income { get; set; }
-
-        [Column(MappingDirections.Both, Letter = "L")]
-        public int SubscriptionIncome { get; set; }
-
-        [Column(MappingDirections.Both, Letter = "M")]
-        public int ExcessIncome { get; set; }
-
-        [Column(MappingDirections.Both, Letter = "N")]
-        public int SeasonalIncome { get; set; }
-
-        [Column(MappingDirections.Both, Letter = "O")]
-        public int Note3Price { get; set; }
-
-        [Column(MappingDirections.Both, Letter = "P")]
-        public int Note3Income { get; set; }
-
-        [Column(MappingDirections.Both, Letter = "Q")]
-        public int TotalIncome { get; set; }
-
-        [Column(MappingDirections.Both, Letter = "R")]
-        public int Note7Price { get; set; }
-
-        [Column(MappingDirections.Both, Letter = "S")]
-        public int Note7Income { get; set; }
     }
 }
