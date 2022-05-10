24156f8c920f1b7f92c7039dc41a4340d7970c41
erfaneftekhari <Erfan.Eftekharizade@Gmail.com>
2022-05-10 17:36:52 +0430

    Complete Service

---
+++ src/Datiss.Budget.Reports/Excel/CostCurrentWaterSourcePriceExcel.cs
@@ -0,0 +1,91 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using ClosedXML.Excel;
+using Datiss.Budget.Services.Models;
+
+namespace Datiss.Budget.Reports.Excel
+{
+
+    public static class CostCurrentWaterSourcePriceExcel
+    {
+        private const string _sheetName = "CostCurrentWaterSourcePrice";
+
+        public static XLWorkbook ExportExcel(this IEnumerable<CostCurrentWaterSourcePriceDTO> items) {
+            if(items == null || !items.Any())
+                return null;
+
+            var workbook = new XLWorkbook();
+            var sheet = workbook.Worksheets.Add(_sheetName);
+
+            sheet.RightToLeft = true;
+            sheet.Cell(1, 1).Value = "سال";
+            sheet.Cell(1, 2).Value = "سازمان";
+            sheet.Cell(1, 3).Value = "منبع تولید";
+            sheet.Cell(1, 4).Value = "قیمت";
+
+            var totalCount = items.Count();
+            int row = 2;
+            for(int i = 0; i < totalCount; i++) {
+                var item = items.ElementAt(i);
+                sheet.Cell(row, 1).Value = item.Year.ToString();
+                sheet.Cell(row, 2).Value = item.OrganizationDisplay;
+                sheet.Cell(row, 3).Value = item.WaterSourceTypeDisplay;
+                sheet.Cell(row, 3).DataType = XLDataType.Text;
+                sheet.Cell(row, 4).Value = item.Price;
+                sheet.Cell(row, 4).Style.NumberFormat.Format = ConstantReport.__NumberFormat;
+                sheet.Cell(row, 4).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+                row++;
+            }
+            var range = sheet.Range(1, 1, row - 1, 4);
+            var table = range.CreateTable($"{_sheetName}_Table");
+            table.Theme = XLTableTheme.TableStyleMedium16;
+            sheet.Columns().AdjustToContents();
+
+            return workbook;
+        }
+
+        public static XLWorkbook GetImportTemplate(this IEnumerable<CostCurrentWaterSourcePriceDTO> items, int year) {
+            if (items == null || !items.Any())
+                return null;
+
+            var workbook = new XLWorkbook();
+            var sheet = workbook.Worksheets.Add(_sheetName);
+
+            sheet.RightToLeft = true;
+            sheet.Cell(1, 1).Value = "ورود اطلاعات برای سال مالی : " + year;
+            sheet.Range(1, 1, 1, 5).Merge();
+
+            sheet.Cell(2, 1).Value = "عنوان سازمان";
+            sheet.Cell(2, 2).Value = "کد سازمان";
+            sheet.Cell(2, 3).Value = "منبع تولید";
+            sheet.Cell(2, 4).Value = "کد منبع";
+            sheet.Cell(2, 5).Value = "قیمت";
+
+            var totalCount = items.Count();
+            int row = 3;
+            for (int i = 0; i < totalCount; i++) {
+                var item = items.ElementAt(i);
+                sheet.Cell(row, 1).Value = item.OrganizationDisplay;
+                sheet.Cell(row, 2).Value = item.OrganizationId;
+                sheet.Cell(row, 3).Value = item.WaterSourceTypeDisplay;
+                sheet.Cell(row, 4).Value = item.WaterSourceTypeId;
+                row++; //for keeping index in table records
+            }
+
+            var range = sheet.Range(2, 1, row - 1, 5);
+            range.Column(4).Style.NumberFormat.Format = ConstantReport.__NumberFormat;
+            range.Column(3).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Right;
+            //Other
+            range.Column(5).Style.NumberFormat.Format = ConstantReport.__NumberFormat;
+            range.Column(5).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            //
+            var table = range.CreateTable($"{_sheetName}_Table");
+            table.Theme = XLTableTheme.TableStyleMedium16;
+            sheet.Columns().AdjustToContents();
+
+            return workbook;
+        }
+
+    }
+}
