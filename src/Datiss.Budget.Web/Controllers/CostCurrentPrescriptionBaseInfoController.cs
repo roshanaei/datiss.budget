1c060a3249a0e382732433d3f728d1a5061543f4
erfaneftekhari <Erfan.Eftekharizade@Gmail.com>
2022-05-11 13:12:13 +0430

    Complete Service

---
+++ src/Datiss.Budget.Web/Controllers/CostCurrentPrescriptionBaseInfoController.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+using Mapster;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Datiss.Budget.Services.Contracts;
+using Datiss.Budget.Services.Models;
+using Datiss.Budget.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Datiss.Budget.Services.Identity;
+using Datiss.Budget.Services.Contracts.Identity;
+using Microsoft.AspNetCore.Http;
+using Datiss.Budget.Common.Exceptions;
+using Microsoft.AspNetCore.Hosting;
+using Datiss.Budget.Common.GuardToolkit;
+using Datiss.Budget.Web.Helpers;
+using Datiss.Budget.Resources;
+using ClosedXML.Extensions;
+using Datiss.Budget.Reports.Excel;
+using Microsoft.Extensions.Logging;
+using Datiss.Budget.Common;
+using Datiss.Budget.Enum;
+using Datiss.Budget.Security;
+
+namespace Datiss.Budget.Web.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    public class CostCurrentPrescriptionBaseInfoController : Controller
+    {
+        public const string Name = "CostCurrentPrescriptionBaseInfo";
+        public const string ACTION_Create = nameof(Create);
+        public const string ACTION_Index = nameof(Index);
+        public const string ACTION_Edit = nameof(Edit);
+        public const string ACTION_Copy = nameof(Copy);
+        public const string ACTION_Delete = nameof(Delete);
+        public const string ACTION_DeleteRecords = nameof(DeleteRecords);
+        public const string ACTION_ImportExcel = nameof(ImportExcel);
+        public const string ACTION_ExportExcel = nameof(ExportExcel);
+        public const string ACTION_GetExcelTemplate = nameof(GetExcelTemplate);
+
+
+        private string _indexFilterKey = $"{Name}_{ACTION_Index}_filter";
+
+        private readonly ILogger<CostCurrentPrescriptionBaseInfoController> _logger;
+        private readonly IWebHostEnvironment _env;
+        private readonly ICostCurrentPrescriptionBaseInfoService _costCurrentPrescriptionBaseInfoService;
+        private readonly IConstantService _constantService;
+        private readonly IFinanceYearService _financeYearService;
+        private readonly ISecurityTrimmingService _securityTrimmingService;
+
+        public CostCurrentPrescriptionBaseInfoController(
+            ILogger<CostCurrentPrescriptionBaseInfoController> logger,
+            IWebHostEnvironment environment,
+            ICostCurrentPrescriptionBaseInfoService costCurrentPrescriptionBaseInfoService,
+            IFinanceYearService financeYearService,
+            IConstantService constantService,
+            ISecurityTrimmingService securityTrimmingService)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _env = environment ?? throw new ArgumentNullException(nameof(environment));
+            _costCurrentPrescriptionBaseInfoService = costCurrentPrescriptionBaseInfoService ?? throw new ArgumentNullException(nameof(costCurrentPrescriptionBaseInfoService));
+            _financeYearService = financeYearService ?? throw new ArgumentNullException(nameof(financeYearService));
+            _constantService = constantService ?? throw new ArgumentNullException(nameof(constantService));
+            _securityTrimmingService = securityTrimmingService ?? throw new ArgumentNullException(nameof(securityTrimmingService));
+        }
+
+
+        [HttpPost("[action]")]
+        [HasPermission(claimType: Name, PermissionActionType.Create)]
+        public async Task<IActionResult> Create(CreateCostCurrentPrescriptionBaseInfoViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.AddError(ViewMessages.InvalidData);
+                return Json(model);
+            }
+            var data = model.Adapt<CreateCostCurrentPrescriptionBaseInfoDTO>();
+
+            var result = await _costCurrentPrescriptionBaseInfoService.CreateAsync(data);
+
+            if (!result.IsValid)
+            {
+                model.AddError(result.Message);
+                return Json(model);
+            }
+
+            return Json(result.Result.Adapt<CostCurrentPrescriptionBaseInfoViewModel>());
+        }
+
+        [HttpPost("[action]")]
+        [HasPermission(claimType: Name, PermissionActionType.Edit)]
+        public async Task<IActionResult> Edit(UpdateCostCurrentPrescriptionBaseInfoViewModel model)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                model.AddError(ViewMessages.InvalidData);
+                return Json(model);
+            }
+
+            var data = model.Adapt<UpdateCostCurrentPrescriptionBaseInfoDTO>();
+            var result = await _costCurrentPrescriptionBaseInfoService.UpdateAsync(data);
+
+            if (!result.IsValid)
+            {
+                model.AddError(result.Message);
+                return Json(model);
+            }
+
+            return Json(
+                result.Result.Adapt<CostCurrentPrescriptionBaseInfoViewModel>()
+            );
+        }
+
+        [HttpGet("{page?}")]
+        [HasPermission(claimType: Name, PermissionActionType.List)]
+        public async Task<IActionResult> Index(int page = 1)
+        {
+            var filter = new CostCurrentPrescriptionBaseInfoFilterDTO();
+
+            var yearSource = (await _financeYearService.GetDropDownDataAsync())
+                .Adapt<IEnumerable<DropDownItemViewModel>>();
+            int maxYear = yearSource.Max(_ => _.Id);
+
+            var userSource = (await _constantService.GetByConstantKeyAsync(ConstantKeys.__UserType))
+                .Adapt<IEnumerable<DropDownItemViewModel>>();
+
+            filter.YearId = maxYear;
+
+            var myfilter = TempData.Get<CostCurrentPrescriptionBaseInfoFilterViewModel>(_indexFilterKey);
+            if (myfilter != null)
+            {
+                filter = myfilter.Adapt<CostCurrentPrescriptionBaseInfoFilterDTO>();
+                TempData.Put(_indexFilterKey, myfilter);
+            }
+
+            filter.PageNumber = page;
+
+            var result = await _costCurrentPrescriptionBaseInfoService.GetListAsync(filter);
+            var model = result.Adapt<CostCurrentPrescriptionBaseInfoIndexViewModel>();
+
+            model.SetYearSource(yearSource);
+
+            model.SetFinanceYearFilterSource(yearSource, filter.YearId);
+
+            model.Filter.YearId = filter.YearId;
+            model.Filter.PageNumber = filter.PageNumber;
+            model.Filter.PageSize = filter.PageSize;
+
+            return View(model);
+        }
+
+        [HttpPost("{page?}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(CostCurrentPrescriptionBaseInfoIndexViewModel model)
+        {
+
+            var filter = model.Filter.Adapt<CostCurrentPrescriptionBaseInfoFilterDTO>();
+
+            TempData.Put(_indexFilterKey, filter);
+
+            var result = await _costCurrentPrescriptionBaseInfoService.GetListAsync(filter);
+            
+            model = result.Adapt<CostCurrentPrescriptionBaseInfoIndexViewModel>();
+            
+            model.Filter = filter.Adapt<CostCurrentPrescriptionBaseInfoFilterViewModel>();
+
+            var yearSource = (await _financeYearService.GetDropDownDataAsync())
+                .Adapt<IEnumerable<DropDownItemViewModel>>();
+
+            var userSource = (await _constantService.GetByConstantKeyAsync(ConstantKeys.__UserType))
+                .Adapt<IEnumerable<DropDownItemViewModel>>();
+
+            model.SetYearSource(yearSource);
+            model.SetFinanceYearFilterSource(yearSource, filter.YearId);
+
+            return View(model);
+        }
+
+        [HttpPost("[action]")]
+        [HasPermission(claimType: Name, PermissionActionType.Create)]
+        public async Task<IActionResult> ImportExcel(ImportExcelViewModel model)
+        {
+            model.CheckArgumentIsNull(nameof(model));
+
+            if (model.ExcelFile == null || model.ExcelFile.Length == 0)
+                return Json(new
+                {
+                    hasError = true,
+                    message = ViewMessages.ImportExcelInvalidFile
+                });
+
+            try
+            {
+                var result = await _costCurrentPrescriptionBaseInfoService.ImportExcelAsync(model.ExcelFile,model.YearId);
+
+                if (result.Success)
+                {
+                    return Json(new
+                    {
+                        hasError = false,
+                        message = result.Message
+                    });
+
+                }
+                else
+                {
+                    return Json(new
+                    {
+                        hasError = true,
+                        message = result.Message
+                    });
+                }
+            }
+            catch (ImportExcelFileFormatInvalidException)
+            {
+                return Json(new
+                {
+                    hasError = true,
+                    message = ViewMessages.ImportExcelFileFormatInvalid
+                });
+            }
+            catch (ImportExcelFileSizeInvalidException)
+            {
+                return Json(new
+                {
+                    hasError = true,
+                    message = ViewMessages.ImportExcelFileSizeInvalid
+                });
+            }
+
+        }
+
+        [HttpPost("records/delete")]
+        [HasPermission(claimType: Name, PermissionActionType.Delete)]
+        public async Task<IActionResult> DeleteRecords(int yearId)
+        {
+            try
+            {
+                var result = await _costCurrentPrescriptionBaseInfoService.HardDeleteAllAsync(yearId);
+
+                return Json(new
+                {
+                    success = true,
+                    message = string.Format(
+                        ViewMessages.DeleteMultipleDataForYear,
+                        result.Year)
+                });
+            }
+            catch (DisbaledYearDataInputException)
+            {
+                return Json(new
+                {
+                    hasError = true,
+                    message = ViewMessages.Logic_InputDisableYearData
+                });
+            }
+            catch (DeleteNullRecordException)
+            {
+                return Json(new
+                {
+                    hasError = true,
+                    message = ViewMessages.DeleteNullRecord
+                });
+            }
+            catch (NullReferenceException)
+            {
+                return Json(new
+                {
+                    hasError = true,
+                    message = ViewMessages.NullRef
+                });
+            }
+        }
+
+        [HttpPost("[action]/{id}")]
+        [HasPermission(claimType: Name, PermissionActionType.Delete)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _costCurrentPrescriptionBaseInfoService.HardDeleteAsync(id);
+            }
+            catch (DisbaledYearDataInputException)
+            {
+                return Json(new
+                {
+                    hasError = true,
+                    message = ViewMessages.Logic_InputDisableYearData
+                });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.GetBaseException().Message);
+                return Json(new
+                {
+                    hasError = true,
+                    message = ViewMessages.InvalidUpdateData
+                });
+            }
+
+            return Json(new
+            {
+                hasError = false,
+                message = ViewMessages.DeleteRowSuccess
+            });
+        }
+
+        [HttpGet("[action]")]
+        [HasPermission(claimType: Name, PermissionActionType.Create)]
+        public async Task<IActionResult> Copy()
+        {
+            var model = new CopyViewModel();
+
+            model.SetYearSource(
+                (await _financeYearService.GetDropDownDataByStatusAsync(EntityStatus.Disbaled))
+                    .Adapt<IEnumerable<DropDownItemViewModel>>()
+            );
+
+            model.SetTargetYearSource(
+                (await _financeYearService.GetDropDownDataByStatusAsync(EntityStatus.Enabled))
+                    .Adapt<IEnumerable<DropDownItemViewModel>>()
+            );
+
+            return PartialView("_copyModal", model);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Copy(CopyViewModel model)
+        {
+            model.CheckArgumentIsNull(nameof(model));
+
+            try
+            {
+                await _costCurrentPrescriptionBaseInfoService.CopyAsync(model.SourceYearId, model.TargetYearId);
+
+                model.Succeed(ViewMessages.CopySuccess);
+            }
+            catch (CopySameYearException)
+            {
+                model.AddError(ViewMessages.CopySameYear);
+            }
+            catch (CopyDestYearExxeption)
+            {
+                model.AddError(ViewMessages.CopyErrorDestYear);
+            }
+            catch (CopyOrgNullDataException)
+            {
+                model.AddError(ViewMessages.CopySourceOrgNullData);
+            }
+            catch (CopyDestYearHasDataException)
+            {
+                model.AddError(ViewMessages.CopyDestYearHasData);
+            }
+            catch (CopyDataBaseException)
+            {
+                model.AddError(ViewMessages.CalculationField);
+            }
+            catch (Exception)
+            {
+                model.AddError(ViewMessages.SystemError);
+            }
+
+            return Json(model);
+        }
+
+        [HttpGet("import/template/{yearId}")]
+        public async Task<IActionResult> GetExcelTemplate(int yearId)
+        {
+            var year = await _financeYearService.GetByIdAsync(yearId);
+
+            using var workbook = year.Year.GetImportTemplate();
+            return workbook.Deliver("CostCurrentPrescriptionBaseInfo-Import-Template.xlsx");
+        }
+
+        [HttpGet("[action]/{yearid}")]
+        public async Task<IActionResult> ExportExcel(int yearid)
+        {
+            var result = await _costCurrentPrescriptionBaseInfoService.GetExportItemsAsync(yearid);
+            if (result.Count() == 0)
+                return RedirectToAction("Index");
+            using var workbook = result.ExportExcel();
+            return workbook.Deliver("CostCurrentPrescriptionBaseInfo.xlsx");
+        }
+    }
+}
