608e55e73dcd387397251680edac86c1d73ef083
Your Name <you@example.com>
2022-05-22 16:04:30 +0430

    Add CostForcastPipingW ViewModel

---
+++ src/Datiss.Budget.Web/Controllers/CostForcastPipingWController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Datiss.Budget.Web.Controllers
+{
+    public class CostForcastPipingWController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
