cd528da69dab131e71b4fccb0fdf45cb5d0a5be3
Your Name <you@example.com>
2022-05-22 11:59:44 +0430

    Add CostForcastPipingW DTO

---
+++ src/Datiss.Budget.Services/Models/CreateCostForcastPipingWDTO.cs
@@ -0,0 +1,48 @@
+﻿namespace Datiss.Budget.Services.Models
+{
+    public class CreateCostForcastPipingWDTO
+    {
+        public int YearId { get; set; }
+
+        public int TubeTypeId { get; set; }
+
+        public int DiameterPipeTypeId { get; set; }
+
+        public int DigTypeId { get; set; }
+
+        public long TubeBuyCost { get; set; }
+
+        public long RunCost { get; set; }
+
+    }
+
+    public class UpdateCostForcastPipingWDTO : CreateCostForcastPipingWDTO
+    {
+        public int Id { get; set; }
+
+    }
+
+    public class CostForcastPipingWDTO
+    {
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+        public int Year { get; set; }
+
+        public int TubeTypeId { get; set; }
+        public string TubeTypeDisplay { get; set; }
+
+        public int DiameterPipeTypeId { get; set; }
+        public string DiameterPipeTypeDisplay { get; set; }
+
+        public int DigTypeId { get; set; }
+        public string DigTypeDisplay { get; set; }
+
+        public long TubeBuyCost { get; set; }
+
+        public long RunCost { get; set; }
+
+        public long TotalCost { get; set; }
+
+    }
+}
