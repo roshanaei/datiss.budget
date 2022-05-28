f43e15dedaec1b092d1d197d1a2aae98e042f317
Your Name <you@example.com>
2022-05-28 17:40:12 +0430

    Add CostForcastBuyDescription DTO

---
+++ src/Datiss.Budget.Services/Models/CostForcastBuyDescriptionDTO.cs
@@ -0,0 +1,42 @@
+﻿namespace Datiss.Budget.Services.Models
+{
+    public class CreateCostForcastBuyDescriptionDTO
+    {
+        public int YearId { get; set; }
+
+        public int AssetTypeId { get; set; }
+
+        public int AssetDetailTypeId { get; set; }
+
+        public int MeasurementTypeId { get; set; }
+
+        public long UnitPrice { get; set; }
+
+    }
+
+    public class UpdateCostForcastBuyDescriptionDTO : CreateCostForcastBuyDescriptionDTO
+    {
+        public int Id { get; set; }
+
+    }
+
+    public class CostForcastBuyDescriptionDTO
+    {
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+        public int Year { get; set; }
+
+        public int AssetTypeId { get; set; }
+        public string AssetTypeDisplay { get; set; }
+
+        public int AssetDetailTypeId { get; set; }
+        public string AssetDetailTypeIdDisplay { get; set; }
+
+        public int MeasurementTypeId { get; set; }
+        public string MeasurementTypeDisplay { get; set; }
+
+        public long UnitPrice { get; set; }
+
+    }
+}
