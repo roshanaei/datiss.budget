655e5c6b54e1fb68dd2b7a037c71981b56d5fa64
Your Name <you@example.com>
2022-05-28 17:44:10 +0430

    Add CostForcastBuyDescription ImportModel

---
+++ src/Datiss.Budget.Services/Excel/Models/CostForcastBuyDescriptionImportModel.cs
@@ -0,0 +1,30 @@
+﻿using Ganss.Excel;
+
+namespace Datiss.Budget.Services.Excel
+{
+    public class CostForcastBuyDescriptionImportModel
+    {
+
+        [Column(MappingDirections.Both, Letter = "A")]
+        public string AssetTypeDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "B")]
+        public int AssetTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public string AssetDetailTypeDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "D")]
+        public int AssetDetailTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "E")]
+        public string MeasurementTypeDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "F")]
+        public int MeasurementTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "G")]
+        public long UnitPrice { get; set; }
+
+    }
+}
