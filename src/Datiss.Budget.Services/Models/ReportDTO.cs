1d1463b4babcd543b56a776a567134732c3108f1
Your Name <you@example.com>
2022-06-12 15:50:06 +0430

    Add category To ReportDTO

--- src/Datiss.Budget.Services/Models/ReportDTO.cs
+++ src/Datiss.Budget.Services/Models/ReportDTO.cs
@@ -11,6 +11,7 @@ namespace Datiss.Budget.Services.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public EntityStatus Status { get; set; }
+        public int CategoryTypeId { get; set; }
         public byte[] FileData { get; set; }
         public string FilePath { get; set; }
         public IList<ReportParamDTO> Params { get; set; }
@@ -35,6 +36,7 @@ namespace Datiss.Budget.Services.Models
         public string Name { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public int CategoryTypeId { get; set; }
         public byte[] FileData { get; set; }
         public IList<CreateReportParamDTO> Params { get; set; }
     }
