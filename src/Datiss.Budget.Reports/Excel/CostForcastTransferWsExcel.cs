db0ec5540a0fa6ebcd05b5f7d97606eac6aca09f
erfaneftekhari <Erfan.Eftekharizade@Gmail.com>
2022-07-03 13:49:38 +0430

    Put StoredProsedure In CostCurrentReport & Fixed Excel CostForcastTransfer

--- src/Datiss.Budget.Reports/Excel/CostForcastTransferWsExcel.cs
+++ src/Datiss.Budget.Reports/Excel/CostForcastTransferWsExcel.cs
@@ -204,11 +204,11 @@ namespace Datiss.Budget.Reports.Excel
             sheet.Cell(20, 8).Value = "کد جنس لوله";
             sheet.Cell(20, 9).Value = "کد قطر لوله";
             sheet.Cell(20, 10).Value = "طول اجرا";
-            sheet.Cell(20, 11).Value = "هزینه هر متر خرید لوله";
-            sheet.Cell(20, 12).Value = "هزینه هر متر اجرا";
-            sheet.Cell(20, 13).Value = "جمع کل هزینه ها (هزار ریال)";
-            sheet.Cell(20, 14).Value = "کد بهره برداری";
-            sheet.Cell(20, 15).Value = "کد سر فصل کلی در بودجه";
+            //sheet.Cell(20, 11).Value = "هزینه هر متر خرید لوله";
+            //sheet.Cell(20, 12).Value = "هزینه هر متر اجرا";
+            //sheet.Cell(20, 13).Value = "جمع کل هزینه ها (هزار ریال)";
+            sheet.Cell(20, 11).Value = "کد بهره برداری";
+            sheet.Cell(20, 12).Value = "کد سر فصل کلی در بودجه";
 
             var totalCount = model.Items.Count();
             row = 21;
@@ -220,7 +220,7 @@ namespace Datiss.Budget.Reports.Excel
                 row++; //for keeping index in table records
             }
 
-            var range = sheet.Range(20, 1, row - 1, 15);
+            var range = sheet.Range(20, 1, row - 1, 12);
             //range.Column(4).Style.NumberFormat.Format = "#,##0";
             //range.Column(3).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Right;
             //Other
