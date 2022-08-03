1b492ff78ea230bc26777109114fc4ac7865c60e
Your Name <you@example.com>
2022-08-03 17:22:17 +0430

    Add CreateCostCurrentPersonelViewModel Service

--- src/Datiss.Budget.Services/Models/CostCurrentPersonelDTO.cs
+++ src/Datiss.Budget.Services/Models/CostCurrentPersonelDTO.cs
@@ -19,12 +19,15 @@ namespace Datiss.Budget.Services.Models
         public bool GenderId { get; set; }
 
         public int GradeTypeId { get; set; }
+        public string GradeTypeTitle { get; set; }
 
         public int ContractTypeId { get; set; }
 
         public int JobDepartmentTypeId { get; set; }
 
         public int CostCenterTypeId { get; set; }
+        public string CostCenterTypeTitle { get; set; }
+
 
         public int JobStatusTypeId { get; set; }
 
@@ -34,84 +37,84 @@ namespace Datiss.Budget.Services.Models
 
         public int ExperienceMonth { get; set; }
 
-        //مزد ثابت سال جاری
-        public long FixSalary { get; set; }
+        ////مزد ثابت سال جاری
+        //public long FixSalary { get; set; }
 
-        //حق جذب سال جاری
-        public long EmployRight { get; set; }
+        ////حق جذب سال جاری
+        //public long EmployRight { get; set; }
 
-        //فوق العاده منطقه
-        public long RegionRight { get; set; }
+        ////فوق العاده منطقه
+        //public long RegionRight { get; set; }
 
-        //ساعت اضافه کاری
-        public int OverTimeValue { get; set; }
+        ////ساعت اضافه کاری
+        //public int OverTimeValue { get; set; }
 
-        // مبلغ اضافه کاری
-        public long OverTimeCost { get; set; }
+        //// مبلغ اضافه کاری
+        //public long OverTimeCost { get; set; }
 
-        //تعداد روز  تعطیل کاری
-        public int HolidayValue { get; set; }
+        ////تعداد روز  تعطیل کاری
+        //public int HolidayValue { get; set; }
 
-        // مبلغ تعطیل کاری
-        public long HolidayCost { get; set; }
+        //// مبلغ تعطیل کاری
+        //public long HolidayCost { get; set; }
 
-        //درصد نوبت کاری
-        public long ShiftPercent { get; set; }
-        //نوبت کاری و کشیک
-        public long ShiftPCost { get; set; }
+        ////درصد نوبت کاری
+        //public long ShiftPercent { get; set; }
+        ////نوبت کاری و کشیک
+        //public long ShiftPCost { get; set; }
 
-        // تعداد ماموریت
-        public long MissionCount { get; set; }
-        // هزینه ماموریت
-        public long MissionDayCost { get; set; }
+        //// تعداد ماموریت
+        //public long MissionCount { get; set; }
+        //// هزینه ماموریت
+        //public long MissionDayCost { get; set; }
 
-        // حق سختی کار
-        public long HardWorkingRt { get; set; }
+        //// حق سختی کار
+        //public long HardWorkingRt { get; set; }
 
-        // حق ایاب و ذهاب
-        public long TrafficRt { get; set; }
-
-
-        //حق مسکن
-        public long HouseRt { get; set; }
+        //// حق ایاب و ذهاب
+        //public long TrafficRt { get; set; }
 
 
-        // حق اولاد
-        public long ChildRt { get; set; }
+        ////حق مسکن
+        //public long HouseRt { get; set; }
 
 
-        //حق خواروبار
-        public long StuffRt { get; set; }
+        //// حق اولاد
+        //public long ChildRt { get; set; }
 
 
-        //آموزش
-        public long Education { get; set; }
+        ////حق خواروبار
+        //public long StuffRt { get; set; }
 
 
-        //بیمه سهم کارفرما
-        public long InsuranceMaster { get; set; }
-
-        // بیمه عمر و حادثه تکمیلی
-        public long InsuranceAging { get; set; }
+        ////آموزش
+        //public long Education { get; set; }
 
 
-        //عیدی سالیانه
-        public long HolidayYearly { get; set; }
+        ////بیمه سهم کارفرما
+        //public long InsuranceMaster { get; set; }
+
+        //// بیمه عمر و حادثه تکمیلی
+        //public long InsuranceAging { get; set; }
 
 
-        //ذخیره مزایای پایان خدمت کارکنان
-        public long MilitaryServiceCost { get; set; }
+        ////عیدی سالیانه
+        //public long HolidayYearly { get; set; }
 
 
-        //  سنوات
-        public long EndJobReward { get; set; }
+        ////ذخیره مزایای پایان خدمت کارکنان
+        //public long MilitaryServiceCost { get; set; }
 
 
-        //هزینه های رفاهی
-        public long WelfareCost { get; set; }
+        ////  سنوات
+        //public long EndJobReward { get; set; }
 
-        //ماه بازنشستگی
-        public int RetirementMonth { get; set; }
+
+        ////هزینه های رفاهی
+        //public long WelfareCost { get; set; }
+
+        ////ماه بازنشستگی
+        //public int RetirementMonth { get; set; }
     }
 
     public class UpdateCostCurrentPersonelDTO : CreateCostCurrentPersonelDTO
