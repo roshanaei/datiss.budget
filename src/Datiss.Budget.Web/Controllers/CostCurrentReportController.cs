e29cfd91dc4e42ab48741deb021486862c430b96
Your Name <you@example.com>
2022-09-07 15:00:11 +0430

    add Export Excell controller

--- src/Datiss.Budget.Web/Controllers/CostCurrentReportController.cs
+++ src/Datiss.Budget.Web/Controllers/CostCurrentReportController.cs
@@ -34,6 +34,7 @@ namespace Datiss.Budget.Web.Controllers
         public const string ACTION_DeleteRecords = nameof(DeleteRecords);
         public const string ACTION_ImportExcel = nameof(ImportExcel);
         public const string ACTION_Calculation = nameof(Calculation);
+        public const string ACTION_ExportExcel = nameof(ExportExcel);
         public const string ACTION_GetExcelTemplate = nameof(GetExcelTemplate);
 
         private string _indexFilterKey = $"{Name}_{ACTION_Index}_filter";
@@ -418,6 +419,16 @@ namespace Datiss.Budget.Web.Controllers
 
             using var workbook = items.GetImportTemplate(year.Year);
             return workbook.Deliver("CostCurrentReport-Import-Template.xlsx");
+        }
+
+        [HttpGet("[action]/{orgid}/{yearid}")]
+        public async Task<IActionResult> ExportExcel(int orgid, int yearid)
+        {
+            var result = await _costCurrentReportService.GetExportItemsAsync(yearid, orgid);
+            if (result.Count() == 0)
+                return RedirectToAction("Index");
+            using var workbook = result.ExportExcel();
+            return workbook.Deliver("CostForcastReports.xlsx");
         }
     }
 }
