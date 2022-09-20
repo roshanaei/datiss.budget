9507f88deee5dca433e928e97414e7ab88108090
Your Name <you@example.com>
2022-09-20 13:26:41 +0430

    Add Entity

---
+++ src/Datiss.Budget.Entities/DWH/IncomeForcastSourcesReport.cs
@@ -0,0 +1,36 @@
+﻿using Datiss.Budget.Entities.AuditableEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datiss.Budget.Enum;
+
+namespace Datiss.Budget.Entities.DWH
+{
+    public class IncomeForcastSourcesReport :IAuditableEntity
+    {
+        public IncomeForcastSourcesReport() { }
+
+        #region Properties
+        public int Id { get; set; }
+        public int YearId { get; set; }
+        public int OrganizationId { get; set; }
+        public int SourceDescriptionId { get;set; }
+        public long FunctionalBasicYear { get; set; }
+        public long FunctionalLastYear { get; set; }
+        public long ApproveYear_1 { get; set; }
+        public long PercentBudget { get; set; }
+        public long ForcastY { get; set; }
+
+        #endregion
+
+        #region navigations
+        public FinanceYear FinanceYear { get; set; }
+
+        public Organization Organization { get; set; }  
+
+        public Constant SourceDescription { get; set; }       
+        #endregion
+    }
+}
