06dde0087bd719b9edf1683e9cff8138fe24f903
Your Name <you@example.com>
2022-09-21 16:37:21 +0430

    Add DTO

---
+++ src/Datiss.Budget.Services/Models/CostCurrentProfitLossReportDTO.cs
@@ -0,0 +1,51 @@
+﻿namespace Datiss.Budget.Services.Models
+{
+    public class CreateCostCurrentProfitLossReportDTO
+    {
+        public int YearId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public int SectionTypeId { get; set; }
+
+        public string SectionTypeTitle { get; set; }
+
+        public long FunctionalYear_1 { get; set; }
+
+        public long FunctionalBasicYear { get; set; }
+
+        public long ApproveYear_1 { get; set; }
+
+        public long ForcastY { get; set; }
+    }
+
+    public class UpdateCostCurrentProfitLossReportDTO : CreateCostCurrentProfitLossReportDTO
+    {
+        public int Id { get; set; }
+    }
+
+    public class CostCurrentProfitLossReportDTO
+    {
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+
+        public int Year { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string OrganizationDisplay { get; set; }
+
+        public int SectionTypeId { get; set; }
+
+        public string SectionTypeDisplay { get; set; }
+
+        public long FunctionalYear_1 { get; set; }
+
+        public long FunctionalBasicYear { get; set; }
+
+        public long ApproveYear_1 { get; set; }
+
+        public long ForcastY { get; set; }
+    }
+}
