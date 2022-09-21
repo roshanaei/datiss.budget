dc3c1e32b9154275ad7db5fc7d63cafb482f670e
Your Name <you@example.com>
2022-09-21 16:29:02 +0430

    Add Migration

---
+++ src/Datiss.Budget.DataLayer/Migrations/20220921115513_add CostCurrentProfitLossReport.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Datiss.Budget.DataLayer.Migrations
+{
+    public partial class addCostCurrentProfitLossReport : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CostCurrentProfitLossReport",
+                columns: table => new
+                {
+                    CCPRId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    YearId = table.Column<int>(type: "int", nullable: false),
+                    OrganizationId = table.Column<int>(type: "int", nullable: false),
+                    SectionTypeId = table.Column<int>(type: "int", nullable: false),
+                    FunctionalYear_1 = table.Column<long>(type: "bigint", nullable: false),
+                    FunctionalBasicYear = table.Column<long>(type: "bigint", nullable: false),
+                    ApproveYear_1 = table.Column<long>(type: "bigint", nullable: false),
+                    ForcastY = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedByBrowserName = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
+                    CreatedByIp = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    CreatedByUserId = table.Column<int>(type: "int", nullable: true),
+                    CreatedDateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedByBrowserName = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
+                    ModifiedByIp = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    ModifiedByUserId = table.Column<int>(type: "int", nullable: true),
+                    ModifiedDateTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CostCurrentProfitLossReport", x => x.CCPRId);
+                    table.ForeignKey(
+                        name: "FK_CostCurrentProfitLossReport_Constants_SectionTypeId",
+                        column: x => x.SectionTypeId,
+                        principalTable: "Constants",
+                        principalColumn: "ConstantId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_CostCurrentProfitLossReport_FinanceYears_YearId",
+                        column: x => x.YearId,
+                        principalTable: "FinanceYears",
+                        principalColumn: "FinanceYearId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_CostCurrentProfitLossReport_Organizations_OrganizationId",
+                        column: x => x.OrganizationId,
+                        principalTable: "Organizations",
+                        principalColumn: "OrganizationId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CostCurrentProfitLossReport_OrganizationId",
+                table: "CostCurrentProfitLossReport",
+                column: "OrganizationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CostCurrentProfitLossReport_SectionTypeId",
+                table: "CostCurrentProfitLossReport",
+                column: "SectionTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CostCurrentProfitLossReport_YearId",
+                table: "CostCurrentProfitLossReport",
+                column: "YearId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CostCurrentProfitLossReport");
+        }
+    }
+}
