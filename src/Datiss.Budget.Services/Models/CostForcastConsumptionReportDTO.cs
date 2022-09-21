3e5d507877a5fb11602946f0b650f56024120874
Your Name <you@example.com>
2022-09-21 12:30:13 +0430

    Add DTO

---
+++ src/Datiss.Budget.Services/Models/CostForcastConsumptionReportDTO.cs
@@ -0,0 +1,66 @@
+﻿
+namespace Datiss.Budget.Services.Models
+{
+    public class UpdateCostForcastConsumptionReportDTO
+    {
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public int SectionTypeId { get; set; }
+
+        public string SectionTypeTitle { get; set; }
+
+        public long FunctionalYear_1 { get; set; }
+
+        public long FunctionalBasicYear { get; set; }
+
+        public long ApproveYear_1 { get; set; }
+
+        public long ForcastY { get; set; }
+
+        public decimal ReceiptPercent { get; set; }
+
+        public long Fee { get; set; }
+
+        public decimal ForcastFunctionalPercent { get; set; }
+
+        public decimal ForcastBudgetPercent { get; set; }
+
+    }
+
+    public class CostForcastConsumptionReportDTO
+    {
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+
+        public int Year { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string OrganizationDisplay { get; set; }
+
+        public int SectionTypeId { get; set; }
+
+        public string SectionTypeDisplay { get; set; }
+
+        public long FunctionalYear_1 { get; set; }
+
+        public long FunctionalBasicYear { get; set; }
+
+        public long ApproveYear_1 { get; set; }
+
+        public long ForcastY { get; set; }
+
+        public decimal ReceiptPercent { get; set; }
+
+        public long Fee { get; set; }
+
+        public decimal ForcastFunctionalPercent { get; set; }
+
+        public decimal ForcastBudgetPercent { get; set; }
+    }
+}
