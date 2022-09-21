a8fbc5aa48968f5ed32b74f18c4eaf114e630620
Your Name <you@example.com>
2022-09-21 16:51:06 +0430

    Add Excell

---
+++ src/Datiss.Budget.Reports/Excel/CostCurrentProfitLossReportExcel.cs
@@ -0,0 +1,107 @@
+﻿using ClosedXML.Excel;
+using Datiss.Budget.Services.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Datiss.Budget.ViewModels;
+
+namespace Datiss.Budget.Reports.Excel
+{
+    public static class CostCurrentProfitLossReportExcel
+    {
+        private const string _sheetName = "CostCurrentProfitLossReport";
+
+        public static XLWorkbook ExportExcel(this IEnumerable<CostCurrentProfitLossReportDTO> items)
+        {
+            if (items == null || !items.Any())
+                return null;
+
+            var workbook = new XLWorkbook();
+            var sheet = workbook.Worksheets.Add(_sheetName);
+
+            sheet.RightToLeft = true;
+
+            sheet.Cell(1, 1).Value = "سال";
+            sheet.Cell(1, 2).Value = "سازمان";
+            sheet.Cell(1, 3).Value = "شرح";
+            sheet.Cell(1, 4).Value = "پیش بینی سال بودجه";
+            sheet.Cell(1, 5).Value = "عملکرد سال پایه";
+            sheet.Cell(1, 6).Value = "عملکرد سال  ما قبل";
+            sheet.Cell(1, 7).Value = "مصوب سال ما قبل";
+
+            var totalCount = items.Count();
+            int row = 2;
+            for (int i = 0; i < totalCount; i++)
+            {
+                var item = items.ElementAt(i);
+                sheet.Cell(row, 1).Value = item.Year.ToString();
+                sheet.Cell(row, 2).Value = item.OrganizationDisplay;
+                sheet.Cell(row, 3).Value = item.SectionTypeDisplay;
+                sheet.Cell(row, 4).Value = item.ForcastY;
+                sheet.Cell(row, 4).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Right;
+                sheet.Cell(row, 5).Value = item.FunctionalBasicYear;
+                sheet.Cell(row, 5).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                sheet.Cell(row, 6).Value = item.FunctionalYear_1;
+                sheet.Cell(row, 6).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                sheet.Cell(row, 7).Value = item.ApproveYear_1;
+                sheet.Cell(row, 7).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+
+                row++;
+            }
+            var range = sheet.Range(1, 1, row - 1, 7);
+            var table = range.CreateTable($"{_sheetName}_Table");
+            table.Theme = XLTableTheme.TableStyleMedium16;
+            sheet.Columns().AdjustToContents();
+
+            return workbook;
+        }
+
+        public static XLWorkbook GetImportTemplate(this IEnumerable<CostCurrentProfitLossReportDTO> items, int year)
+        {
+            if (items == null || !items.Any())
+                return null;
+
+            var workbook = new XLWorkbook();
+            var sheet = workbook.Worksheets.Add(_sheetName);
+
+            sheet.RightToLeft = true;
+            sheet.Cell(1, 1).Value = "ورود اطلاعات برای سال مالی : " + year;
+            sheet.Range(1, 1, 1, 6).Merge();
+
+            sheet.Cell(2, 1).Value = "عنوان سازمان";
+            sheet.Cell(2, 2).Value = "کد سازمان";
+            sheet.Cell(2, 3).Value = "شرح";
+            sheet.Cell(2, 4).Value = "کد شرح";
+            sheet.Cell(2, 5).Value = $"عملکرد سال{year - 2}";
+            sheet.Cell(2, 6).Value = $"عملکرد سال {year - 1}";
+
+            var totalCount = items.Count();
+            int row = 3;
+            for (int i = 0; i < totalCount; i++)
+            {
+                var item = items.ElementAt(i);
+                sheet.Cell(row, 1).Value = item.OrganizationDisplay;
+                sheet.Cell(row, 2).Value = item.OrganizationId;
+                sheet.Cell(row, 3).Value = item.SectionTypeDisplay;
+                sheet.Cell(row, 4).Value = item.SectionTypeId;
+                row++; //for keeping index in table records
+            }
+
+            var range = sheet.Range(2, 1, row - 1, 10);
+            range.Column(3).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Right;
+
+            //Other
+            range.Column(5).Style.NumberFormat.Format = ConstantReport.__NumberFormat;
+            range.Column(5).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            range.Column(6).Style.NumberFormat.Format = ConstantReport.__NumberFormat;
+            range.Column(6).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            //
+            var table = range.CreateTable($"{_sheetName}_Table");
+            table.Theme = XLTableTheme.TableStyleMedium16;
+            sheet.Columns().AdjustToContents();
+
+            return workbook;
+        }
+
+    }
+}
