6a7636eb95f11b522186dcf4b483c8c8b32b5a8b
Your Name <you@example.com>
2022-09-21 16:10:56 +0430

    Add Entity

---
+++ src/Datiss.Budget.Entities/DWH/CostCurrentProfitLossReport.cs
@@ -0,0 +1,40 @@
+﻿using Datiss.Budget.Entities.AuditableEntity;
+
+namespace Datiss.Budget.Entities.DWH
+{
+    public  class CostCurrentProfitLossReport : IAuditableEntity
+    {
+        public CostCurrentProfitLossReport() { }
+
+        #region Properties
+
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public int SectionTypeId { get; set; }
+
+        public long FunctionalYear_1 { get; set; }
+
+        public long FunctionalBasicYear { get; set; }
+
+        public long ApproveYear_1 { get; set; }
+
+        public long ForcastY { get; set; }
+
+
+        #endregion
+
+        #region Navigations
+
+        public FinanceYear FinanceYear { get; set; }
+
+        public Organization Organization { get; set; }
+
+        public Constant SectionType { get; set; }
+
+        #endregion
+    }
+}
