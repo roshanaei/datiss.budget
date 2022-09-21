db0ebaefa41f73b852077a939322cd7452cbeed7
Your Name <you@example.com>
2022-09-21 17:34:27 +0430

    Add Interface -ImportViewModel -FilterInput -Service

---
+++ src/Datiss.Budget.Services/Contracts/ICostCurrentProfitLossReportService.cs
@@ -0,0 +1,25 @@
+﻿using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Services.Infrastructure;
+using Datiss.Budget.Services.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services
+{
+    public interface ICostCurrentProfitLossReportService
+    {
+        Task<CostCurrentProfitLossReport> GetByIdAsync(int id);
+        Task<ValidationResult<CostCurrentProfitLossReportDTO>> UpdateAsync(UpdateCostCurrentProfitLossReportDTO model);
+        Task<OrganizationDeleteDataResult> HardDeleteAsync(int yearId, int organizationId);
+        Task<PagedResult<CostCurrentProfitLossReportDTO>> GetListAsync(CostCurrentProfitLossReportFilterDTO filter);
+        Task CopyAsync(int sourceYearId, int sourceOrgId, int destYearId);
+        Task<ValidationResult> CalculationAsync(int yearId, int organizationId);
+        Task<IEnumerable<CostCurrentProfitLossReportDTO>> GetExportItemsAsync(int yearId, int organizationId);
+        Task<ImportResult> ImportExcelAsync(IFormFile fileInfo, int yearId, bool continueIfAnyOrgMissing = false);
+    }
+}
---
+++ src/Datiss.Budget.Services/Excel/Models/CostCurrentProfitLossReportImportModel.cs
@@ -0,0 +1,31 @@
+﻿using Ganss.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datiss.Budget.Enum;
+
+namespace Datiss.Budget.Services.Excel.Models
+{
+    public class CostCurrentProfitLossReportImportModel
+    {
+        [Column(MappingDirections.Both, Letter = "A")]
+        public string OrganizationDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "B")]
+        public int OrganizationId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public string SectionTypeDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "D")]
+        public int SectionTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "E")]
+        public long FunctionalBasicYear { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "F")]
+        public long FunctionalYear_1 { get; set; }
+    }
+}
