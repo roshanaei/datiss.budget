65a03954470339c38213e9acfd2e8c0e55fce881
Your Name <you@example.com>
2022-09-28 17:48:15 +0330

    Add FilterDTO & Contracts

--- src/Datiss.Budget.Services/Models/FilterInputDTO.cs
+++ src/Datiss.Budget.Services/Models/FilterInputDTO.cs
@@ -397,6 +397,15 @@ namespace Datiss.Budget.Services.Models
         public int? SectionTypeId { get; set; }
     }
 
+    public class TotalBudgetWReportFilterDTO : FilterInputDTO
+    {
+        public int? YearId { get; set; }
+        public int? OrganizationId { get; set; }
+        public int? NumberYear { get; set; }
+        public int? SectionTypeId { get; set; }
+    }
+
+
     public class CostCurrentProfitLossReportFilterDTO : FilterInputDTO
     {
         public int? YearId { get; set; }
