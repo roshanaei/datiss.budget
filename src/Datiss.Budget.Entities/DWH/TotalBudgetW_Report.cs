b050f6488ea72272e7afcd8753b5427caed2ad40
Your Name <you@example.com>
2022-09-28 17:07:36 +0330

    Add Entity

---
+++ src/Datiss.Budget.Entities/DWH/TotalBudgetW_Report.cs
@@ -0,0 +1,48 @@
+﻿using Datiss.Budget.Entities.AuditableEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datiss.Budget.Enum;
+
+namespace Datiss.Budget.Entities.DWH
+{
+    public class TotalBudgetW_Report :IAuditableEntity
+    {
+        public TotalBudgetW_Report() { }
+
+        #region Properties
+        public int Id { get; set; }
+
+        public int YearId { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public int SectionTypeId { get; set; }
+
+        public long FunctionalYear_1 { get; set; }
+
+        public long FunctionalBasicYear { get; set; }
+
+        public long ApproveYear_1 { get; set; }
+
+        public long ForcastY { get; set; }
+
+        public decimal ForcastFunctionalPercent { get; set; }
+
+        public decimal ForcastBudgetPercent { get; set; }
+
+        #endregion
+
+        #region Navigations
+
+        public FinanceYear FinanceYear { get; set; }
+
+        public Organization Organization { get; set; }
+
+        public Constant SectionType { get; set; }
+
+        #endregion
+    }
+}
