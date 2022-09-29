159661d75b39495ab986c1bf9260f20175768ead
Your Name <you@example.com>
2022-09-29 16:05:55 +0330

    Add MapperConfig

--- src/Datiss.Budget.Services/Infrastructure/MapperConfig.cs
+++ src/Datiss.Budget.Services/Infrastructure/MapperConfig.cs
@@ -162,6 +162,12 @@ namespace Datiss.Budget.Services.Infrastructure
                   .Map(d => d.Year, s => s.FinanceYear != null ? s.FinanceYear.Year : 0)
                   .Map(d => d.OrganizationDisplay, s => s.Organization != null ? s.Organization.Title : null)
                   .Map(d => d.SectionTypeDisplay, s => s.SectionType != null ? s.SectionType.Title : null);
+
+            TypeAdapterConfig<TotalBudgetWsReport, TotalBudgetWsReportDTO>
+                  .NewConfig()
+                  .Map(d => d.Year, s => s.FinanceYear != null ? s.FinanceYear.Year : 0)
+                  .Map(d => d.OrganizationDisplay, s => s.Organization != null ? s.Organization.Title : null)
+                  .Map(d => d.SectionTypeDisplay, s => s.SectionType != null ? s.SectionType.Title : null);
         }
     }
 }
