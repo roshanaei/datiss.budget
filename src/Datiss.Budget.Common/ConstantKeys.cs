50f3273f44080b428423ed1cc387410391594792
Your Name <you@example.com>
2022-11-09 19:50:28 +0330

    AddService

--- src/Datiss.Budget.Common/ConstantKeys.cs
+++ src/Datiss.Budget.Common/ConstantKeys.cs
@@ -59,5 +59,8 @@
         public const string __CCProfitLossReportType = "CCProfitLossReport";
         public const string __GeneralWaterReportType = "GeneralWaterReport";
         public const string __GeneralWasteReportType = "GeneralWasteReport";
+        public const string __WInvestmentReportType  = "CostForcastWInvestmentReport";
+        public const string __WsInvestmentReportType = "CostForcastWsInvestmentReport";
+
     }
 }
