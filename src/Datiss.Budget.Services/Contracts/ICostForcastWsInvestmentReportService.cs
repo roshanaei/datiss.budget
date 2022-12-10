cb225ed54167a85d41c0cdf28c5572e2f10e7dbf
Your Name <you@example.com>
2022-12-10 12:43:17 +0330

    AddService

---
+++ src/Datiss.Budget.Services/Contracts/ICostForcastWsInvestmentReportService.cs
@@ -0,0 +1,25 @@
+﻿using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Services.Infrastructure;
+using Datiss.Budget.Services.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services
+{
+    public interface ICostForcastWsInvestmentReportService
+    {
+        Task<CostForcastWsInvestmentReport> GetByIdAsync(int id);
+        Task<ValidationResult<CostForcastWsInvestmentReportDTO>> UpdateAsync(UpdateCostForcastWsInvestmentReportDTO model);
+        Task<OrganizationDeleteDataResult> HardDeleteAsync(int yearId, int organizationId);
+        Task<PagedResult<CostForcastWsInvestmentReportDTO>> GetListAsync(CostForcastWsInvestmentReportFilterDTO filter);
+        Task CopyAsync(int sourceYearId, int sourceOrgId, int destYearId);
+        Task<ValidationResult> CalculationAsync(int yearId, int organizationId);
+        Task<IEnumerable<CostForcastWsInvestmentReportDTO>> GetExportItemsAsync(int yearId, int organizationId);
+        Task<ImportResult> ImportExcelAsync(IFormFile fileInfo, int yearId, bool continueIfAnyOrgMissing = false);
+    }
+}
