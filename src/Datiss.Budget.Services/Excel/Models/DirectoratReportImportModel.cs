39f703274ddb45cbf900cd3b54b94a47852723ed
katayon <katayon.bagherimaki@gmail.com[Bgit git pull origin mastergit config --global user.email katayon.bagherimaki@gmail.com>
2023-01-19 22:14:32 +0330

    AddImportViewModel

---
+++ src/Datiss.Budget.Services/Excel/Models/DirectoratReportImportModel.cs
@@ -0,0 +1,28 @@
+﻿using Ganss.Excel;
+
+namespace Datiss.Budget.Services.Excel.Models
+{
+    public class DirectoratReportImportModel
+    {
+        [Column(MappingDirections.Both, Letter = "A")]
+        public string OrganizationDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "B")]
+        public int OrganizationId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "C")]
+        public string SectionTypeDisplay { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "D")]
+        public int? SectionTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "E")]
+        public long ForcastY { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "F")]
+        public long FunctionalBasicYear { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "G")]
+        public long FunctionalYear_1 { get; set; }
+    }
+}
