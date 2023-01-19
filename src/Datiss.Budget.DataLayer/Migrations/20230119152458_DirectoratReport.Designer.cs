f085c449a19c1a90aa486c4fffeb0a7e359bb79d
katayon <katayon.bagherimaki@gmail.com[Bgit git pull origin mastergit config --global user.email katayon.bagherimaki@gmail.com>
2023-01-19 22:07:44 +0330

    AddMigration

--- src/Datiss.Budget.DataLayer/Migrations/20230119152458_DirectoratReport.Designer.cs
+++ src/Datiss.Budget.DataLayer/Migrations/20230119152458_DirectoratReport.Designer.cs
@@ -4,14 +4,16 @@ using Datiss.Budget.DataLayer.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Datiss.Budget.DataLayer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230119152458_DirectoratReport")]
+    partial class DirectoratReport
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
