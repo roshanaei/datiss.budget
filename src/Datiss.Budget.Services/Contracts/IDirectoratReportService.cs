c5557445fca82baa9eddd08bc9ee53db21c7bb38
katayon <katayon.bagherimaki@gmail.com[Bgit git pull origin mastergit config --global user.email katayon.bagherimaki@gmail.com>
2023-01-19 22:39:43 +0330

    AddService&InterFace

---
+++ src/Datiss.Budget.Services/Contracts/IDirectoratReportService.cs
@@ -0,0 +1,21 @@
+﻿using Datiss.Budget.Entities.DWH;
+using Datiss.Budget.Services.Infrastructure;
+using Datiss.Budget.Services.Models;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Datiss.Budget.Services
+{
+    public interface IDirectoratReportService
+    {
+        Task<DirectoratReport> GetByIdAsync(int id);
+        Task<ValidationResult<DirectoratReportDTO>> UpdateAsync(UpdateDirectoratReportDTO model);
+        Task<OrganizationDeleteDataResult> HardDeleteAsync(int yearId, int organizationId);
+        Task<PagedResult<DirectoratReportDTO>> GetListAsync(DirectoratReportFilterDTO filter);
+        Task CopyAsync(int sourceYearId, int sourceOrgId, int destYearId);
+        Task<ValidationResult> CalculationAsync(int yearId, int organizationId);
+        Task<IEnumerable<DirectoratReportDTO>> GetExportItemsAsync(int yearId, int organizationId);
+        Task<ImportResult> ImportExcelAsync(IFormFile fileInfo, int yearId, bool continueIfAnyOrgMissing = false);
+    }
+}
