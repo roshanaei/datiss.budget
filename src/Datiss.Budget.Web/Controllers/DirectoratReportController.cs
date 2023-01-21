534a973680b2960ed309a260077b04ee589eae42
Your Name <you@example.com>
2023-01-21 10:43:02 +0330

    Change ConstantKey

--- src/Datiss.Budget.Web/Controllers/DirectoratReportController.cs
+++ src/Datiss.Budget.Web/Controllers/DirectoratReportController.cs
@@ -378,7 +378,7 @@ namespace Datiss.Budget.Web.Controllers
         {
             var year = await _financeYearService.GetByIdAsync(yearId);
             var organizations = await _organizationService.GetWithChildrenAsync(orgId, input: true);
-            var sectionTypes = await _constantService.GetDataByKeyAsync(ConstantKeys.__GeneralWaterReportType);
+            var sectionTypes = await _constantService.GetDataByKeyAsync(ConstantKeys.__DirectorateReportType);
             var unitTypes = await _constantService.GetDataByKeyAsync(ConstantKeys.__MeasurementType);
 
 
