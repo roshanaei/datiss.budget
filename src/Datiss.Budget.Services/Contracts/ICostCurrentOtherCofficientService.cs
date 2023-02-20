936803cbdc50f422ce1e93730493653f9ccecf5c
Your Name <you@example.com>
2023-02-20 16:01:21 +0330

    addInfrastructure

---
+++ src/Datiss.Budget.Services/Contracts/ICostCurrentOtherCofficientService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Datiss.Budget.Services.Infrastructure;
+using Datiss.Budget.Services.Models;
+using Datiss.Budget.Entities.DWH;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace Datiss.Budget.Services.Contracts
+{
+    public interface ICostCurrentOtherCofficientService
+    {
+
+        Task<CostCurrentOtherCofficient> GetByIdAsync(int id);
+
+        Task<ValidationResult<CostCurrentOtherCofficientDTO>> CreateAsync(CreateCostCurrentOtherDTO model);
+
+        Task<ValidationResult<CostCurrentOtherCofficientDTO>> UpdateAsync(UpdateCostCurrentOtherDTO model);
+
+        Task HardDeleteAsync(int Id);
+
+        Task<PagedResult<CostCurrentOtherCofficientDTO>> GetListAsync(CostCurrentOtherFilterDTO filter);
+
+        Task CopyAsync(int sourceYearId, int sourceOrgId, int destYearId);
+
+        Task<Stream> ExportExcelAsync(CostCurrentOtherCofficientFilterDTO filter);
+
+        Task<IEnumerable<CostCurrentOtherCofficientDTO>> GetExportItemsAsync(int yearId, int organizationId);
+
+        Task<ImportResult> ImportExcelAsync(IFormFile fileInfo, int yearId, bool continueIfAnyOrgMissing = false);
+
+    }
+
+}
