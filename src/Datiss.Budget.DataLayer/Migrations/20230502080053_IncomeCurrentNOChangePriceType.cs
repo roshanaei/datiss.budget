78a99518d0cc4e0c750cdf1421d179fb59265ae1
Your Name <you@example.com>
2023-05-02 11:38:59 +0330

    AddMigration

---
+++ src/Datiss.Budget.DataLayer/Migrations/20230502080053_IncomeCurrentNOChangePriceType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Datiss.Budget.DataLayer.Migrations
+{
+    public partial class IncomeCurrentNOChangePriceType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "NOICPrice",
+                table: "IncomeCurrentNOperationals",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "NOICPrice",
+                table: "IncomeCurrentNOperationals",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
