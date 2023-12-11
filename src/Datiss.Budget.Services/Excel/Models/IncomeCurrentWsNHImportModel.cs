b8ca00171f4227a9b5464bc0d68ab140b6ec3a1b
Your Name <you@example.com>
2023-12-11 13:33:13 +0330

    IncomeWsCorrectExcell

--- src/Datiss.Budget.Services/Excel/Models/IncomeCurrentWsNHImportModel.cs
+++ src/Datiss.Budget.Services/Excel/Models/IncomeCurrentWsNHImportModel.cs
@@ -21,5 +21,44 @@ namespace Datiss.Budget.Services.Excel.Models
         [Column(MappingDirections.Both, Letter = "D")]
         public int UserTypeId { get; set; }
 
+        [Column(MappingDirections.Both, Letter = "E")]
+        public int NumberUser { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "F")]
+        public int UnitUser { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "G")]
+        public decimal AvgConsumeUser { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "H")]
+        public decimal ExcessConsumption { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "I")]
+        public int ConsumptionUser { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "J")]
+        public long Cost { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "K")]
+        public long Income { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "L")]
+        public long SubscriptionIncome { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "M")]
+        public long ExcessIncome { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "N")]
+        public long SeasonalIncome { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "O")]
+        public long Note3Price { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "P")]
+        public long TotalIncome { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "Q")]
+        public long Note3Income { get; set; }
+
     }
 }
