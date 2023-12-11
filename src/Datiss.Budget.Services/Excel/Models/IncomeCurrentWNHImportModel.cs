41bd82f75284f8d00d50a2635ed4f4e5e77676fd
Your Name <you@example.com>
2023-12-11 11:59:00 +0330

    ChangeIncomeWNHimportViewModel

--- src/Datiss.Budget.Services/Excel/Models/IncomeCurrentWNHImportModel.cs
+++ src/Datiss.Budget.Services/Excel/Models/IncomeCurrentWNHImportModel.cs
@@ -20,5 +20,54 @@ namespace Datiss.Budget.Services.Excel.Models
 
         [Column(MappingDirections.Both, Letter = "D")]
         public int UserTypeId { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "E")]
+        public int NumberUser { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "F")]
+        public int UnitUser { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "G")]
+        public decimal AvgConsumeUser { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "H")]
+        public int ConsumptionUser { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "I")]
+        public decimal ExcessConsumption { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "J")]
+        public long Cost { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "K")]
+        public long Income { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "L")]
+        public long ExcessIncome { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "M")]
+        public long SeasonalIncome { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "N")]
+        public long Note3Price { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "O")]
+        public long Note3Income { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "P")]
+        public long SubscriptionIncome { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "Q")]
+        public decimal TotalIncome { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "R")]
+        public long Note2Income { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "S")]
+        public int WasteVolume { get; set; }
+
+        [Column(MappingDirections.Both, Letter = "T")]
+        public int Diff_ConsWsVolume { get; set; }
+
     }
 }
