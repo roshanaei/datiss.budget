cdd726b236ab5af8f1c217b350791674706d84ef
Alireza Ganji <ganji.ali37@gmail.com>
2025-11-30 16:51:27 +0330

    Added `ForcastIncome` to the `CofficientsGroup` enum. Updated `_Menu.cshtml` to include a new menu item for Capital Income under the `CapitalIncome` section. The menu item is conditionally displayed based on admin status or user permissions for `CofficientController`.

--- src/Datiss.Budget.Entities/Enum/CofficientsGroup.cs
+++ src/Datiss.Budget.Entities/Enum/CofficientsGroup.cs
@@ -5,5 +5,6 @@
         CurrentIncome = 0,//درآمدهای جاری
         CurrentCost = 1,  //هزینه های جاری
         ForcastCost = 2,  //هزینه سرمایه ای
+        ForcastIncome = 3 // درآمد سرمایه ای
     }
 }
